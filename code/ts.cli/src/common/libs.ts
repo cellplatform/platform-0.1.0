a115d360efea5f80dddff094c75723990c3c2b96
Phil Cockfield <phil@cockfield.net>
2019-02-18 11:56:14 +1300

    fix call-sites

--- code/ts.cli/src/common/libs.ts
+++ code/ts.cli/src/common/libs.ts
@@ -1,2 +1,5 @@
 export { fs } from '@platform/fs';
-export { exec, result, tasks } from '@platform/util.exec';
+export { exec, tasks } from '@platform/util.exec';
+
+import { util } from '@platform/util.exec';
+export const result = util.result;
