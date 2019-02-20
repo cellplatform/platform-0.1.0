b0e15455ee4c18d4e40134308d9213b14247ace0
Phil Cockfield <phil@cockfield.net>
2019-02-21 11:01:55 +1300

    Stub in text samples.

--- code/ui.text/src/components/Test.tsx
+++ code/ui.text/src/components/Test.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { css } from '../common';
+import { color, css } from '../common';
+import { Text } from '..';
+
+const LOREM =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec quam lorem. Praesent fermentum, augue ut porta varius, eros nisl euismod ante, ac suscipit elit libero nec dolor. Morbi magna enim, molestie non arcu id, varius sollicitudin neque. In sed quam mauris. Aenean mi nisl, elementum non arcu quis, ultrices tincidunt augue. Vivamus fermentum iaculis tellus finibus porttitor. Nulla eu purus id dolor auctor suscipit. Integer lacinia sapien at ante tempus volutpat.';
 
 export const Test = () => {
   const styles = {
@@ -8,8 +12,36 @@ export const Test = () => {
 
   return (
     <div {...styles.base}>
-      Text
-      <div />
+      <Text>
+        Default. <br />
+        {LOREM}
+      </Text>
+      <Hr />
+      <Text fontWeight={'LIGHT'}>
+        fontWeight: LIGHT <br />
+        {LOREM}
+      </Text>
+      <Hr />
+      <Text fontWeight={'NORMAL'}>
+        fontWeight: NORMAL <br />
+        {LOREM}
+      </Text>
+      <Hr />
+      <Text fontWeight={'BOLD'}>
+        fontWeight: BOLD <br />
+        {LOREM}
+      </Text>
     </div>
   );
+};
+
+const Hr = () => {
+  const styles = {
+    base: css({
+      border: 'none',
+      borderTop: `solid 1px ${color.format(-0.1)}`,
+      MarginY: 30,
+    }),
+  };
+  return <hr {...styles.base} />;
 };
