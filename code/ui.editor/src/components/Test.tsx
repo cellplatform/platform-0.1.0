4703daddedeb0cc139c11dfea354f6dbb8bb0544
Phil Cockfield <phil@cockfield.net>
2019-02-25 14:45:34 +1300

    add prose mirror modules

--- code/ui.editor/src/components/Test.tsx
+++ code/ui.editor/src/components/Test.tsx
@@ -1,10 +1,42 @@
 import * as React from 'react';
-import { css } from '../common';
+import { css, color } from '../common';
+import { ObjectView } from './primitives';
 
 export const Test = () => {
   const styles = {
-    base: css({ padding: 30 }),
+    base: css({
+      Absolute: 0,
+      boxSizing: 'border-box',
+      display: 'flex',
+    }),
+    columns: css({
+      margin: 20,
+      Flex: 'horizontal-stretch-stretch',
+      flex: 1,
+    }),
+    left: css({
+      flex: 1,
+      border: `solid 1px ${color.format(-0.1)}`,
+    }),
+    right: css({
+      marginLeft: 15,
+      width: 300,
+    }),
   };
 
-  return <div {...styles.base}>Test!</div>;
+  const data = { foo: 123 };
+
+  return (
+    <div {...styles.base}>
+      <div {...styles.columns}>
+        <div {...styles.left}>
+          <div>left</div>
+        </div>
+
+        <div {...styles.right}>
+          <ObjectView name={'state'} data={data} />
+        </div>
+      </div>
+    </div>
+  );
 };
--- code/ui.editor/test/web.tsx
+++ code/ui.editor/test/web.tsx
@@ -1,9 +1,7 @@
 import '@babel/polyfill';
 import '../node_modules/@uiharness/electron/css/normalize.css';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import { Test } from '../src/components/Test';
 
 ReactDOM.render(<Test />, document.getElementById('root'));
