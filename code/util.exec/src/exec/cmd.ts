10975c0a332261c57c1b73884c39fed82cce4a11
Phil Cockfield <phil@cockfield.net>
2019-02-28 22:31:15 +1300

    export command from exec

--- code/util.exec/src/exec/cmd.ts
+++ code/util.exec/src/exec/cmd.ts
@@ -3,5 +3,4 @@ import { Command } from '../command';
 export { run } from './cmd.run';
 export { runList } from './cmd.runList';
 
-export { Command };
 export const create = Command.create;
--- code/util.exec/src/exec/exec.ts
+++ code/util.exec/src/exec/exec.ts
@@ -1,6 +1,9 @@
 import * as cmd from './cmd';
 import * as process from './process';
+import { Command } from '../command';
 
 export { cmd, process };
 export { tasks } from '../tasks';
 export * from '../types';
+
+export const create = Command.create;
