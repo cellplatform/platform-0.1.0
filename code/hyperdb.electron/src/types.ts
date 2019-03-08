80c9bf9b9261cd88921f3710338abc4a2d3c5eb6
Phil Cockfield <phil@cockfield.net>
2019-03-09 09:57:44 +1300

    delete duplicate code

--- code/hyperdb.electron/src/types.ts
+++ code/hyperdb.electron/src/types.ts
@@ -9,76 +9,8 @@ import { IpcClient, ILog } from '@platform/electron/lib/types';
 export { IpcClient, ILog };
 
 /**
- * [Network]
- */
-
-type Bitfield = any;
-type TreeIndex = { [key: string]: Bitfield };
-
-/**
- * Local writable feed.
- * You have to get an owner of the hyperdb to authorize you to have your writes replicate.
- * The first person to create the hyperdb is the first owner.
- */
-export type IFeed = {
-  id: Buffer;
-  key: Buffer;
-  bitfield: Bitfield;
-  discoveryKey: Buffer;
-  secretKey: Buffer;
-  peers: IPeer[];
-  tree: TreeIndex;
-  length: number;
-  maxRequests: number;
-  byteLength: number;
-  remoteLength: number;
-  allowPush: boolean;
-  live: boolean;
-  opened: boolean;
-  readable: boolean;
-  sparse: boolean;
-  writable: boolean;
-  closed: false;
-};
-
-export type IPeer = {
-  channel: Buffer;
-  host: string;
-  id: Buffer;
-  initiator: boolean;
-  port: number;
-  type: 'tcp';
-};
-
-export type IProtocol = {
-  id: Buffer;
-  key: Buffer;
-  ack: boolean;
-  allowHalfOpen: boolean;
-  discoveryKey: Buffer;
-  expectedFeeds: number;
-  feeds: IFeed[];
-  userData: any;
-  maxFeeds: number;
-  live: boolean;
-  readable: boolean;
-  writable: boolean;
-  encrypted: boolean;
-  extension: any[];
-  destroyed: boolean;
-
-  remoteId: Buffer;
-  remoteAck: boolean;
-  remoteDiscoveryKey: Buffer;
-  removeLive: boolean;
-  remoteUserData: any;
-  remoteExtensions: any[];
-};
-
-/**
  * [IPC] Events
  */
-
 export type DbIpcClient = IpcClient<DbIpcEvent>;
 export type DbIpcEvent = IDbGetStateEvent | IDbUpdateStateEvent | IDbInvokeEvent | DbEvent;
 
