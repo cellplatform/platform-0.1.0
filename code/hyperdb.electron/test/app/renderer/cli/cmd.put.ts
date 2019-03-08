2960c86b881f33a0dd6297cab0e0b75cef9f82b0
Phil Cockfield <phil@cockfield.net>
2019-03-08 14:36:51 +1300

    parsing json

--- code/hyperdb.electron/test/app/renderer/cli/cmd.put.ts
+++ code/hyperdb.electron/test/app/renderer/cli/cmd.put.ts
@@ -1,4 +1,4 @@
-import { Command } from '../common';
+import { Command, value as valueUtil, hjson } from '../common';
 import { updateWatch } from './cmd.watch';
 import * as t from './types';
 
@@ -9,7 +9,18 @@ type P = t.ITestCommandProps;
  */
 export const put = Command.create<P>('put', async e => {
   const { db } = e.props;
-  const [key, value] = e.args.params;
+  const params = e.args.params;
+  const key = params[0];
+  let value = params[1];
+
+  if (typeof value === 'string' && valueUtil.isJson(value)) {
+    try {
+      value = hjson.parse(value);
+    } catch (error) {
+      // Ignore.  Just set the given value.
+    }
+  }
+
   if (key) {
     await db.put(key as any, value);
     await updateWatch({ db, addKeys: [key.toString()] });
