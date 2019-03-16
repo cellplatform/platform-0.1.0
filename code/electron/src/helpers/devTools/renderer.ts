03b520d9800691564939f8253c49c86ee06213be
Phil Cockfield <phil@cockfield.net>
2019-03-16 21:10:31 +1300

    Clear console on load.

--- code/electron/src/helpers/devTools/renderer.ts
+++ code/electron/src/helpers/devTools/renderer.ts
@@ -1,5 +1,6 @@
 import { delay } from 'rxjs/operators';
 import * as t from './types';
+import { value } from '@platform/util.value';
 
 /**
  * Renderer API for working with the dev-tools.
@@ -11,12 +12,18 @@ export class DevTools {
   /**
    * [Constructor]
    */
-  constructor(args: { ipc: t.IpcClient }) {
+  constructor(args: { ipc: t.IpcClient; clearConsoleOnLoad?: boolean }) {
     const ipc = (this.ipc = args.ipc);
     ipc
       .on<t.ClearConsoleEvent>('@platform/DEV_TOOLS/clearConsole')
       .pipe(delay(0))
       .subscribe(e => clearConsole());
+
+    // Perform initial clear of the console upon load to
+    // remove warning junk.
+    if (value.defaultValue(args.clearConsoleOnLoad, true)) {
+      clearConsole();
+    }
   }
 
   /**
