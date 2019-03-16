45637612e5cfc2680b7934c7704fe113ce4eb671
Phil Cockfield <phil@cockfield.net>
2019-03-17 11:32:21 +1300

    Update call sites (refs to UI components from UIHarness)

--- code/ui.datagrid/test/components/common.ts
+++ code/ui.datagrid/test/components/common.ts
@@ -4,4 +4,4 @@ import datagrid from '../../src';
 export { datagrid };
 export * from '../../src/common';
 export { MeasureSize } from '@platform/react';
-export { ObjectView, Button, Hr } from '@uiharness/electron/lib/components';
+export { ObjectView, Button, Hr } from '@uiharness/ui';
--- code/ui.icon/test/components/common.ts
+++ code/ui.icon/test/components/common.ts
@@ -4,4 +4,4 @@ import '../../node_modules/@platform/css/reset.css';
 export * from '../../src/common';
 export * from '../../src/components.test';
 export { Icon, ImageSprite } from '../../src';
-export { Button, ObjectView, Hr } from '@uiharness/electron/lib/components';
+export { Button, ObjectView, Hr } from '@uiharness/ui';
