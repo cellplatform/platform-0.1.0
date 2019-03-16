808d105f07bddb1ea3baec05123685f37161efd7
Phil Cockfield <phil@cockfield.net>
2019-03-17 11:38:23 +1300

    stub in ui.tree module

---
+++ code/ui.tree/README.md
@@ -0,0 +1,6 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![NPM](https://img.shields.io/npm/v/@platform/ui.tree.svg?colorB=blue&style=flat)](https://www.npmjs.com/package/@platform/ui.tree)
+
+# @platform/ui.tree
+Flexible hierarchical tree view.
+
---
+++ code/ui.tree/test/components/Test.tsx
@@ -0,0 +1,34 @@
+import '../../node_modules/@platform/css/reset.css';
+import '@babel/polyfill';
+
+import { Button, ObjectView } from '@uiharness/ui';
+import * as React from 'react';
+
+import { MyComponent } from '../../src';
+
+/**
+ * Test Component
+ */
+export type IState = { count?: number };
+export class Test extends React.PureComponent<{}, IState> {
+  public state: IState = {};
+
+  public render() {
+    return (
+      <div style={{ paddingLeft: 25 }}>
+        <MyComponent />
+        <div style={{ marginBottom: 10 }}>
+          <Button label={'Increment'} onClick={this.increment(1)} />
+          <Button label={'Decrement'} onClick={this.increment(-1)} />
+        </div>
+        <ObjectView name={'state'} data={this.state} />
+      </div>
+    );
+  }
+
+  private increment = (amount: number) => {
+    return () => {
+      this.setState({ count: (this.state.count || 0) + amount });
+    };
+  };
+}
