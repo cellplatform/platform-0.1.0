8aa3616302bd18f2646a3ff21c9d4070ae68df21
Phil Cockfield <phil@cockfield.net>
2019-03-20 17:45:35 +1300

    rendering sample grid

--- code/hyperdb.electron-test/test/renderer/components/Shell.Main/components/DbGrid.tsx
+++ code/hyperdb.electron-test/test/renderer/components/Shell.Main/components/DbGrid.tsx
@@ -1,24 +1,41 @@
 import * as React from 'react';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { css, color, GlamorValue, t } from '../../../common';
+import { filter, takeUntil } from 'rxjs/operators';
 
 import dg from '../../../../../node_modules/@platform/ui.datagrid';
+import { css, GlamorValue, t } from '../../../common';
 
 export type IDbGridProps = { db: t.ITestRendererDb; style?: GlamorValue };
-export type IDbGridState = {};
+export type IDbGridState = { values?: dg.IGridValues };
+
+const DEFAULT = {
+  A1: 'A1',
+  B1: 'locked',
+  B2: 'cancel',
+};
 
 export class DbGrid extends React.PureComponent<IDbGridProps, IDbGridState> {
-  public state: IDbGridState = {};
+  public state: IDbGridState = { values: DEFAULT };
   private unmounted$ = new Subject();
   private state$ = new Subject<Partial<IDbGridState>>();
   private events$ = new Subject<dg.GridEvent>();
+
+  public datagrid!: dg.DataGrid;
+  private datagridRef = (ref: dg.DataGrid) => (this.datagrid = ref);
 
   /**
    * [Lifecycle]
    */
   public componentWillMount() {
     this.state$.pipe(takeUntil(this.unmounted$)).subscribe(e => this.setState(e));
+
+    const events$ = this.events$.pipe(takeUntil(this.unmounted$));
+
+    events$.pipe(filter(e => !['GRID/keydown'].includes(e.type))).subscribe(e => {
+      // const cell = e.payload.cell;
+      // const key = cell ? cell.key : undefined;
+      console.log('🌳  EVENT', e.type, e.payload);
+    });
   }
 
   public componentWillUnmount() {
@@ -26,21 +43,54 @@ export class DbGrid extends React.PureComponent<IDbGridProps, IDbGridState> {
   }
 
   /**
+   * [Properties]
+   */
+  public get grid() {
+    return this.datagrid.grid;
+  }
+
+  /**
    * [Render]
    */
   public render() {
-    console.log('dg', dg);
-
     const styles = {
       base: css({
-        backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
+        position: 'relative',
         flex: 1,
+      }),
+      grid: css({
+        Absolute: 0,
       }),
     };
     return (
       <div {...css(styles.base, this.props.style)}>
-        <div>DbGrid</div>
+        <dg.DataGrid
+          key={'test.grid'}
+          ref={this.datagridRef}
+          values={this.state.values}
+          events$={this.events$}
+          factory={this.factory}
+          totalColumns={52}
+          totalRows={1000}
+          initial={{ selection: 'A1' }}
+          style={styles.grid}
+        />
       </div>
     );
   }
+
+  private factory: dg.GridFactory = req => {
+    switch (req.type) {
+      case 'EDITOR':
+        return <div>Editor</div>;
+
+      case 'CELL':
+        const value = typeof req.value === 'object' ? JSON.stringify(req.value) : req.value;
+        return <div>{value}</div>;
+
+      default:
+        console.log(`Factory type '${req.type}' not supported by test.`);
+        return null;
+    }
+  };
 }
