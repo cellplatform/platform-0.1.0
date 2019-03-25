a540189d87b63dea4e0dd721378239f5f2be0cf9
Phil Cockfield <phil@cockfield.net>
2019-03-25 22:05:13 +1300

    sample URL

--- code/ui.graphql/src/common/fetch.ts
+++ code/ui.graphql/src/common/fetch.ts
@@ -5,7 +5,9 @@ const fetch = require('isomorphic-fetch');
  * - https://github.com/graphql/graphiql#getting-started
  */
 export function graphqlFetcher(graphQLParams: any) {
-  return fetch(window.location.origin + '/graphql', {
+  const url = `${window.location.origin}/graphql`;
+  // const url = `https://api.blocktap.io/graphql`;
+  return fetch(url, {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(graphQLParams),
