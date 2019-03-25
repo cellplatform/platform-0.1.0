8d700282531ad5c1bc962031b98f6555d0f6497b
Phil Cockfield <phil@cockfield.net>
2019-03-25 21:49:35 +1300

    style tweaks

--- code/ui.graphql/src/components/GraphqlEditor/styles.ts
+++ code/ui.graphql/src/components/GraphqlEditor/styles.ts
@@ -1,11 +1,32 @@
 import '../../../node_modules/graphiql/graphiql.css';
-import { css, constants, COLORS } from '../../common';
+import { css, color, constants, COLORS } from '../../common';
 
 const ROOT = constants.CSS.ROOT;
 
 const styles = {
   '.topBar': {
     background: COLORS.DARK,
+    padding: 10,
+  },
+
+  '.toolbar-button': {
+    background: 'none',
+    border: `solid 1px ${color.format(0.1)}`,
+    boxShadow: 'none',
+    color: color.format(0.7),
+    fontSize: 13,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+
+  '.execute-button-wrap': {
+    marginLeft: 0,
+  },
+
+  '.execute-button-wrap button': {
+    boxShadow: 'none',
   },
 };
 
