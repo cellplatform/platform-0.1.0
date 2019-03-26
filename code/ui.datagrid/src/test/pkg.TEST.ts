50468f91f081c1fae81f39a10d557843992e5497
Phil Cockfield <phil@cockfield.net>
2019-03-27 10:14:23 +1300

    test to safeguard against `handsontable` being bumped beyond 6.2.2 (MIT version)

---
+++ code/ui.datagrid/src/test/pkg.TEST.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import { log } from '@platform/log/lib/server';
+
+describe('package.json', () => {
+  const pkg = require('../../package.json');
+
+  it('has locked version: handsontable@6.2.2', () => {
+    const deps = pkg.dependencies;
+    const PKG = 'handsontable';
+    const MIT = '6.2.2';
+
+    if (deps.handsontable !== MIT) {
+      let msg = `\nWARNING [${PKG}] must be version ${log.green(MIT)} not ${log.red(MIT)}.`;
+      msg += log.gray(`\nv${MIT} is the latest version licended under the MIT licence.\n`);
+      log.info.yellow(msg);
+    }
+
+    expect(deps[PKG]).to.eql(MIT);
+  });
+});
