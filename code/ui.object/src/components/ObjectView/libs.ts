c03c86829023a2cd2af84c1bc5bbdaa69bc8a46d
Phil Cockfield <phil@cockfield.net>
2019-04-01 14:22:35 +1300

    bump

--- code/ui.object/src/components/ObjectView/libs.ts
+++ code/ui.object/src/components/ObjectView/libs.ts
@@ -10,8 +10,8 @@ export const ObjectLabel = m.ObjectLabel as React.ComponentClass<any>;
 export const ObjectRootLabel = m.ObjectRootLabel as React.ComponentClass<any>;
 export const ObjectName = m.ObjectName as React.ComponentClass<any>;
 
-export const CHROME_LIGHT = require('react-inspector/lib/styles/themes/chromeLight.js');
-export const CHROME_DARK = require('react-inspector/lib/styles/themes/chromeDark.js');
+export const CHROME_LIGHT = require('../../../themes/chromeLight.js');
+export const CHROME_DARK = require('../../../themes/chromeDark.js');
 
 export const THEME = {
   CHROME_LIGHT: CHROME_LIGHT.default,
