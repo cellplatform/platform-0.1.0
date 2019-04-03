507ae520b622bc4dab7f86b52d6ba8901719f1b6
Phil Cockfield <phil@cockfield.net>
2019-04-03 19:25:02 +1300

    bump

--- code/npm.electron/test/cli/cmds.ts
+++ code/npm.electron/test/cli/cmds.ts
@@ -10,34 +10,10 @@ export const root = Command.create<P>('root')
   .add('install', async e => {
     const { npm } = e.props;
     const { params } = e.args;
-    console.log('e.props', e.props);
-    console.log('e.args', e.args);
-
     const name = (params[0] || '').toString();
     const version = (params[1] || '').toString();
 
     if (name) {
       await npm.install({ name, version });
     }
-
-    // const res = await npm.getInfo('@platform/npm');
-    // console.log('res', res);
-
-    // const dir = `${remote.app.getPath('desktop')}/tmp-app`;
-
-    // console.log('dir', dir);
-    // console.log('fs', fs);
-
-    // await fs.ensureDir(dir);
-
-    // const cmd = exec
-    //   .command()
-    //   .add(`cd ${dir}`)
-    //   .add(`&&`)
-    //   .add(`npm install @platform/libs`);
-
-    // console.log('cmd.toString', cmd.toString());
-    // const res = await cmd.run();
-    // console.log('-------------------------------------------');
-    // console.log('res', res);
   });
---
+++ code/ui.panel/test/components/common.ts
@@ -0,0 +1,6 @@
+import '../../node_modules/@platform/css/reset.css';
+import '@babel/polyfill';
+
+export { Button } from '@uiharness/ui';
+export { Foo } from '@platform/react';
+export * from '../../src/common';
