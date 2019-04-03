e2801f2285163838d48fce27f4902c508759445f
Phil Cockfield <phil@cockfield.net>
2019-04-03 20:46:35 +1300

    delete

--- code/hyperdb.electron.test/test/main.ts
+++ code/hyperdb.electron.test/test/main.ts
@@ -1,12 +1,12 @@
 import { app } from 'electron';
 import uiharness from '@uiharness/electron/lib/main';
 import { filter } from 'rxjs/operators';
-import { fs } from './common';
+import { fs } from '@platform/fs';
 
 import main from '@platform/hyperdb.electron/lib/main';
-import * as t from '../types';
+import * as t from './types';
 
-const config = require('../../.uiharness/config.json') as uiharness.IRuntimeConfig;
+const config = require('../.uiharness/config.json') as uiharness.IRuntimeConfig;
 
 (async () => {
   const context = await uiharness.init({ config });
