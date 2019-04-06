9ba4e938fd020080768a54e972c6bb884008f0d5
Phil Cockfield <phil@cockfield.net>
2019-04-06 19:37:33 +1300

    Only clear console if INPUT is not focused

--- code/electron/src/renderer/keyboard.ts
+++ code/electron/src/renderer/keyboard.ts
@@ -14,7 +14,14 @@ export function init() {
 
   command$
     // Clear console handler.
-    .pipe(filter(e => e.command === 'CONSOLE/clear'))
+    .pipe(
+      filter(e => e.command === 'CONSOLE/clear'),
+      filter(e => {
+        // NB: If an <input> is focused it may have it's own [CMD+K] handler.
+        const el = document.activeElement;
+        return el ? el.tagName !== 'INPUT' : true;
+      }),
+    )
     .subscribe(e => {
       console.clear(); // tslint:disable-line
     });
