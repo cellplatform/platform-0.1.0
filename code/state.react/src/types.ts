c292988ce16d5c44b42fd87923b1f5c8ad1335ae
Phil Cockfield <phil@cockfield.net>
2019-04-10 11:17:10 +1200

    refs

--- code/state.react/src/types.ts
+++
@@ -1,1 +0,0 @@
-export * from '@platform/ui.cli/lib/types';
--- code/state.react/test/components/common.ts
+++
@@ -1,9 +0,0 @@
-import '../../node_modules/@platform/css/reset.css';
-import '@babel/polyfill';
-
-import * as t from '../types';
-
-export { Button, ObjectView, Shell, CommandState, Command } from '@uiharness/ui';
-export * from '../../src/common';
-export * from '../../src';
-export { t };
