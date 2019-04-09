6f898d91951ca4dae2f9a626dd168f63ee8b1429
Phil Cockfield <phil@cockfield.net>
2019-04-09 23:18:23 +1200

    Update test call-site

--- code/ui.datagrid/test/cli/cmd.grid.tsx
+++ code/ui.datagrid/test/cli/cmd.grid.tsx
@@ -1,34 +1,24 @@
 import * as React from 'react';
 
-import { Command, css, t } from '../common';
-import { DataGrid, TestGridView } from '../components/Test.Grid.view';
+import { Command, t } from '../common';
+import { TestGridView } from '../components/Test.Grid.view';
 
 type P = t.ITestCommandProps & { ref: TestGridView; count: number };
-
-let datagrid: DataGrid | undefined;
-const ref = (ref: TestGridView) => {
-  if (ref) {
-    datagrid = ref.datagrid;
-  }
-};
 
 /**
  * The root of the CLI application.
  */
 export const grid = Command.create<P>('g', e => {
-  const styles = { base: css({ Absolute: 0 }) };
-  const el = <TestGridView ref={ref} editorType={'default'} style={styles.base} />;
+  const ref = (ref: TestGridView) => {
+    if (ref) {
+      e.set('ref', ref);
+    }
+  };
+
+  const el = <TestGridView ref={ref} editorType={'default'} style={{ Absolute: 0 }} />;
   e.props.state$.next({ el, view: 'foo' });
 
-  e.set('count', 123);
-  console.log('ns/count:', e.get('count'));
-})
-  .add('focus', e => {
-    console.log('focus/count:', e.get('count'));
-    if (datagrid) {
-      datagrid.focus();
-    }
-  })
-  .add('count', e => {
-    console.log('count:', e.get('count'));
-  });
+  console.log('ns/count:', e.get('count'), e.command.name);
+}).add('focus', e => {
+  e.get('ref').datagrid.focus();
+});
