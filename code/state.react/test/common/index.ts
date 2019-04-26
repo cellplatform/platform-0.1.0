8291b307b775a61ab2a2e8b576b35705474e74c0
Phil Cockfield <phil@cockfield.net>
2019-04-27 11:43:43 +1200

    exports

--- code/state.react/test/common/index.ts
+++ code/state.react/test/common/index.ts
@@ -1,9 +1,8 @@
 import '../../node_modules/@platform/css/reset.css';
 import '@babel/polyfill';
 
-export * from './libs';
-
 import * as t from '../types';
 
+export * from './libs';
 export * from '../../src';
 export { t };
