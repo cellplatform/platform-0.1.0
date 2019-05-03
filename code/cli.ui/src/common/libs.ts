a8a710bc9cb696eb38cc98c954b8f40b55865497
Phil Cockfield <phil@cockfield.net>
2019-05-03 18:26:55 +1200

    test code

--- code/cli.ui/src/common/libs.ts
+++ code/cli.ui/src/common/libs.ts
@@ -1,7 +1,8 @@
 export * from '@platform/cli.spec';
 export { GlamorValue, css, color, events, containsFocus, Keyboard } from '@platform/react';
 export { str } from '@platform/util.string';
-export { value, storage } from '@platform/util.value';
+export { value } from '@platform/util.value';
+export { localStorage } from '@platform/util.local-storage';
 
 /**
  * [Ramda]
--- code/cli.ui/test/cli/cmds.TMP.ts
+++ code/cli.ui/test/cli/cmds.TMP.ts
@@ -1,23 +1,27 @@
-import { Command, t, storage } from '../common';
+import { Command, t, localStorage } from '../common';
 
 export type IMyObject = {
   message: string;
   count: number;
   foo: { msg: string } | undefined;
 };
-const local = storage.localStorage<IMyObject>({ count: 0, message: '', foo: undefined });
+const local = localStorage<IMyObject>({
+  count: { default: 0, key: 'FOO/count' },
+  message: 'KEY/message',
+  foo: 'KEY/foo',
+});
 
 type P = t.ITestCommandProps;
 
 /**
  * The root of the CLI application.
  */
-export const tmp = Command.create<P>('tmp', () => {
-  console.log('local.foo', local.foo);
+export const tmp = Command.create<P>('tmp', e => {
+  // console.log('local.foo', local.foo);
+  // local.foo = { msg: 'hello' };
 
-  local.foo = { msg: 'hello' };
-
-  console.log('local.foo', local.foo);
+  // console.log('local.foo', local.foo);
+  local.count = e.param<number>(0, 0);
 
   const count = local.count;
   console.log('count', count);
