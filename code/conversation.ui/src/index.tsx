6f4ebbaec479a4710bb98b8e16f4716fc1a963dd
Phil Cockfield <phil@cockfield.net>
2019-05-08 13:02:10 +1200

    folder rename

--- code/conversation.ui/src/index.tsx
+++ code/conversation.ui/src/index.tsx
@@ -1,6 +1,6 @@
 export * from './types';
 export * from './store';
-export * from './data';
+export * from './data.graphql';
 export * from './components/Conversation';
 export * from './components/ThreadComment';
 export * from './components/ThreadCommentHeader';
