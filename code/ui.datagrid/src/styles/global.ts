cd2d7c837c3c1fd65772168af384be554b18ac48
Phil Cockfield <phil@cockfield.net>
2019-05-18 13:05:44 +1200

    refs

--- code/ui.datagrid/src/styles/global.ts
+++ code/ui.datagrid/src/styles/global.ts
@@ -6,4 +6,4 @@ import { constants, css } from '../common';
 /**
  * Ensure required CSS style sheets are in the <head>.
  */
-css.head.importStylesheet(constants.ROBOTO.GOOGLE_FONTS.URL);
+css.head.importStylesheet(constants.ROBOTO.GOOGLE.URL);
