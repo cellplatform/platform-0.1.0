ced073944638ef963f28c2348bcc401364a8605b
Phil Cockfield <phil@cockfield.net>
2019-05-19 09:23:40 +1200

    Bump

--- code/graphql/src/common/fetch.ts
+++ code/graphql/src/common/fetch.ts
@@ -1,16 +1,15 @@
 import * as t from '../types';
-const isomorphic = require('isomorphic-fetch');
 import { http } from './libs';
 
+type GetHeaders = (headers?: t.IHttpHeaders) => Promise<t.IHttpHeaders>;
+
 /**
- * Wrapper around fetch.
+ * Wrapper around fetch that injects custom headers
+ * for each request.
  */
-export function fetcher(args: { headers?: (headers?: t.IHttpHeaders) => Promise<t.IHttpHeaders> }) {
+export function fetcher(args: { headers?: GetHeaders }) {
   return async (url: string, options: { headers: t.IHttpHeaders }) => {
     options.headers = args.headers ? await args.headers(options.headers) : options.headers;
-
     return http.fetch(url, options as any);
-
-    // return isomorphic(url, options);
   };
 }
