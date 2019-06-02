10aedf491c16b61fbab6cfc003b34cf28734a0d7
Phil Cockfield <phil@cockfield.net>
2019-06-02 16:53:57 +1200

    Stub in `electron.loader`

---
+++ code/electron.loader/test/common/libs.ts
@@ -0,0 +1,4 @@
+export { Button, ObjectView, CommandShell, CommandState, Command, Hr } from '@uiharness/ui';
+export { log } from '@platform/log/lib/client';
+export * from '../../src/common';
+export * from '../../src';
