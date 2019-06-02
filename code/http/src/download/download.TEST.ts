17a1e1e35315f7b59956460749021de959cdac7c
Phil Cockfield <phil@cockfield.net>
2019-06-02 17:56:32 +1200

    Download zip file (server)

---
+++ code/http/src/download/download.TEST.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { download } from '.';
+import { fs } from '@platform/fs';
+
+const TMP = fs.resolve('./tmp');
+
+describe.skip('download (INTEGRATION)', function() {
+  this.timeout(30000);
+  afterEach(async () => fs.remove(TMP));
+
+  it('downloads zip file', async () => {
+    const url = 'https://uiharness.sfo2.digitaloceanspaces.com/%40platform/http/images.zip';
+    const path = fs.join(TMP, 'images.zip');
+    await download(url).save(path);
+
+    const size = await fs.size.file(path);
+    expect(size.bytes).to.greaterThan(665000);
+  });
+});
