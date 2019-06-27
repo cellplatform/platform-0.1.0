549ada5ee129796c58ca43d44e72eae9b42156d8
Phil Cockfield <phil@cockfield.net>
2019-06-27 18:33:13 +1200

    bump

--- code/cli.ui/src/types.ts
+++ code/cli.ui/src/types.ts
@@ -1,7 +1,6 @@
 import { ICommand } from '@platform/cli.spec/lib/types';
 
 export * from '@platform/cli.spec/lib/types';
-export * from '@platform/ui.tree/lib/types';
 
 export * from './components/CommandPrompt/types';
 export * from './components/CommandTree/types';
