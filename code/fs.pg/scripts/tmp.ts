1682512fe55830b06552a4022d574c10377d2400
Phil Cockfield <phil@cockfield.net>
2019-07-11 14:11:17 +1200

    PG regex example

--- code/fs.pg/scripts/tmp.ts
+++ code/fs.pg/scripts/tmp.ts
@@ -2,33 +2,52 @@ import * as pg from 'pg';
 
 (async () => {
   const client = new pg.Client({
-    user: 'phil',
+    user: 'dev',
     host: 'localhost',
     database: 'fs',
   });
 
   await client.connect();
 
-  // const res = await client.query(`CREATE TABLE IF NOT EXISTS tbl_Test (Rno INT)`);
+  await client.query(`    DROP TABLE IF EXISTS cards  `);
+
+  // return;
+
   await client.query(`
-   CREATE TABLE IF NOT EXISTS cards (
-     id integer NOT NULL,
-     board_id integer NOT NULL,
-     data jsonb
-   )
+    CREATE TABLE IF NOT EXISTS "public"."cards" (
+      "id" serial,
+      "board" integer NOT NULL,
+      "data" jsonb,
+      "path" text,
+      PRIMARY KEY ("id")
+    );
+ `);
+
+  await client.query(`
+  INSERT INTO cards VALUES (1, 1, '{"name": "Paint house", "tags": ["Improvements", "Office"], "finished": true}', '/foo/1');
+  INSERT INTO cards VALUES (2, 1, '{"name": "Wash dishes", "tags": ["Clean", "Kitchen"], "finished": false}', '/foo/2');
+  INSERT INTO cards VALUES (3, 1, '{"name": "Cook lunch", "tags": ["Cook", "Kitchen", "Tacos"], "ingredients": ["Tortillas", "Guacamole"], "finished": false}', 'foo/3');
+  INSERT INTO cards VALUES (4, 1, '{"name": "Vacuum", "tags": ["Clean", "Bedroom", "Office"], "finished": false}', 'bar/foo/123');
+  INSERT INTO cards VALUES (5, 1, '{"name": "Hang paintings", "tags": ["Improvements", "Office"], "finished": false}', 'bar/foo');
   `);
 
-  // await client.query(`
-  // INSERT INTO cards VALUES (1, 1, '{"name": "Paint house", "tags": ["Improvements", "Office"], "finished": true}');
-  // INSERT INTO cards VALUES (2, 1, '{"name": "Wash dishes", "tags": ["Clean", "Kitchen"], "finished": false}');
-  // INSERT INTO cards VALUES (3, 1, '{"name": "Cook lunch", "tags": ["Cook", "Kitchen", "Tacos"], "ingredients": ["Tortillas", "Guacamole"], "finished": false}');
-  // INSERT INTO cards VALUES (4, 1, '{"name": "Vacuum", "tags": ["Clean", "Bedroom", "Office"], "finished": false}');
-  // INSERT INTO cards VALUES (5, 1, '{"name": "Hang paintings", "tags": ["Improvements", "Office"], "finished": false}');
-  // `);
-
-  const res = await client.query(`SELECT * FROM cards WHERE data->>'finished' = 'true';`);
+  const res = await client.query(`
+    SELECT * from cards
+    WHERE path ~ '^/foo'
+  `);
 
   console.log('res', res);
+
+  res.rows.forEach(row => {
+    console.log('row', row);
+  });
+
+  console.log('-------------------------------------------');
+  console.log('count', res.rowCount);
+
+  // const res = await client.query(`SELECT * FROM cards WHERE data->>'finished' = 'true';`);
+
+  // console.log('res', res);
 
   await client.end();
 })();
