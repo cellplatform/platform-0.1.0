188622d6f34760521ede5cf12d982e891f87116c
Phil Cockfield <phil@cockfield.net>
2019-07-17 13:58:14 +1200

    delete row/column

--- code/util.value.cell/src/table/table.shift.ts
+++ code/util.value.cell/src/table/table.shift.ts
@@ -1,13 +1,18 @@
 import { cell } from '../cell';
 import { t, defaultValue } from '../common';
 
-type Dimension = 'row' | 'column';
+type Axis = 'row' | 'column';
 type TableItem<V = any> = { key: string; value: V; column: number; row: number };
 type IInsertArgs = {
   table: t.IGridTable;
   index: number;
   total?: number;
-  emptyValue?: any; // The empty value that inserts are replaced with.
+  emptyValue?: any; // The empty value that inserted cells are replaced with.
+};
+type IRemoveArgs = {
+  table: t.IGridTable;
+  index: number;
+  total?: number;
 };
 
 /**
@@ -18,36 +23,57 @@ export const insert = {
    * Insert a column(s) into a table
    */
   column(args: IInsertArgs) {
-    return shiftInsert({ ...args, type: 'column' });
+    return shiftInsert({ ...args, axis: 'column' });
   },
 
   /**
    * Insert a row(s) into a table
    */
   row(args: IInsertArgs) {
-    return shiftInsert({ ...args, type: 'row' });
+    return shiftInsert({ ...args, axis: 'row' });
   },
 };
-
-function shiftInsert(args: IInsertArgs & { type: Dimension }) {
-  const { type, table, index, emptyValue } = args;
+function shiftInsert(args: IInsertArgs & { axis: Axis }) {
+  const { axis, table, index, emptyValue } = args;
   const by = Math.max(0, defaultValue(args.total, 1));
-  return by < 1 ? table : shift({ by, type, table, index, emptyValue });
+  return by < 1 ? table : shift({ index, by, axis, table, emptyValue });
+}
+
+/**
+ * Removes column/row(s) from a table.
+ */
+export const remove = {
+  column(args: IRemoveArgs) {
+    return shiftRemove({ ...args, axis: 'column' });
+  },
+  row(args: IRemoveArgs) {
+    return shiftRemove({ ...args, axis: 'row' });
+  },
+};
+function shiftRemove(args: IInsertArgs & { axis: Axis }) {
+  const { axis, table, emptyValue } = args;
+  const total = Math.max(0, defaultValue(args.total, 1));
+  const index = args.index + total;
+  const by = 0 - total;
+  return total < 1 ? table : shift({ index, by, axis, table, emptyValue });
 }
 
 /**
  * Shifts the given row/column in a table.
  */
 export function shift(args: {
-  type: Dimension;
+  axis: Axis;
   table: t.IGridTable;
   index: number;
-  by?: number;
+  by: number;
   emptyValue?: any; // The empty value that inserts are replaced with.
 }): t.IGridTable {
   const result: t.IGridTable = {};
-  const { type, table } = args;
-  const by = defaultValue(args.by, 1);
+  const { axis, table, by } = args;
+
+  // console.log('\nindex', args.index);
+  // console.log('by', by);
+  // console.log();
 
   if (args.index < 0) {
     throw new Error(`Index must be >= 0`);
@@ -64,30 +90,33 @@ export function shift(args: {
   });
 
   // Extract the set of cells before the insertion-point, and after the insertion-point.
-  const set = {
-    before: items.filter(item => item[type] < args.index),
-    after: items.filter(item => item[type] >= args.index),
+  const SET = {
+    before: items.filter(item => item[axis] < args.index),
+    after: items.filter(item => item[axis] >= args.index),
   };
 
+  // console.log('before', SET.before.map(item => item.key));
+  // console.log('after', SET.after.map(item => item.key));
+
   // Write all shifted values to the table as "empty".
-  set.after.forEach(item => {
-    const index = item[type];
-    const column = type === 'column' ? index : item.column;
-    const row = type === 'row' ? index : item.row;
+  SET.after.forEach(item => {
+    const index = item[axis];
+    const column = axis === 'column' ? index : item.column;
+    const row = axis === 'row' ? index : item.row;
     const key = cell.toKey(column, row);
     result[key] = args.emptyValue;
   });
 
   // Overwrite all shifted values with the new key/value after the shift.
-  set.after.forEach(item => {
-    const index = item[type] + by;
-    const column = type === 'column' ? index : item.column;
-    const row = type === 'row' ? index : item.row;
+  SET.after.forEach(item => {
+    const index = item[axis] + by;
+    const column = axis === 'column' ? index : item.column;
+    const row = axis === 'row' ? index : item.row;
     const key = cell.toKey(column, row);
     result[key] = item.value;
   });
 
   // Finish up.
-  const before = set.before.reduce((acc, next) => ({ ...acc, [next.key]: next.value }), {});
+  const before = SET.before.reduce((acc, next) => ({ ...acc, [next.key]: next.value }), {});
   return { ...before, ...result };
 }
