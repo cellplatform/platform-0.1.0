1994ab1b3e9fe971edbb6d3b7b50e797b0bcc7ab
Phil Cockfield <phil@cockfield.net>
2019-07-25 10:01:56 +1200

    clean up

---
+++ code/fs.nedb/scripts/tmp.ts
@@ -0,0 +1,16 @@
+import { interval, Observable, Subject, BehaviorSubject } from 'rxjs';
+import {
+  takeUntil,
+  take,
+  takeWhile,
+  map,
+  filter,
+  share,
+  delay,
+  distinctUntilChanged,
+  debounceTime,
+} from 'rxjs/operators';
+
+interval(1000).subscribe(e => {
+  console.log('e', e);
+});
