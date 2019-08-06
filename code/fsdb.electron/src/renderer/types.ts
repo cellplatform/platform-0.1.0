9f760a7d2ac9b2d864c4b068fba78ed716ae5ef3
Phil Cockfield <phil@cockfield.net>
2019-08-06 12:01:00 +1200

    fix up types

--- code/fsdb.electron/src/renderer/types.ts
+++ code/fsdb.electron/src/renderer/types.ts
@@ -1,4 +1,4 @@
 export * from '@platform/electron/lib/types';
-export * from '@platform/fsdb.file/lib/types';
+export * from '@platform/fsdb.types/lib/types';
 export * from '@platform/types';
 export * from '../types';
