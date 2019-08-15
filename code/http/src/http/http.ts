4bb5197c8f7d4786cd56bc0a9f9f1be0d5ca5d92
Phil Cockfield <phil@cockfield.net>
2019-08-16 08:53:34 +1200

    refactor http

--- code/http/src/http/http.ts
+++ code/http/src/http/http.ts
@@ -7,7 +7,13 @@ import * as isomorphic from 'isomorphic-fetch';
 export const fetch = isomorphic;
 
 export function create(options: t.IFetchOptions = {}) {
-  const baseOptions = options;
+  const mergeOptions = (methodOptions: t.IFetchOptions) => {
+    const res = { ...options, ...methodOptions };
+    const { mode = 'cors' } = res;
+    const headers = toRawHeaders(res.headers);
+    return { mode, headers };
+  };
+
   const http = {
     create,
     fetch,
@@ -16,42 +22,48 @@ export function create(options: t.IFetchOptions = {}) {
      * `GET` request.
      */
     async get(url: string, options: t.IFetchOptions = {}): Promise<t.IHttpResponse> {
-      const { mode = 'cors' } = { ...baseOptions, ...options };
-      const res = await isomorphic(url, {
-        method: 'GET',
-        headers: toRawHeaders(options.headers),
-        mode,
-      });
-
-      const { ok, status, statusText } = res;
-      const text = await res.text();
-      let json: any;
-
-      const result: t.IHttpResponse = {
-        ok,
-        status,
-        statusText,
-        get headers() {
-          return fromRawHeaders(res.headers);
-        },
-        get body() {
-          return text || '';
-        },
-        json<T>() {
-          if (!json) {
-            try {
-              json = JSON.parse(result.body) as T;
-            } catch (error) {
-              throw new Error(`Failed while parsing JSON for '${url}'. ${error.message}`);
-            }
-          }
-          return json;
-        },
-      };
-
-      return result;
+      const { mode, headers } = mergeOptions(options);
+      const res = await isomorphic(url, { method: 'GET', headers, mode });
+      return toResponse(url, res);
     },
   };
 
+  /**
+   * [API]
+   */
   return http;
+}
+
+/**
+ * [Helpers]
+ */
+
+async function toResponse(url: string, res: Response) {
+  const { ok, status, statusText } = res;
+  const text = await res.text();
+  let json: any;
+
+  const result: t.IHttpResponse = {
+    ok,
+    status,
+    statusText,
+    get headers() {
+      return fromRawHeaders(res.headers);
+    },
+    get body() {
+      return text || '';
+    },
+    json<T>() {
+      if (!json) {
+        try {
+          json = JSON.parse(result.body) as T;
+        } catch (error) {
+          throw new Error(`Failed while parsing JSON for '${url}'. ${error.message}`);
+        }
+      }
+      return json;
+    },
+  };
+
+  return result;
 }
