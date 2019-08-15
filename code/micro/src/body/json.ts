03ba06b40928fc3890e24bbaa4ebd7dad1a82e7e
Phil Cockfield <phil@cockfield.net>
2019-08-16 08:53:27 +1200

    refactor JSON body parser

--- code/micro/src/body/json.ts
+++ code/micro/src/body/json.ts
@@ -2,21 +2,21 @@ import { t } from '../common';
 import { json as parse } from 'micro';
 
 /**
- * Parse JSON body.
+ * Parse body as JSON.
  *
- *   - limit      (example "1mb", 1024 as bytes)
- *   - encoding   (example "utf-8")
+ *   limit      (example "1mb", 1024 as bytes)
+ *   encoding   (example "utf-8", default)
  *
  */
 export async function json<T>(
   req: t.IncomingMessage,
-  options: { default?: T; limit?: string | number; encoding?: string } = {},
+  defaultValue?: T,
+  options: { limit?: string | number; encoding?: string } = {},
 ): Promise<T> {
   try {
-    const { limit, encoding } = options;
-    const body = await parse(req, { limit, encoding });
-    return (body === undefined ? options.default : body) as T;
+    const body = await parse(req, options);
+    return (body === undefined ? defaultValue : body) as T;
   } catch (error) {
-    return options.default as T;
+    return defaultValue as T;
   }
 }
