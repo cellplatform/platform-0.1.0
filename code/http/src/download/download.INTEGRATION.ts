3e2b0484326354e5920108ef017540b53b0f752b
Phil Cockfield <phil@cockfield.net>
2019-08-18 16:27:27 +1200

    refactor http integration tests

--- code/http/src/download/download.INTEGRATION.ts
+++ code/http/src/download/download.INTEGRATION.ts
@@ -4,14 +4,19 @@ import { fs } from '@platform/fs';
 
 const TMP = fs.resolve('./tmp');
 
-describe.skip('download (INTEGRATION)', function() {
+describe('download (INTEGRATION)', function() {
   this.timeout(30000);
   afterEach(async () => fs.remove(TMP));
 
   it('downloads zip file', async () => {
-    const url = 'https://platform.sfo2.digitaloceanspaces.com/http/images.zip';
+    const url = 'https://platform.sfo2.digitaloceanspaces.com/modules/http/images.zip';
     const path = fs.join(TMP, 'images.zip');
-    await download(url).save(path);
+
+    const file = download(url);
+
+    const res = await file.save(path);
+    expect(res.ok).to.eql(true);
+    expect(res.status).to.eql(200);
 
     const size = await fs.size.file(path);
     expect(size.bytes).to.greaterThan(665000);
