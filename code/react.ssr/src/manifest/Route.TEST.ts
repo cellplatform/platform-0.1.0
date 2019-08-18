98116f84259b92aedc6aa2acea83c2814a2c6e82
Phil Cockfield <phil@cockfield.net>
2019-08-19 08:32:58 +1200

    now test tweaks (WIP)

--- code/react.ssr/src/manifest/Route.TEST.ts
+++ code/react.ssr/src/manifest/Route.TEST.ts
@@ -1,8 +1,5 @@
 import { expect, fs, YAML_MANIFEST, t, testManifest } from '../test';
 
 describe('Route', () => {
-
   // it('foo', async () => {});
-
-
 });
--- code/react.ssr/test/now/api/index.ts
+++ code/react.ssr/test/now/api/index.ts
@@ -1,16 +1,22 @@
+import { server } from '@platform/react.ssr';
+
+// import { fs } from '@platform/fs';
+// const r = fs.resolve('./foo');
+// console.log('r', r);
+
+// const manifest = 'https://platform.sfo2.digitaloceanspaces.com/modules/react.ssr/manifest.yml';
+// const app = server.init({ manifest });
+
+// console.log('server.init', server.init);
+
+/**
+ * temp
+ */
+
 import { micro } from '@platform/micro';
-
-// import { server } from '@platform/react.ssr';
-
-// const manifestUrl = 'https://platform.sfo2.digitaloceanspaces.com/modules/react.ssr/manifest.yml';
-// const app = server.init({ manifestUrl });
-
-// export default app.server;
-
 const app = micro.init();
-
 app.router.get('*', async req => {
+  // console.log('-------------------------------------------');
   return { data: { msg: 123 } };
 });
-
 export default app.server;
