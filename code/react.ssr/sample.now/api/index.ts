30a96e8181607cfd3c0ce431c3d799542b6c99d3
Phil Cockfield <phil@cockfield.net>
2019-08-19 17:46:47 +1200

    remove pkg ref

--- code/react.ssr/sample.now/api/index.ts
+++ code/react.ssr/sample.now/api/index.ts
@@ -1,32 +1,16 @@
 import { server } from '@platform/react.ssr';
 
-import { fs } from '@platform/fs';
-// const r = fs.resolve('./foo');
-// console.log('r', r);
-
 const manifest = 'https://platform.sfo2.digitaloceanspaces.com/modules/react.ssr/manifest.yml';
-// const app = server.init({ manifest });
-
-console.log('server', server);
-
-// export default app.server;
-// console.log('server.init', server.init);
-
-/**
- * Glob
- */
-
-// import glob from 'glob';
-
-// console.log('glob', glob);
+const app = server.init({ manifest });
+export default app.server;
 
 /**
  * // TEMP 🐷
  */
 
-import { micro } from '@platform/micro';
-const mini = micro.init();
-mini.router.get('*', async req => {
-  return { data: { msg: 123 } };
-});
-export default mini.server;
+// import { micro } from '@platform/micro';
+// const mini = micro.init();
+// mini.router.get('*', async req => {
+//   return { data: { msg: 123 } };
+// });
+// export default mini.server;
