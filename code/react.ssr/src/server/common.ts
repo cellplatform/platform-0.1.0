7a66c530e3d41e8786bf4548edc8162ffbaf8a7d
Phil Cockfield <phil@cockfield.net>
2019-08-30 22:57:09 +1200

    group assets (meta)

--- code/react.ssr/src/server/common.ts
+++ code/react.ssr/src/server/common.ts
@@ -3,3 +3,6 @@ import * as t from './types';
 export * from '../common';
 export { Manifest, Site } from '../manifest';
 export { t };
+
+import { groupBy } from 'ramda';
+export const R = { groupBy };
