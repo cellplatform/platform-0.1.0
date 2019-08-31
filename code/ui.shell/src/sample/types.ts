1aacc9bd9376a7f122f2b5c9e61d1b1b645bfa95
Phil Cockfield <phil@cockfield.net>
2019-09-01 10:27:45 +1200

    clean

--- code/ui.shell/src/sample/types.ts
+++ code/ui.shell/src/sample/types.ts
@@ -1,13 +1,2 @@
-import * as t from '../types';
-
 export * from '../types';
 export { ITreeNode } from '@platform/ui.tree/lib/types';
-
-/**
- * Extension point of the context passed down through
- * the React hierarchy.
- */
-export type IMyContext = t.IShellContext & {
-  foo: string;
-  bar: number;
-};
