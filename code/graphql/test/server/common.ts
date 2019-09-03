6aea625b808ac6976dc3aa00d725412a5947f71a
Phil Cockfield <phil@cockfield.net>
2019-09-03 17:08:02 +1200

    graphic module example using micro

--- code/graphql/test/server/common.ts
+++ code/graphql/test/server/common.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 
 export { express };
-export { ApolloServer, gql } from 'apollo-server-express';
+export { ApolloServer, gql } from 'apollo-server-micro';
 export { log } from '@platform/log/lib/server';
 export { is } from '@platform/util.is';
--- code/graphql/test/server/start.ts
+++ code/graphql/test/server/start.ts
@@ -1,29 +1,28 @@
-import { ApolloServer, express, is, log } from './common';
+import * as http from 'http';
+import { ApolloServer, is, log } from './common';
 import { init } from './schema';
 
 const pkg = require('../../../package.json');
+
+/**
+ * [Graphql]
+ */
 const schema = init({});
-
-/**
- * [Express] web server.
- */
-export const app = express();
-
-/**
- * [GraphQL] server.
- */
-export const server = new ApolloServer({
+const graphql = new ApolloServer({
   schema,
   context(e) {
     const headers = e.req.headers;
     log.info(`🌼  ${log.yellow('Headers:')}\n\n`, headers, '\n');
   },
 });
-server.applyMiddleware({ app });
 
-const port = 5000;
-app.listen({ port }, () => {
-  const url = log.cyan(`http://localhost:${log.magenta(port)}${log.gray('/graphql')}`);
+/**
+ * Start within an HTTP server.
+ */
+const server = new http.Server(graphql.createHandler({ path: '/graphql' }));
+const PORT = 5000;
+server.listen(PORT, () => {
+  const url = log.cyan(`http://localhost:${log.magenta(PORT)}${log.gray('/graphql')}`);
   log.info.gray(`\n👋  Running on ${url}`);
   log.info();
   log.info.gray(`   - version:   ${log.white(pkg.version)}`);
