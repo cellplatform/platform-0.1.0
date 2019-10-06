2c4037216a7c3d405b6b97cc40a833bf3b0d44bd
Phil Cockfield <phil@cockfield.net>
2019-10-06 19:52:03 +1300

    View property

--- code/ui.datagrid.types/src/types.cell/cell.props.ts
+++ code/ui.datagrid.types/src/types.cell/cell.props.ts
@@ -4,6 +4,7 @@ export type ICellProps = {
   readonly value?: t.CellValue; // The calculated display value if different from the raw cell value.
   readonly style?: ICellPropsStyle;
   readonly merge?: ICellPropsMerge;
+  readonly view?: ICellPropsView;
 };
 
 export type ICellPropsStyle = {
@@ -15,4 +16,11 @@ export type ICellPropsStyle = {
 export type ICellPropsMerge = {
   readonly rowspan?: number;
   readonly colspan?: number;
+};
+
+/**
+ * The details of a view to display for the cell.
+ */
+export type ICellPropsView<V extends string = any> = {
+  type: V;
 };
