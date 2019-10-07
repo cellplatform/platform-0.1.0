1efffe43f7fbc649e482ac0529d95fb03410de81
Phil Cockfield <phil@cockfield.net>
2019-10-07 19:41:13 +1300

    bump

--- code/util.cell/src/func.sys/types.ts
+++ code/util.cell/src/func.sys/types.ts
@@ -1,4 +1,4 @@
 export type FuncName = FuncNameArithmetic | FuncNameStats;
 export type FuncNameArithmetic = 'SUM' | 'SUBTRACT' | 'MULTIPLY' | 'DIVIDE';
 export type FuncNameStats = 'AVG' | 'AVERAGE';
-export type BinaryExprOperator = '>' | '<' | '=' | '>=' | '<=' | '+' | '-' | '&' | '*' | '/'
+export type BinaryExprOperator = '>' | '<' | '=' | '>=' | '<=' | '+' | '-' | '&' | '*' | '/';
