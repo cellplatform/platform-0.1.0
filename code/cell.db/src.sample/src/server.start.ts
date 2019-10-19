de5ecac0b436c0e7366c6d9baa1c42acee8d9404
Phil Cockfield <phil@cockfield.net>
2019-10-20 12:08:55 +1300

    cell.db: stub in sample server (deployable to now)

---
+++ code/cell.db/src.sample/src/server.start.ts
@@ -0,0 +1,2 @@
+import { app } from './server';
+app.listen({ port: 8080 });
---
+++ code/cell.db/src.sample/src/server.ts
@@ -0,0 +1,21 @@
+import { micro } from '@platform/micro';
+import { log } from '@platform/log/lib/server';
+
+const PKG = require('../package.json') as { name: string; version: string };
+
+export const app = micro.init({
+  cors: true,
+  log: {
+    package: log.white(PKG.name),
+    version: PKG.version,
+  },
+});
+
+app.router.get('/foo', async req => {
+  log.info('GET', req.url);
+  return {
+    status: 200,
+    headers: { 'x-foo': 'hello' },
+    data: { message: 'hello' },
+  };
+});
