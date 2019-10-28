1cbb0e46d003a493b36a766916f8726546770e06
Phil Cockfield <phil@cockfield.net>
2019-10-29 11:46:56 +1300

    Read props from data

--- code/ui.datagrid.util/src/util/util.cell.TEST.ts
+++ code/ui.datagrid.util/src/util/util.cell.TEST.ts
@@ -5,6 +5,16 @@ import * as util from './util.cell';
 
 describe('util.cell', () => {
   describe('toCellProps', () => {
+    const A2: t.IGridCellData = {
+      value: 'Hello',
+      props: {
+        value: 456, // NB: Display value.
+        style: { bold: true },
+        merge: { colspan: 3 },
+        view: { screen: { type: 'SHOP' } },
+      },
+    };
+
     it('default props (empty {})', () => {
       const test = (input?: any) => {
         const res = util.toGridCellProps(input);
@@ -18,17 +28,16 @@ describe('util.cell', () => {
       test({});
     });
 
-    it('props', () => {
-      const A2: t.IGridCellData = {
-        value: 'Hello',
-        props: {
-          value: 456, // NB: Display value.
-          style: { bold: true },
-          merge: { colspan: 3 },
-          view: { screen: { type: 'SHOP' } },
-        },
-      };
+    it('from [cell.props]', () => {
       const props = util.toGridCellProps(A2.props);
+      expect(props.style.bold).to.eql(true);
+      expect(props.merge.colspan).to.eql(3);
+      expect(props.value).to.eql(456);
+      expect(props.view.screen && props.view.screen.type).to.eql('SHOP');
+    });
+
+    it('from [cell]', () => {
+      const props = util.toGridCellProps(A2);
       expect(props.style.bold).to.eql(true);
       expect(props.merge.colspan).to.eql(3);
       expect(props.value).to.eql(456);
--- code/ui.datagrid.util/src/util/util.cell.ts
+++ code/ui.datagrid.util/src/util/util.cell.ts
@@ -6,8 +6,13 @@ export type CellChangeField = keyof t.IGridCellProps | 'VALUE' | 'PROPS';
 /**
  * Produces a uniform cell properties object.
  */
-export function toGridCellProps(input?: t.IGridCellProps): t.IGridCellPropsAll {
-  const props = input || {};
+export function toGridCellProps(input?: t.IGridCellData | t.IGridCellProps): t.IGridCellPropsAll {
+  const props: t.IGridCellProps =
+    typeof input !== 'object' || input === null
+      ? {}
+      : typeof (input as any).props === 'object'
+      ? (input as any).props
+      : input;
   const value: t.CellValue = props.value;
   const style: t.IGridCellPropsStyle = props.style || {};
   const merge: t.IGridCellPropsMerge = props.merge || {};
