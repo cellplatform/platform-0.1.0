665b1f09424096fec071a5b5e096731c2c3a7af6
Phil Cockfield <phil@cockfield.net>
2019-11-02 14:21:46 +1300

    bump

--- code/cell.types/src/types.map.ts
+++ code/cell.types/src/types.map.ts
@@ -5,4 +5,3 @@ export type IMap<V = any> = { [key: string]: V | undefined };
 export type ICellMap<T extends t.ICellData = t.ICellData> = IMap<T>;
 export type IColumnMap<T extends t.IColumnData = t.IColumnData> = IMap<T>;
 export type IRowMap<T extends t.IRowData = t.IRowData> = IMap<T>;
-
