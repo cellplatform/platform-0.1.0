f721f02d05f90304658edf7d9a1dd849b974685a
Phil Cockfield <phil@cockfield.net>
2019-11-23 15:54:31 +1300

    add `cell.func`

--- code/cell.http/src/server/common/libs.cell.ts
+++ code/cell.http/src/server/common/libs.cell.ts
@@ -1,4 +1,5 @@
 export { Schema, Uri } from '@platform/cell.schema';
 export { value } from '@platform/cell.value';
 export { coord } from '@platform/cell.coord';
+export { func } from '@platform/cell.func';
 export { models } from '@platform/cell.db';
