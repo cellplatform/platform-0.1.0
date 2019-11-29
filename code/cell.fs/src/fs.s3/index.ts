978520de190d03a5e24b6ae81b33c4d906845bfd
Phil Cockfield <phil@cockfield.net>
2019-11-30 10:26:07 +1300

    Stub in S3 entry point.

---
+++ code/cell.fs/src/fs.s3/index.ts
@@ -0,0 +1,2 @@
+import * as s3 from './s3';
+export { s3 };
---
+++ code/cell.fs/src/fs.s3/s3.ts
@@ -0,0 +1,13 @@
+export * from '../types';
+
+/**
+ * Initializes an "S3" compatible file-system API.
+ * eg:
+ *  - AWS "S3"
+ *  - Digital Ocean "Spaces"
+ */
+export function init(args: { dir: string }) {
+  /**
+   * TODO 🐷
+   */
+}
