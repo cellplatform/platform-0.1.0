187009d2d54d56f990ede7ce08f30739a6a1dcf2
Phil Cockfield <phil@cockfield.net>
2019-11-30 11:10:43 +1300

    display host

--- code/cell.http/src/server/router/route.sys/sys.info.ts
+++ code/cell.http/src/server/router/route.sys/sys.info.ts
@@ -15,17 +15,21 @@ export function init(args: { router: t.IRouter; title?: string }) {
   const { router } = args;
   const region = fs.env.value('NOW_REGION') || 'local';
 
+  /**
+   * GET: /
+   */
   router.get(ROUTES.SYS.INFO, async req => {
     const version = {
       [MODULE.SCHEMA]: (DEPS || {})[MODULE.SCHEMA],
       [PKG.name]: PKG.version,
       [MODULE.DB]: (DEPS || {})[MODULE.DB],
     };
+
     return {
       status: 200,
       data: {
         system: args.title || 'Untitled',
-        host: 'TMP', // TODO 🐷
+        host: req.headers.host,
         region,
         version,
       },
