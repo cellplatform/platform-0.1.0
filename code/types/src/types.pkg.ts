5f4493910b8fbac055c5690bcad8774875225be5
Phil Cockfield <phil@cockfield.net>
2019-12-01 11:53:06 +1300

    Move NPM package type to @platform/types

---
+++ code/types/src/types.pkg.ts
@@ -0,0 +1,23 @@
+/**
+ * NPM [package.json] file.
+ */
+export type INpmPackageJson = {
+  name?: string;
+  description?: string;
+  version?: string;
+  main?: string;
+  scripts?: INpmPackageFields;
+  dependencies?: INpmPackageFields;
+  devDependencies?: INpmPackageFields;
+  peerDependencies?: INpmPackageFields;
+  resolutions?: INpmPackageFields;
+  license?: string;
+  private?: boolean;
+};
+export type INpmPackageFields = { [key: string]: string };
+export type NpmPackageFieldsKey =
+  | 'scripts'
+  | 'dependencies'
+  | 'devDependencies'
+  | 'peerDependencies'
+  | 'resolutions';
--- code/types/src/types.ts
+++ code/types/src/types.ts
@@ -1,3 +1,4 @@
 export * from './types.dispose';
 export * from './types.http';
 export * from './types.json';
+export * from './types.pkg';
