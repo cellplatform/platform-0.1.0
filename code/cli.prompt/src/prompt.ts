3c6456ec78fd439de3c2fcb702874d78a312b59a
Phil Cockfield <phil@cockfield.net>
2019-11-30 20:42:59 +1300

    Prompt checkbox.

--- code/cli.prompt/src/prompt.ts
+++ code/cli.prompt/src/prompt.ts
@@ -1,33 +1,7 @@
 import { inquirer } from './common';
 
 export type IPromptListOption<V = string> = { name: string; value?: V };
-
-/**
- * Prompts the user for selection from a list.
- */
-export async function list<V>(args: {
-  message: string;
-  items: Array<string | IPromptListOption<V>>;
-  pageSize?: number;
-}) {
-  const { message, pageSize } = args;
-  const choices: Array<inquirer.DistinctChoice<any>> = args.items.map(item => {
-    return typeof item === 'string'
-      ? item.startsWith('---')
-        ? new inquirer.Separator()
-        : { name: item }
-      : item;
-  });
-  const question: inquirer.DistinctQuestion = {
-    type: 'list',
-    name: 'result',
-    message,
-    choices,
-    pageSize,
-  };
-  const { result } = (await inquirer.prompt(question)) as { result: V };
-  return result;
-}
+export type IPromptCheckboxOption<V = string> = { name: string; value?: V };
 
 /**
  * Prompts the user for a simple text value.
@@ -42,4 +16,43 @@ export async function text(args: { message: string; default?: string }) {
   };
   const { result } = (await inquirer.prompt(question)) as { result: string };
   return result;
+}
+
+/**
+ * Prompts the user for selection from a list.
+ */
+export async function list<V>(args: {
+  message: string;
+  items: Array<string | IPromptListOption<V>>;
+  pageSize?: number;
+  type?: 'list' | 'checkbox';
+}) {
+  const { message, pageSize, type = 'list' } = args;
+  const choices: Array<inquirer.DistinctChoice<any>> = args.items.map(item => {
+    return typeof item === 'string'
+      ? item.startsWith('---')
+        ? new inquirer.Separator()
+        : { name: item }
+      : item;
+  });
+  const question: inquirer.DistinctQuestion = {
+    type,
+    name: 'result',
+    message,
+    choices,
+    pageSize,
+  };
+  const { result } = (await inquirer.prompt(question)) as { result: V };
+  return result;
+}
+
+/**
+ * Prompts the user with a list of checkboxes (ie. multi-select).
+ */
+export async function checkbox<V>(args: {
+  message: string;
+  items: Array<string | IPromptCheckboxOption<V>>;
+  pageSize?: number;
+}) {
+  return list({ ...args, type: 'checkbox' });
 }
