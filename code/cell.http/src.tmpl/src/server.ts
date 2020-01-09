5ec57f339f40bab706c994403436359b6e8c805c
Phil Cockfield <phil@cockfield.net>
2020-01-09 19:19:53 +1300

    sample: use S3

--- code/cell.http/src.tmpl/src/server.ts
+++ code/cell.http/src.tmpl/src/server.ts
@@ -1,6 +1,7 @@
 import { server, util } from './common';
 import { NeDb } from '@platform/fsdb.nedb';
-import { local } from '@platform/cell.fs';
+import { local, s3 } from '@platform/cell.fs';
+import { SECRETS } from './constants';
 
 const TMP = util.resolve('tmp');
 
@@ -13,10 +14,22 @@ const db = NeDb.create({ filename });
 /**
  * File system.
  */
-const fs = local.init({ root: `${TMP}/fs` });
+const getLocalFs = () => local.init({ root: `${TMP}/fs` });
+
+const getRemoteFs = () =>
+  s3.init({
+    root: 'platform/tmp/test.http',
+    endpoint: 'sfo2.digitaloceanspaces.com',
+    accessKey: SECRETS.S3.KEY,
+    secret: SECRETS.S3.SECRET,
+  });
 
 /**
  * Initialize and start the HTTP application server.
  */
-const app = server.init({ title: 'sample', db, fs });
+const app = server.init({
+  title: 'sample',
+  db,
+  fs: getRemoteFs(), // TEMP 🐷 - revert to local FS.
+});
 app.start({ port: 8080 });
