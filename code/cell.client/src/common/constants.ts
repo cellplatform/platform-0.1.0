eae2b748daa1069da052aa6e24e38ca68eb6e5ab
Phil Cockfield <phil@cockfield.net>
2020-01-13 11:17:28 +1300

    log version (wip)

--- code/cell.client/src/common/constants.ts
+++ code/cell.client/src/common/constants.ts
@@ -1,1 +1,13 @@
 export { ERROR } from '@platform/cell.schema';
+
+type Pkg = { name: string; version: string; dependencies: { [key: string]: string } };
+
+const pkg = require('../../package.json') as Pkg;
+const toVersion = (key: string) => (pkg.dependencies || {})[key];
+
+export const VERSION = {
+  '@platform/cell.schema': toVersion('@platform/cell.schema'),
+};
+
+console.log('VERSION', VERSION);
+console.log('TODO: put schema version as "cell-os:schema" on http headers for all calls');
