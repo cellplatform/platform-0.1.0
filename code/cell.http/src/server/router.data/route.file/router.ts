2d9575aa01efe9b1d16fcc006f8f0452d329ac43
Phil Cockfield <phil@cockfield.net>
2020-01-15 09:36:58 +1300

    refactor

--- code/cell.http/src/server/router.data/route.file/router.ts
+++ code/cell.http/src/server/router.data/route.file/router.ts
@@ -1,9 +1,10 @@
-import { constants, routes, Schema, t } from '../common';
+import { routes, t } from '../common';
 import { deleteFileHandler } from './handler.delete';
 import { getFileDownloadHandler } from './handler.download';
 import { getFileInfoHandler } from './handler.info';
 import { fileUploadLocalHandler } from './handler.local';
 import { fileUploadCompleteHandler } from './handler.upload.complete';
+import { getParams } from './params';
 
 /**
  * File-system routes (fs:).
@@ -11,48 +12,14 @@ import { fileUploadCompleteHandler } from './handler.upload.complete';
 export function init(args: { db: t.IDb; fs: t.IFileSystem; router: t.IRouter }) {
   const { db, fs, router } = args;
 
-  const getParams = (req: t.Request) => {
-    const params = req.params as t.IUrlParamsFile;
-    const data = {
-      ns: (params.ns || '').toString(),
-      file: (params.file || '').toString(),
-      uri: '',
-    };
-
-    const error: t.IError = {
-      type: constants.ERROR.HTTP.MALFORMED_URI,
-      message: '',
-    };
-
-    const toMessage = (msg: string) => `Malformed "file:" URI, ${msg} ("${req.url}").`;
-
-    if (!data.ns) {
-      error.message = toMessage('does not contain a namespace-identifier');
-      return { ...data, status: 400, error };
-    }
-
-    if (!data.file) {
-      error.message = toMessage('does not contain a file-identifier');
-      return { ...data, status: 400, error };
-    }
-
-    try {
-      data.uri = Schema.uri.create.file(data.ns, data.file);
-    } catch (err) {
-      error.message = toMessage(err.message);
-      return { ...data, status: 400, error };
-    }
-
-    return { ...data, status: 200, error: undefined };
-  };
-
   /**
    * GET (file info/meta).
    */
   router.get(routes.FILE.INFO, async req => {
     const host = req.host;
     const query = req.query as t.IUrlQueryFileInfo;
-    const { status, ns, error, uri } = getParams(req);
+    const params = req.params as t.IUrlParamsFile;
+    const { status, ns, error, uri } = getParams(params);
     return !ns || error
       ? { status, data: { error } }
       : getFileInfoHandler({ uri, db, query, host });
@@ -64,7 +31,8 @@ export function init(args: { db: t.IDb; fs: t.IFileSystem; router: t.IRouter }) 
   router.get(routes.FILE.BASE, async req => {
     const host = req.host;
     const query = req.query as t.IUrlQueryFileDownload;
-    const { status, ns, error, uri } = getParams(req);
+    const params = req.params as t.IUrlParamsFile;
+    const { status, ns, error, uri } = getParams(params);
     return !ns || error
       ? { status, data: { error } }
       : getFileDownloadHandler({ db, fs, uri, host, query });
@@ -87,7 +55,8 @@ export function init(args: { db: t.IDb; fs: t.IFileSystem; router: t.IRouter }) 
     const host = req.host;
     const query = req.query as t.IUrlQueryFileVerified;
     const body = await req.body.json<t.IReqPostFileVerifiedBody>({ default: { overwrite: false } });
-    const { status, ns, error, uri } = getParams(req);
+    const params = req.params as t.IUrlParamsFile;
+    const { status, ns, error, uri } = getParams(params);
     const { overwrite } = body;
     return !ns || error
       ? { status, data: { error } }
@@ -110,7 +79,8 @@ export function init(args: { db: t.IDb; fs: t.IFileSystem; router: t.IRouter }) 
   router.delete(routes.FILE.BASE, async req => {
     const host = req.host;
     const query = req.query as t.IUrlQueryFileDelete;
-    const { status, ns, error, uri } = getParams(req);
+    const params = req.params as t.IUrlParamsFile;
+    const { status, ns, error, uri } = getParams(params);
     return !ns || error ? { status, data: { error } } : deleteFileHandler({ fs, uri, db, host });
   });
 }
