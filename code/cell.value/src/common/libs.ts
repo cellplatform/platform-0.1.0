cadd1d35b9ea743f4e0f5f7f30c391a2148aeeb5
Phil Cockfield <phil@cockfield.net>
2020-01-16 15:09:39 +1300

    Remove hash ref from cell.value (moved to Schema)

--- code/cell.value/src/common/libs.ts
+++ code/cell.value/src/common/libs.ts
@@ -3,5 +3,4 @@ export const R = { equals, uniq, flatten };
 
 export { value, defaultValue } from '@platform/util.value';
 export { diff } from '@platform/util.diff';
-
-export { Uri } from '@platform/cell.schema';
+export { Schema, Uri } from '@platform/cell.schema';
