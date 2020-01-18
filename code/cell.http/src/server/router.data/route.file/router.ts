52e2d22f15c63ec8bd635fde4260f676bc9041ca
Phil Cockfield <phil@cockfield.net>
2020-01-18 14:13:21 +1300

    remove obsolete body

--- code/cell.http/src/server/router.data/route.file/router.ts
+++ code/cell.http/src/server/router.data/route.file/router.ts
@@ -43,9 +43,7 @@ export function init(args: { db: t.IDb; fs: t.IFileSystem; router: t.IRouter }) 
   router.post(routes.FILE.UPLOADED, async req => {
     const host = req.host;
     const query = req.query as t.IUrlQueryFileUploadComplete;
-    const body = await req.body.json<t.IReqPostFileUploadCompleteBody>({
-      default: { overwrite: false },
-    });
+    const body = await req.body.json<t.IReqPostFileUploadCompleteBody>();
     const params = req.params as t.IUrlParamsFile;
     const { status, ns, error, fileUri } = getParams(params);
     const sendChanges = query.changes;
