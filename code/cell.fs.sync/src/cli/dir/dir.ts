27ceac6ff8a2547b63295cec1b26eb69d08ea5ba
Phil Cockfield <phil@cockfield.net>
2020-01-23 08:47:28 +1300

    refactor output

--- code/cell.fs.sync/src/cli/dir/dir.ts
+++ code/cell.fs.sync/src/cli/dir/dir.ts
@@ -1,4 +1,4 @@
-import { util, open, log, promptConfig } from '../common';
+import { log, promptConfig, util } from '../common';
 
 const gray = log.info.gray;
 
@@ -17,37 +17,53 @@ export async function dir(args: {
     return;
   }
 
-  // Print the target URL.
+  // Print host and target systems.
   const uri = config.target.uri;
   const key = uri.parts.key;
   const keyTitle = util.log.cellKeyBg(key);
 
+  const hostParts = config.data.host.replace(/\/*$/, '').split('.');
+  const host = hostParts
+    .map((part, i) => ({ part, isLast: i === hostParts.length - 1 }))
+    .map(({ part, isLast }) => (isLast ? part : log.blue(part)))
+    .join('.');
+
   log.info(`${keyTitle}`);
   log.info();
 
-  gray(`host:     ${config.data.host.replace(/\/*$/, '')}`);
+  gray(`host:     ${host}`);
   gray(`target:   ${util.log.cellUri(uri, 'blue')}`);
   log.info();
 
-  let printFinalBlankLine = false;
+  let count = 0;
+
+  const cmd = (name: string, alias: string) => {
+    return `${log.cyan(name)} (${log.cyan(alias)})`;
+  };
 
   // Open the local folder.
   if (args.local) {
     util.open(config).local();
   } else {
-    gray(`• Use ${log.cyan('--local (-l)')} to open folder locally`);
-    printFinalBlankLine = true;
+    gray(`• Use ${cmd('--local', 'l')} to open folder locally`);
+    count++;
   }
 
   // Open the remote target cell (browser).
   if (args.remote) {
     util.open(config).remote();
   } else {
-    gray(`• Use ${log.cyan('--remote (-r)')} to open remote target in browser`);
-    printFinalBlankLine = true;
+    gray(`• Use ${cmd('--remote', 'r')} to open remote target in browser`);
+    count++;
   }
 
-  if (printFinalBlankLine) {
-    log.info();
+  // Watch command.
+  if (count > 0) {
+    gray(`• or watch and sync with ${log.cyan('cell w')}`);
+  } else {
+    gray(`• to watch and sync run ${log.cyan('cell w')}`);
   }
+
+  // Finish up.
+  log.info();
 }
