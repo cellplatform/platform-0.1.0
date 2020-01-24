2619a9f4226a7b067422c2634a6b5f11acb15d33
Phil Cockfield <phil@cockfield.net>
2020-01-25 11:03:25 +1300

    export chalk

--- code/log/src/server/common.ts
+++ code/log/src/server/common.ts
@@ -1,8 +1,4 @@
 export * from '../common';
 
-/**
- * HACK: Typing issues with the chalk library.
- *       version `chalk@2.4.2`.
- */
-import * as ChalkTypes from 'chalk';
-export const chalk = require('chalk') as ChalkTypes.Chalk;
+import * as chalk from 'chalk';
+export { chalk };
