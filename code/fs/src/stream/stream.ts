837c0c49839f0815121204a4419d9cba46fe9aee
Phil Cockfield <phil@cockfield.net>
2020-01-24 18:15:08 +1300

    fs: save stream (option as string also)

--- code/fs/src/stream/stream.ts
+++ code/fs/src/stream/stream.ts
@@ -1,7 +1,7 @@
 import * as stream from 'stream';
 import * as util from 'util';
 
-import { ensureDir, createWriteStream } from 'fs-extra';
+import { ensureDir, createWriteStream, writeFile } from 'fs-extra';
 import { dirname } from 'path';
 
 const pipeline = util.promisify(stream.pipeline);
@@ -9,11 +9,15 @@ const pipeline = util.promisify(stream.pipeline);
 /**
  * Saves a readable stream to disk.
  */
-export async function save(path: string, data: ReadableStream) {
+export async function save(path: string, data: ReadableStream | string) {
   try {
     await ensureDir(dirname(path));
-    const output = createWriteStream(path);
-    await pipeline(data as any, output);
+    if (typeof data === 'string') {
+      await writeFile(path, data);
+    } else {
+      const output = createWriteStream(path);
+      await pipeline(data as any, output);
+    }
   } catch (err) {
     throw new Error(`Failed to save stream to '${path}'. ${err.message}`);
   }
