ebd5b1a45ed1eedbb3526e058a6c39643807fcfe
Phil Cockfield <phil@cockfield.net>
2020-01-26 08:25:00 +1300

    css - demo

---
+++ code/css/sh/bundle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 
+cd demo
+
+echo "parcel v1 (css) \n"
+cd parcel-v1 && yarn && yarn bundle && cd ..
+echo 
+
---
+++ code/css/sh/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd demo
+cd parcel-v1 && yarn clean && cd ..
+
