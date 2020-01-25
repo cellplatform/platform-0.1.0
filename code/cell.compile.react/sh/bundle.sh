1a1b12e3f2034a01b620f1a8c1035ec43413ea9e
Phil Cockfield <phil@cockfield.net>
2020-01-26 08:28:57 +1300

    change "browser" to "web"
    https://spectrum.chat/parcel/parcel2/parcel-build-external-modules-are-n
    ot-supported-when-building-for-browser-v2~21dc1fd3-fd08-42a9-91bf-0add4d
    4ed9ef?m=MTU3OTkwNTYwMDIyMg==

--- code/cell.compile.react/sh/bundle.sh
+++ code/cell.compile.react/sh/bundle.sh
@@ -3,17 +3,17 @@
 echo 
 cd demo
 
-echo "hello-world (parcel v1)\n"
-cd parcel-v1.hello-world && yarn && yarn bundle && cd ..
-echo 
+# echo "hello-world (parcel v1)\n"
+# cd parcel-v1.hello-world && yarn && yarn bundle && cd ..
+# echo 
 
-echo "react (parcel v1)"
-cd parcel-v1.react && yarn && yarn bundle && cd ..
-echo 
+# echo "react (parcel v1)"
+# cd parcel-v1.react && yarn && yarn bundle && cd ..
+# echo 
 
-echo "react.application (parcel v1)"
-cd parcel-v1.react.application && yarn && yarn bundle && cd ..
-echo 
+# echo "react.application (parcel v1)"
+# cd parcel-v1.react.application && yarn && yarn bundle && cd ..
+# echo 
 
 echo "hello-world (parcel v2)"
 cd parcel-v2.hello-world && yarn && yarn bundle && cd ..
