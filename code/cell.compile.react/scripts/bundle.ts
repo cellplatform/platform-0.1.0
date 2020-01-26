0d421577685054a8c6cf5ba720a4947ab0e8f913
Phil Cockfield <phil@cockfield.net>
2020-01-27 09:50:54 +1300

    Bundle script

--- code/cell.compile.react/scripts/bundle.ts
+++ code/cell.compile.react/scripts/bundle.ts
@@ -7,7 +7,8 @@ const { fs, prompt, log } = cli;
 
   const tasks = cli.tasks();
   dirs.forEach(dir => {
-    const title = fs.basename(dir);
+    const dirname = fs.basename(dir);
+    const title = log.gray(`bundle: ${log.white(dirname)}`);
     tasks.task(title, async () => {
       const res = await cli.exec.command(`yarn bundle`).run({ cwd: dir, silent: true });
       info.push(`${log.green(title)}`);
@@ -18,18 +19,25 @@ const { fs, prompt, log } = cli;
     });
   });
 
+  log.info();
   const res = await tasks.run({ concurrent: true });
 
   log.info();
   info.forEach(line => cli.log.info(line));
 })();
 
+/**
+ * Prompt user for selection of a directory.
+ */
 async function promptDirs(dir: string) {
   const paths = await fs.glob.find(fs.join(fs.resolve(dir), '*/package.json'));
   const items = paths.map(path => {
     const dir = fs.dirname(path);
     const name = fs.basename(dir);
-    return { name, value: dir };
+    return {
+      name: log.gray(`• demo/${log.white(name)}/`),
+      value: dir,
+    };
   });
 
   const res = await prompt.list({
