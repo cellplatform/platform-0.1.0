13001d768347ee9eb7993295bdbf2287c7eb5dd6
Phil Cockfield <phil@cockfield.net>
2020-01-30 11:29:12 +1300

    prompt: all option

--- code/cli.prompt/scripts/prompt.fs.checkbox.ts
+++ code/cli.prompt/scripts/prompt.fs.checkbox.ts
@@ -1,7 +1,7 @@
 import { prompt, log } from './common';
 
 (async () => {
-  const res = await prompt.fs.paths('scripts').checkbox();
+  const res = await prompt.fs.paths('scripts', { all: true }).checkbox();
 
   log.info('-------------------------------------------');
   log.info(res);
