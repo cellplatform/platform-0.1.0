ab48ead26ea993a3408fcfc1e1851ad0b7d20727
Phil Cockfield <phil@cockfield.net>
2020-01-30 08:52:12 +1300

    folder rename

--- code/cell.compile.react/sh/update.sh
+++ code/cell.compile.react/sh/update.sh
@@ -3,7 +3,7 @@ cd demo
 
 cd parcel-v1.hello-world && npm outdated && yarn && cd ..
 cd parcel-v1.react && npm outdated && yarn && cd ..
-cd parcel-v1.react.application && npm outdated && yarn && cd ..
+cd parcel-v1.react.modules && npm outdated && yarn && cd ..
 
 cd parcel-v2.hello-world && npm outdated && yarn && cd ..
 
