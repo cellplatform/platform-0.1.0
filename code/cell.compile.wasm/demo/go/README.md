98b00559963215008b151994f2dedb820cbe64f0
Phil Cockfield <phil@cockfield.net>
2020-02-01 11:32:25 +1300

    Go program compiling

---
+++ code/cell.compile.wasm/demo/go/README.md
@@ -0,0 +1,12 @@
+# Go WASM
+
+<p>&nbsp;</p>
+
+## Refs
+
+- Go WebAssembly Tutorial  
+  https://www.youtube.com/watch?v=4kBvvk2Bzis  
+  https://tutorialedge.net/golang/go-webassembly-tutorial
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
---
+++ code/cell.compile.wasm/demo/go/sh/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compile the GO program into a WASM file.
+cd src 
+GOARCH=wasm GOOS=js go build -o ../dist/lib.wasm main.go
+
+# Copy static assets.
+cd ..
+cp src/index.html dist/index.html
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" dist/wasm-exec.js
