a6d954f478c48c59e4bc344f130d8a19cbe65c90
Phil Cockfield <phil@cockfield.net>
2020-04-02 14:14:39 +1300

    Input

--- code/cell.typesystem/src/TypeSystem/util/util.uri.ts
+++ code/cell.typesystem/src/TypeSystem/util/util.uri.ts
@@ -2,11 +2,11 @@ import { t, Uri } from '../../common';
 
 type FormatOptions = { throw?: boolean };
 
-export const formatNsUri = (input: string | t.INsUri, options?: FormatOptions) => {
+export const formatNsUri = (input?: string | t.INsUri, options?: FormatOptions) => {
   return format<t.INsUri>({ ...options, input, type: 'NS', prefix: 'ns' });
 };
 
-export const formatRowUri = (input: string | t.IRowUri = '', options?: FormatOptions) => {
+export const formatRowUri = (input?: string | t.IRowUri, options?: FormatOptions) => {
   return format<t.IRowUri>({ ...options, input, type: 'ROW', prefix: 'cell' });
 };
 
@@ -15,7 +15,7 @@ export const formatRowUri = (input: string | t.IRowUri = '', options?: FormatOpt
  */
 
 const format = <U extends t.IUri>(args: {
-  input: U | string;
+  input?: U | string;
   prefix: string;
   type: t.UriType;
   throw?: boolean;
