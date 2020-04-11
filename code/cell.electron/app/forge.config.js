d8272223fe1acb0b3bda2a0e3b0c55a17a901b1d
Phil Cockfield <phil@cockfield.net>
2020-04-12 08:39:50 +1200

    Ignore - build app

--- code/cell.electron/app/forge.config.js
+++ code/cell.electron/app/forge.config.js
@@ -1,11 +1,18 @@
 /**
+ *
  * https://www.electronforge.io/configuration
  * https://electron.github.io/electron-packager/master/interfaces/electronpackager.options.html
+ *
  */
 module.exports = {
   packagerConfig: {
     name: 'Cell',
     icon: './assets/icons/app/app.icns',
+    ignore: path => ignore(path),
+    prune: true,
+
+    // MacOS:
+    appCategoryType: 'public.app-category.productivity', // See Apple Docs: https://developer.apple.com/library/archive/documentation/General/Reference/InfoPlistKeyReference/Articles/LaunchServicesKeys.html#//apple_ref/doc/uid/TP40009250-SW8
   },
   makers: [
     {
@@ -21,3 +28,37 @@ module.exports = {
   ],
   hooks: {},
 };
+
+/**
+ * [Helpers]
+ */
+
+/**
+ * Filter to exclude various files from being
+ * included in the distribution bundle.
+ */
+function ignore(path) {
+  if (path.startsWith('/node_modules/')) {
+    return false; // Allow.
+  }
+
+  const startsWith = [
+    '/src',
+    '/sh',
+    '/tmp',
+    '/yarn.lock',
+    '/tslint.json',
+    '/tsconfig.json',
+    '/forge.config.js',
+  ];
+  if (startsWith.some(match => path.startsWith(match))) {
+    return true; // Ignore.
+  }
+
+  const endsWith = ['.iconsproj', '.d.ts', '.mjs'];
+  if (endsWith.some(match => path.endsWith(match))) {
+    return true; // Ignore.
+  }
+
+  return false; // Allow.
+}
