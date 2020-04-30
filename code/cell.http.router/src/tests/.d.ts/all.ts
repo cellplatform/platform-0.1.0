0d972aa6bf0d102c35e553083e5a057ff7ec0083
Phil Cockfield <phil@cockfield.net>
2020-04-30 19:59:36 +1200

    bump

--- code/cell.http.router/src/tests/.d.ts/all.ts
+++ code/cell.http.router/src/tests/.d.ts/all.ts
@@ -9,7 +9,7 @@
  *    |
  *
  * By:
- *    @platform/cell.typesystem@0.0.29
+ *    @platform/cell.typesystem@0.0.30
  * 
  * Notes: 
  * 
--- code/cell.typesystem/src/test/.d.ts/foo.enum.ts
+++ code/cell.typesystem/src/test/.d.ts/foo.enum.ts
@@ -6,7 +6,7 @@
  *    |
  *
  * By:
- *    @platform/cell.typesystem@0.0.29
+ *    @platform/cell.typesystem@0.0.30
  * 
  * Notes: 
  * 
