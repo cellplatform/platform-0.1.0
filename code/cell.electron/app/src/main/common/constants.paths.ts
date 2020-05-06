4e8ced1c983bb584da9c3a1f6cfec392b1d3dd44
Phil Cockfield <phil@cockfield.net>
2020-05-07 09:11:08 +1200

    refactor

--- code/cell.electron/app/src/main/common/constants.paths.ts
+++ code/cell.electron/app/src/main/common/constants.paths.ts
@@ -8,6 +8,8 @@ const resolve = (path: string) =>
  * File paths.
  */
 export const paths = {
+  resolve,
+
   data(args: { prod?: boolean; dirname?: string }) {
     const { prod = false, dirname = 'CellOS' } = args;
     const dir = prod ? fs.join(app.getPath('documents'), dirname) : fs.resolve('../.data');
@@ -17,9 +19,11 @@ export const paths = {
       fs: `${dir}/local.fs`,
     };
   },
+
   assets: {
     icons: resolve('assets/icons'),
   },
+
   bundle: {
     preload: resolve('lib/preload.js'),
     sys: resolve('.bundle/ui.sys'),
