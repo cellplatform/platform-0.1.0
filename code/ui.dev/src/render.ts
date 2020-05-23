eb61bc98e806ce56060ac9699d5ab66a70798f7d
Phil Cockfield <phil@cockfield.net>
2020-05-23 13:32:56 +1200

    render

---
+++ code/ui.dev/src/render.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { reset } from '@platform/css';
+
+/**
+ * Renders an element into the DOM.
+ */
+export function render(
+  element: React.SFCElement<any> | Array<React.SFCElement<any>>,
+  container?: Element | DocumentFragment | null | string,
+  callback?: () => void,
+) {
+  reset(); // Normalise CSS.
+  container = container || 'root';
+  container = typeof container === 'string' ? document.getElementById(container) : container;
+  ReactDOM.render(element, container, callback);
+}
