b7814ae456ba0472f2d9d25aaf7eb48b26786a58
Phil Cockfield <phil@cockfield.net>
2020-05-23 12:53:03 +1200

    refactor

--- code/css/src/test.parcel/index.html.tsx
+++ code/css/src/test.parcel/index.html.tsx
@@ -2,10 +2,9 @@ import '@platform/polyfill';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import './test.global';
+import './styles';
 import { Test } from './Test';
 import { reset } from '../reset';
 
 reset();
-
 ReactDOM.render(<Test />, document.getElementById('root'));
