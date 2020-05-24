5cfcde9821342133b2e5cd5d9a87b95c3cc389b4
Phil Cockfield <phil@cockfield.net>
2020-05-24 13:24:09 +1200

    PropList

--- code/cell.ui.finder/src/components/primitives.ts
+++ code/cell.ui.finder/src/components/primitives.ts
@@ -1,7 +1,8 @@
 import { WindowTitleBar } from '@platform/cell.ui/lib/components/WindowTitleBar';
 export { WindowTitleBar };
 
-import { PropList, IPropListItem, IPropListProps } from '@platform/cell.ui/lib/components/PropList';
+import { IPropListItem } from '@platform/cell.ui/lib/types';
+import { PropList, IPropListProps } from '@platform/cell.ui/lib/components/PropList';
 export { PropList, IPropListItem, IPropListProps };
 
 import { Spinner } from '@platform/ui.spinner';
