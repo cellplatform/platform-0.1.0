2603dc9d72f699ad6209c33bb320e6393a24202d
Phil Cockfield <phil@cockfield.net>
2020-05-24 14:21:29 +1200

    Fix

--- code/cell.ui.sys/src/components/primitives.ts
+++ code/cell.ui.sys/src/components/primitives.ts
@@ -4,7 +4,8 @@ export { WindowTitleBar };
 import { Card, ICardProps } from '@platform/cell.ui/lib/components/Card';
 export { Card, ICardProps };
 
-import { PropList, IPropListItem } from '@platform/cell.ui/lib/components/PropList';
+import { IPropListItem } from '@platform/cell.ui/lib/types';
+import { PropList } from '@platform/cell.ui/lib/components/PropList';
 export { PropList, IPropListItem };
 
 import { ObjectView } from '@platform/ui.object';
