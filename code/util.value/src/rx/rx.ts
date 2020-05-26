da70c4d295d2000bca091f4e4f99b114e067fdb7
Phil Cockfield <phil@cockfield.net>
2020-05-26 16:20:46 +1200

    payload

--- code/util.value/src/rx/rx.ts
+++ code/util.value/src/rx/rx.ts
@@ -1,14 +1,2 @@
-import { Observable } from 'rxjs';
-import { buffer, debounceTime, publishReplay, refCount } from 'rxjs/operators';
-
-/**
- * Debounces a series of events returning an array of the buffered values.
- *
- * Idea sourced from:
- *    https://stackoverflow.com/a/35739707/1745661
- *    http://jsbin.com/pubowum/7/edit?js,console,output
- */
-export function debounceBuffer<T>(ob$: Observable<T>, debounce = 0) {
-  const shared$ = ob$.pipe(publishReplay(1), refCount());
-  return shared$.pipe(buffer(shared$.pipe(debounceTime(debounce))));
-}
+export * from './rx.event';
+export * from './rx.debounce';
