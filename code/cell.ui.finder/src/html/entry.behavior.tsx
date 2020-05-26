4d9988694b826de1c2113b55853ca9c2f2f3e989
Phil Cockfield <phil@cockfield.net>
2020-05-26 16:22:09 +1200

    tweak

--- code/cell.ui.finder/src/html/entry.behavior.tsx
+++ code/cell.ui.finder/src/html/entry.behavior.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-
 import { Subject } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 import { t } from '../common';
-
-export const event$ = new Subject<t.FinderEvent>();
-
 import { Viewer } from '../components/Viewer';
 import { Doc } from '../components/Viewer.Doc';
 
@@ -16,6 +12,7 @@ import { Doc } from '../components/Viewer.Doc';
  */
 
 export function behavior(args: { uri: string }) {
+  const event$ = new Subject<t.FinderEvent>();
   event$
     .pipe(
       filter((e) => e.type === 'FINDER/render/view'),
--- code/cell.ui.finder/src/html/entry.tsx
+++ code/cell.ui.finder/src/html/entry.tsx
@@ -1,25 +1,22 @@
-console.log("hello", 123)
-
 import '../config';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-// import { t } from '../common';
-// import { Root } from '../components/Finder.Root';
+import { t } from '../common';
+import { Root } from '../components/Finder.Root';
 import { behavior } from './entry.behavior';
 
-// const win = (window as unknown) as t.ITopWindow;
-// const env = win.env;
+const win = (window as unknown) as t.ITopWindow;
+const env = win.env;
 
 /**
  * Render root React element.
  */
 
-
 const uri = 'cell:cka0c7a0i00004369o5ttdbt3:A1'; // TEMP 🐷
 const event$ = behavior({ uri }).event$;
-console.log("event$", event$)
+// console.log('event$', event$);
 
-// const el = <Root env={env} uri={uri} event$={event$} />;
-// ReactDOM.render(el, document.getElementById('root'));
+const el = <Root env={env} uri={uri} event$={event$} />;
+ReactDOM.render(el, document.getElementById('root'));
