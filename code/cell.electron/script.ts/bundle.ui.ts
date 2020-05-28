5c9f9419591b60eb9019c62db9796c94f044b3bd
Phil Cockfield <phil@cockfield.net>
2020-05-28 12:02:46 +1200

    Refactor

--- code/cell.electron/script.ts/bundle.ui.ts
+++ code/cell.electron/script.ts/bundle.ui.ts
@@ -10,11 +10,13 @@ import { log } from '@platform/log/lib/server';
   let modules = ['cell.ui.sys', 'cell.ui.finder', 'cell.ui.ide'];
 
   if (!isSilent) {
-    const res = await prompt.checkbox({ message: 'modules', items: modules });
+    const res = await prompt.checkbox({ message: 'modules', items: ['all', ...modules] });
     if (res.length === 0) {
       return log.info.gray(`\nNo modules selected.`);
     }
-    modules = modules.filter((name) => res.includes(name));
+    if (!res.includes('all')) {
+      modules = modules.filter((name) => res.includes(name));
+    }
   }
 
   const target = constants.paths.bundle;
--- code/cell.ui.sys/src/html/index.html.tsx
+++ code/cell.ui.sys/src/html/index.html.tsx
@@ -1,66 +1,64 @@
-console.log('cell.ui.sys 👋');
+import '../config';
+import { Client, Uri, Schema, t } from '../common';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Root } from '../components/Root';
+import { reset } from '@platform/css';
 
-// import '../config';
-// import { Client, Uri, Schema, t } from '../common';
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import { Root } from '../components/Root';
-// import { reset } from '@platform/css';
+reset();
+const win = (window as unknown) as t.ITopWindow;
+const env = win.env;
 
-// reset();
-// const win = (window as unknown) as t.ITopWindow;
-// const env = win.env;
+(async () => {
+  console.group('🌳 module: cell.ui.sys');
+  console.log('process.argv:', process.argv.length);
+  console.log('env', env);
 
-// (async () => {
-//   console.group('🌳 module: cell.ui.sys');
-//   console.log('process.argv:', process.argv.length);
-//   console.log('env', env);
+  // console.log('uri', uri.toString());
+  const client = Client.typesystem({ http: env.host, cache: env.cache });
+  const uri = Uri.row(env.def);
+  const ns = client.http.ns(uri.ns);
 
-//   // console.log('uri', uri.toString());
-//   const client = Client.typesystem({ http: env.host, cache: env.cache });
-//   const uri = Uri.row(env.def);
-//   const ns = client.http.ns(uri.ns);
+  const tmp = await ns.read({ cells: true });
+  console.log('ns.read:', tmp.body.data);
 
-//   const tmp = await ns.read({ cells: true });
-//   console.log('ns.read:', tmp.body.data);
+  console.log('-------------------------------------------');
 
-//   console.log('-------------------------------------------');
+  const sheet = await client.sheet(uri.ns);
 
-//   const sheet = await client.sheet(uri.ns);
+  const index = Schema.coord.cell.toAxisIndex('ROW', uri.key);
+  const cursor = await sheet.data('AppWindow').load();
 
-//   const index = Schema.coord.cell.toAxisIndex('ROW', uri.key);
-//   const cursor = await sheet.data('AppWindow').load();
+  const row = cursor.row(0);
 
-//   const row = cursor.row(0);
+  console.log('row', row.toObject());
 
-//   console.log('row', row.toObject());
+  console.log('sheet', sheet);
+  console.log('sheet.types', sheet.types);
+  console.log('-------------------------------------------');
 
-//   console.log('sheet', sheet);
-//   console.log('sheet.types', sheet.types);
-//   console.log('-------------------------------------------');
+  console.log('env.cache', env.cache);
+  // console.log('env.cache.keys:', env.cache.keys);
+  // env.cache.keys.forEach(e => {
+  //   console.log('  -- ', e);
+  // });
 
-//   console.log('env.cache', env.cache);
-//   // console.log('env.cache.keys:', env.cache.keys);
-//   // env.cache.keys.forEach(e => {
-//   //   console.log('  -- ', e);
-//   // });
+  // env.cache.keys.forEach(async key => {
+  //   console.log(' > ', key, await env.cache.get(key));
+  // });
 
-//   // env.cache.keys.forEach(async key => {
-//   //   console.log(' > ', key, await env.cache.get(key));
-//   // });
+  console.groupEnd();
 
-//   console.groupEnd();
+  // async function writeRow() {
+  //   console.log('row', row.toObject());
 
-//   // async function writeRow() {
-//   //   console.log('row', row.toObject());
+  // }
 
-//   // }
+  env.event$.subscribe((e) => {
+    console.log('module  | 🌳 event', e);
+    // writeRow()
+  });
 
-//   env.event$.subscribe((e) => {
-//     console.log('module  | 🌳 event', e);
-//     // writeRow()
-//   });
-
-//   const el = <Root env={env} uri={env.def} />;
-//   ReactDOM.render(el, document.getElementById('root'));
-// })();
+  const el = <Root env={env} uri={env.def} />;
+  ReactDOM.render(el, document.getElementById('root'));
+})();
