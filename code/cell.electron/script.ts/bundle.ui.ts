54f560a238555f41004396bd171caa5e40e1ed00
Phil Cockfield <phil@cockfield.net>
2020-06-02 11:16:06 +1200

    reset data

--- code/cell.electron/script.ts/bundle.ui.ts
+++ code/cell.electron/script.ts/bundle.ui.ts
@@ -1,25 +1,36 @@
 import { constants } from '../app/src/main/common';
 import { bundleModules } from './bundle.ui.lib';
 import { prompt } from '@platform/cli.prompt';
+import { fs } from '@platform/fs';
 import { log } from '@platform/log/lib/server';
 
 (async () => {
   var argv = process.argv.slice(2);
   const isSilent = argv.includes('--silent') || argv.includes('-s');
 
+  type Reset = 'yes' | 'no';
+
   let modules = ['cell.ui.sys', 'cell.ui.finder', 'cell.ui.ide'];
+  let reset: Reset = 'no';
 
   if (!isSilent) {
-    const res = await prompt.checkbox({ message: 'modules', items: ['all', ...modules] });
-    if (res.length === 0) {
+    const selectModules = await prompt.checkbox({ message: 'modules', items: ['all', ...modules] });
+    if (selectModules.length === 0) {
       return log.info.gray(`\nNo modules selected.`);
     }
-    if (!res.includes('all')) {
-      modules = modules.filter((name) => res.includes(name));
+    if (!selectModules.includes('all')) {
+      modules = modules.filter((name) => selectModules.includes(name));
     }
+
+    reset = await prompt.radio<Reset>({ message: 'reset data', items: ['yes', 'no'] });
   }
 
   const target = constants.paths.bundle;
   const args = modules.map((name) => ({ sourceDir: `../${name}`, targetDir: target[name] }));
   await bundleModules(args);
+
+  if (reset) {
+    const path = constants.paths.data({ prod: false });
+    await fs.remove(path.dir);
+  }
 })();
