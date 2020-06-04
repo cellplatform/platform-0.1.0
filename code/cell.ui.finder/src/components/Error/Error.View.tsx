c1ed8a3056f399ff07bc1888998d90da0d6f0cb1
Phil Cockfield <phil@cockfield.net>
2020-06-04 14:12:32 +1200

    ErrorView render component list

--- code/cell.ui.finder/src/components/Error/Error.View.tsx
+++ code/cell.ui.finder/src/components/Error/Error.View.tsx
@@ -57,7 +57,7 @@ export class ErrorView extends React.Component<IErrorViewProps, IErrorViewState>
         minWidth: 350,
       }),
       title: css({
-        borderBottom: `solid 1px ${color.format(1)}`,
+        borderBottom: `solid 2px ${color.format(1)}`,
         paddingBottom: 10,
         marginBottom: 10,
       }),
@@ -116,18 +116,51 @@ export class ErrorView extends React.Component<IErrorViewProps, IErrorViewState>
 
     const styles = {
       base: css({
-        borderBottom: `solid 1px ${color.format(1)}`,
+        position: 'relative',
+        borderBottom: `solid 2px ${color.format(1)}`,
         paddingBottom: 10,
         marginBottom: 10,
+        boxSizing: 'border-box',
 
         fontFamily: 'monospace',
         lineHeight: '1.3em',
         fontSize: 12,
       }),
+      timeline: css({
+        Absolute: [16, null, 26, 18],
+        width: 1,
+        backgroundColor: color.format(0.3),
+      }),
+      label: css({ Flex: 'horizontal-center-start' }),
+      indent: css({ marginLeft: 30 }),
+      createdBy: css({
+        marginLeft: 10,
+        opacity: 0.5,
+      }),
     };
+
+    const stack = component.stack;
+    const elStack = stack.map((item, i) => {
+      const isFirst = i === 0;
+      const isLast = i === stack.length - 1;
+      const isEdge = isFirst || isLast;
+      const labelStyle = !isEdge && styles.indent;
+      const label = isEdge ? item.lineIn : item.lineIn || item.createdBy;
+      const createdBy = !(isEdge && label === item.createdBy) && item.createdBy;
+      return (
+        <div key={i}>
+          <div {...css(styles.label, labelStyle)}>
+            <div>{isEdge ? `<${label}>` : label}</div>
+            {createdBy && <div {...styles.createdBy}>created by: {createdBy}</div>}
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div {...styles.base}>
-        <div>{`<${component.name}>`}</div>
+        {stack.length > 2 && <div {...styles.timeline} />}
+        {elStack}
       </div>
     );
   }
