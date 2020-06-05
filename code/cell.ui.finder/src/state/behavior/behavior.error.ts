f4d73a7a2bd9d64fa85cb5c1e7e387415a981e16
Phil Cockfield <phil@cockfield.net>
2020-06-05 15:46:20 +1200

    refactor: immutable state changes (procedural).

--- code/cell.ui.finder/src/state/behavior/behavior.error.ts
+++ code/cell.ui.finder/src/state/behavior/behavior.error.ts
@@ -10,26 +10,10 @@ export function init(args: { store: t.IFinderStore }) {
    * REDUCE: Update error.
    */
   store.on<t.IFinderErrorEvent>('FINDER/error').subscribe((e) => {
-    const state = e.state;
-    let error = { ...(state.error || {}) };
-
-    switch (e.payload.name) {
-      case 'root':
-        error = { ...error, root: e.payload };
-        break;
-
-      case 'view':
-        error = { ...error, view: e.payload };
-        break;
-
-      case 'tree':
-        error = { ...error, tree: e.payload };
-        break;
-
-      default:
-        throw new Error(`Error boundary type '${e.payload.name}' not implemented.`);
-    }
-
-    e.change({ ...state, error });
+    e.change((state) => {
+      const error = e.payload;
+      state.error = state.error || {};
+      state.error[error.name] = error;
+    });
   });
 }
