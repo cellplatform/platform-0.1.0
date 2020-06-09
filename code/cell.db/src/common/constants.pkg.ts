b4538899fef8b09be1a2414c70244d41b01aac31
Phil Cockfield <phil@cockfield.net>
2020-06-09 15:03:49 +1200

    bump

--- code/cell.db/src/common/constants.pkg.ts
+++ code/cell.db/src/common/constants.pkg.ts
@@ -1,15 +1,15 @@
 export const PKG = {
   name: '@platform/cell.db',
-  version: '0.3.88',
+  version: '0.3.90',
   dependencies: {
-    '@platform/cell.coord': '0.8.124',
-    '@platform/cell.schema': '0.4.115',
-    '@platform/cell.types': '0.4.104',
-    '@platform/cell.value': '0.2.76',
-    '@platform/fsdb.model': '0.6.68',
+    '@platform/cell.coord': '0.8.126',
+    '@platform/cell.schema': '0.4.117',
+    '@platform/cell.types': '0.4.105',
+    '@platform/cell.value': '0.2.78',
+    '@platform/fsdb.model': '0.6.69',
     '@platform/fsdb.types': '0.6.26',
     '@platform/libs': '0.2.2',
     '@platform/types': '0.4.22',
-    '@platform/util.value': '0.7.23',
+    '@platform/util.value': '0.7.24',
   },
 };
--- code/cell.schema/src/common/constants.pkg.ts
+++ code/cell.schema/src/common/constants.pkg.ts
@@ -1,4 +1,4 @@
 export const PKG = {
   name: '@platform/cell.schema',
-  version: '0.4.115',
+  version: '0.4.117',
 };
