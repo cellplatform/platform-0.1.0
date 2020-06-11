0d1c63105f0986ede4716e7c9a99baf853024951
Phil Cockfield <phil@cockfield.net>
2020-06-12 11:26:27 +1200

    refactor - name

--- code/cell.http/src/common/constants.pkg.ts
+++ code/cell.http/src/common/constants.pkg.ts
@@ -4,6 +4,7 @@ export const PKG = {
   dependencies: {
     '@platform/cell.http.router': '0.7.98',
     '@platform/cell.schema': '0.4.120',
+    '@platform/cell.types': '^0.4.108',
     '@platform/cell.typesystem': '0.0.59',
     '@platform/cli': '0.5.39',
     '@platform/exec': '0.2.59',
---
+++ code/cell.types/src/types.Http/types.http.config.ts
@@ -0,0 +1,43 @@
+import { t } from '../common';
+
+/**
+ * Configuration
+ */
+
+export type IHttpConfigFileArgs = { path?: string; throw?: boolean };
+export type IHttpConfigFile = {
+  path: string;
+  exists: boolean;
+  data: IHttpConfigDeployment;
+  validate(): IHttpConfigValidation;
+};
+export type IHttpConfigValidation = {
+  isValid: boolean;
+  errors: t.IError[];
+};
+
+export type IHttpConfigDeployment = {
+  name: string;
+  collection: string;
+  fs: {
+    endpoint: string;
+    root: string;
+  };
+  now: {
+    deployment: string; // The "project name" of the now deployment. see CLI: `now ls`.
+    domain: string;
+    subdomain?: string; // NB: Used as DB name (uses "prod" if not specified).
+  };
+  secret: {
+    // Keys for [zeit/now] secrets. NB: the "@" prefix is not required (eg "@mongo").
+    mongo: string;
+    s3: { key: string; secret: string };
+  };
+};
+
+export type IHttpConfigNowFile = {
+  version: number;
+  name: string;
+  alias: string;
+  env: { [key: string]: string };
+};
