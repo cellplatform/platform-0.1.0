58ecae7e1b89afc680c93ac0b68bdb8b90238f70
Phil Cockfield <phil@cockfield.net>
2020-06-14 13:40:06 +1200

    rx.event

--- code/util.value/src/rx/rx.event.ts
+++ code/util.value/src/rx/rx.event.ts
@@ -4,6 +4,19 @@ import { filter, map } from 'rxjs/operators';
 /**
  * Filters on the given event.
  */
+export function event<E extends { type: string; payload: unknown }>(
+  ob$: Observable<{}>,
+  type: E['type'],
+) {
+  return ob$.pipe(
+    filter((e: any) => e.type === type),
+    map((e: any) => e as E),
+  );
+}
+
+/**
+ * Filters on the given event returning the payload.
+ */
 export function payload<E extends { type: string; payload: unknown }>(
   ob$: Observable<{}>,
   type: E['type'],
