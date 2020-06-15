8c2ed29e953049889897fc5e2e5a7e20eeb32aed
Phil Cockfield <phil@cockfield.net>
2020-06-16 07:13:55 +1200

    Stub in Panel for sheet.

---
+++ code/cell.ui.spreadsheet/src/components/Panel/Panel.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { css, color, CssValue, ui, t } from '../../common';
+
+export type IPanelProps = { style?: CssValue };
+export type IPanelState = {};
+
+export class Panel extends React.PureComponent<IPanelProps, IPanelState> {
+  public state: IPanelState = {};
+  private state$ = new Subject<Partial<IPanelState>>();
+  private unmounted$ = new Subject<{}>();
+
+  public static contextType = ui.Context;
+  public context!: t.IAppContext;
+
+  /**
+   * [Lifecycle]
+   */
+  constructor(props: IPanelProps) {
+    super(props);
+  }
+
+  public componentDidMount() {
+    this.state$.pipe(takeUntil(this.unmounted$)).subscribe((e) => this.setState(e));
+    this.load();
+  }
+
+  public componentWillUnmount() {
+    this.unmounted$.next();
+    this.unmounted$.complete();
+  }
+
+  /**
+   * [Methods]
+   */
+  public async load() {
+    //
+  }
+
+  /**
+   * [Render]
+   */
+  public render() {
+    const styles = {
+      base: css({ Absolute: 0 }),
+    };
+    return (
+      <div {...css(styles.base, this.props.style)}>
+        <div>SheetPanel</div>
+      </div>
+    );
+  }
+}
--- code/cell.ui.spreadsheet/src/components/Root/Root.tsx
+++ code/cell.ui.spreadsheet/src/components/Root/Root.tsx
@@ -4,6 +4,7 @@ import { takeUntil } from 'rxjs/operators';
 import { css, CssValue, color } from '../../common';
 import { WindowTitleBar } from '../primitives';
 import { Grid } from '../Grid';
+import { Panel } from '../Panel';
 
 export type IRootProps = { style?: CssValue };
 export type IRootState = {};
@@ -36,7 +37,20 @@ export class Root extends React.PureComponent<IRootProps, IRootState> {
     const styles = {
       base: css({ Absolute: 0, backgroundColor: color.format(1) }),
       titlebar: css({ Absolute: [0, 0, null, 0] }),
-      body: css({ Absolute: [WindowTitleBar.HEIGHT, 0, 0, 0] }),
+      body: css({
+        Absolute: [WindowTitleBar.HEIGHT, 0, 0, 0],
+        Flex: 'horizontal-stretch-stretch',
+      }),
+      grid: css({
+        position: 'relative',
+        flex: 1,
+        display: 'flex',
+        backgroundColor: 'rgba(255, 0, 0, 0.1)' /* RED */,
+      }),
+      panel: css({
+        width: 250,
+        borderLeft: `solid 1px ${color.format(-0.1)}`,
+      }),
     };
 
     const uri = ''; // TEMP 🐷
@@ -44,7 +58,14 @@ export class Root extends React.PureComponent<IRootProps, IRootState> {
     return (
       <div {...css(styles.base, this.props.style)}>
         <WindowTitleBar style={styles.titlebar} address={uri} />
-        <Grid style={styles.body} />
+        <div {...styles.body}>
+          <div {...styles.grid}>
+            <Grid style={{ Absolute: 0 }} />
+          </div>
+          <div {...styles.panel}>
+            <Panel />
+          </div>
+        </div>
       </div>
     );
   }
