be1a944233201a9e777de70db24fcf5053d12833
Phil Cockfield <phil@cockfield.net>
2020-07-17 12:46:30 +1200

    init debug

--- code/cell.ui.sys/src/components/Debug.Sheet/DebugSheet.tsx
+++ code/cell.ui.sys/src/components/Debug.Sheet/DebugSheet.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
-import { color, css, CssValue, t, ui } from '../../common';
+import { color, css, CssValue, t, ui, AppWindowModel, time } from '../../common';
+import { DebugLog } from '../Debug.Log';
+import { Button } from '../primitives';
 
 export type IDebugSheetProps = { style?: CssValue };
 export type IDebugSheetState = t.Object;
@@ -28,22 +30,87 @@ export class DebugSheet extends React.PureComponent<IDebugSheetProps, IDebugShee
     this.unmounted$.complete();
   }
 
+  public init = async () => {
+    const ctx = this.context;
+    const window = await AppWindowModel.load({ client: ctx.client, uri: ctx.env.def });
+
+    console.log('w.toObject()', window.toObject());
+
+    const app = window.app;
+
+    console.log('app.toObject()', app.toObject());
+
+    console.log('-------------------------------------------');
+    const appData = await app.props.data.load();
+
+    console.log('data', appData);
+
+    // const row = data.
+
+    const row = await (await appData.data()).row(0).load();
+
+    // await row.load();
+
+    console.log('row.toObject()', row.toObject());
+
+    console.log('row.props.tmp', row.props.tmp);
+
+    row.props.tmp = 'hello';
+
+    console.log('row.props.tmp', row.props.tmp);
+
+    await time.wait(10);
+
+    const res = await ctx.client.saveChanges({ sheet: appData.sheet });
+    console.log('res', res);
+
+    // ctx.
+
+    // console.log("row.", row.)
+  };
+
   /**
    * [Render]
    */
   public render() {
+    const ctx = this.context;
     const styles = {
       base: css({
         Absolute: 0,
         backgroundColor: color.format(1),
-        padding: 20,
+
         boxSizing: 'border-box',
+        Flex: 'horizontal-stretch-stretch',
+      }),
+      left: css({ flex: 1, padding: 20 }),
+      right: css({
+        flex: 1,
+        display: 'flex',
+        borderLeft: `solid 1px ${color.format(-0.1)}`,
       }),
     };
     return (
       <div {...css(styles.base, this.props.style)}>
-        <div>DebugSheet</div>
+        <div {...styles.left}>{this.renderLeft()}</div>
+        <div {...styles.right}>
+          <DebugLog event$={ctx.event$} style={{ flex: 1 }} />
+        </div>
       </div>
     );
   }
+
+  private renderLeft() {
+    const styles = {
+      base: css({}),
+    };
+    return (
+      <div {...styles.base}>
+        <Button onClick={this.init}>init</Button>
+      </div>
+    );
+  }
+
+  /**
+   * [Handlers]
+   */
 }
