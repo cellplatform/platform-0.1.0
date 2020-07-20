77d014f8bd70b5476d03f03180eead81e07b5220
Phil Cockfield <phil@cockfield.net>
2020-07-20 17:20:04 +1200

    Visual tweaks and close button

--- code/ui.tree/src/test.parcel/components/Test.StateStore.tsx
+++ code/ui.tree/src/test.parcel/components/Test.StateStore.tsx
@@ -8,11 +8,18 @@ import { TreeView } from '../..';
 import { t, COLORS } from '../../common';
 import { TextInput } from '@platform/ui.text/lib/components/TextInput';
 import { TextInputChangeEventHandler } from '@platform/ui.text/lib/types';
+import { defaultValue } from '@platform/util.value';
+import { Icons } from './Icons';
 
 const S = TreeView.State;
+const DEFAULT = {
+  CONNECTOR_HEIGHT: 40,
+};
 
 export type ITestStateStoreProps = {
   store: t.ITreeViewState;
+  isRoot?: boolean;
+  connectorHeight?: number;
   style?: CssValue;
 };
 export type ITestStateStoreState = {
@@ -33,6 +40,7 @@ export class TestStateStore extends React.PureComponent<
 
   public componentDidMount() {
     this.store.changed$.pipe(takeUntil(this.unmounted$)).subscribe((e) => this.forceUpdate());
+    this.store.dispose$.pipe(takeUntil(this.unmounted$)).subscribe(() => this.forceUpdate());
     this.state$.pipe(takeUntil(this.unmounted$)).subscribe((e) => this.setState(e));
   }
 
@@ -48,6 +56,14 @@ export class TestStateStore extends React.PureComponent<
     return this.props.store;
   }
 
+  public get isRoot() {
+    return this.props.isRoot !== false;
+  }
+
+  public get connectorHeight() {
+    return defaultValue(this.props.connectorHeight, DEFAULT.CONNECTOR_HEIGHT);
+  }
+
   /**
    * [Render]
    */
@@ -55,47 +71,99 @@ export class TestStateStore extends React.PureComponent<
     const store = this.store;
     const styles = {
       base: css({
+        position: 'relative',
         boxSizing: 'border-box',
         backgroundColor: color.format(-0.02),
         border: `solid 1px ${color.format(-0.04)}`,
         borderRadius: 4,
         padding: 10,
+        paddingTop: 8,
         fontSize: 14,
         marginBottom: 10,
         ':last-child': { marginBottom: 0 },
       }),
-      title: css({
+      children: css({
+        marginTop: this.connectorHeight - 1,
+      }),
+    };
+    return (
+      <div {...css(styles.base, this.props.style)}>
+        {this.renderConnector()}
+        {this.renderTitle()}
+        {this.renderTools()}
+        {store.children.length > 0 && <div {...styles.children}>{this.renderChildren()}</div>}
+      </div>
+    );
+  }
+
+  private renderTitle() {
+    const store = this.store;
+    const styles = {
+      base: css({
         Flex: 'horizontal-center-spaceBetween',
+        borderBottom: `solid 8px ${color.format(-0.08)}`,
+        paddingBottom: 8,
+        marginBottom: 20,
+      }),
+      left: css({
+        Flex: 'horizontal-center-center',
+      }),
+      right: css({
+        Flex: 'horizontal-center-center',
       }),
       id: css({
         fontFamily: 'menlo, monospace',
         fontSize: 12,
         color: COLORS.PURPLE,
+        marginRight: 8,
       }),
     };
+
     return (
-      <div {...css(styles.base, this.props.style)}>
-        <div {...styles.title}>
-          <div>
-            <strong>TreeState</strong>
-          </div>
-          <div {...styles.id}>{store.id}</div>
+      <div {...styles.base}>
+        <div {...styles.left}>
+          <Icons.Box size={18} style={{ marginRight: 4 }} color={COLORS.PURPLE} />
+          <strong>TreeState</strong>
         </div>
-        {this.renderTools()}
-        {store.children.length > 0 && this.renderChildren()}
+        <div {...styles.right}>
+          <div {...styles.id}>{store.id}</div> {!this.isRoot && this.renderCloseButton()}
+        </div>
       </div>
     );
+  }
+
+  private renderCloseButton() {
+    return (
+      <Button onClick={this.onCloseClick}>
+        <Icons.Close />
+      </Button>
+    );
+  }
+
+  private renderConnector(props: { style?: CssValue } = {}) {
+    if (this.isRoot) {
+      return null;
+    }
+    const height = this.connectorHeight;
+    const styles = {
+      base: css({
+        Absolute: [-height, null, null, 10],
+        width: 10,
+        height,
+        backgroundColor: color.format(-0.08),
+      }),
+    };
+    return <div {...css(styles.base, props.style)}></div>;
   }
 
   private renderTools() {
     const styles = {
       base: css({
         marginTop: 10,
+        marginBottom: 10,
         Flex: 'horizontal-stretch-stretch',
       }),
-      left: css({
-        flex: 1,
-      }),
+      left: css({ flex: 1 }),
       middle: css({ width: 20 }),
       right: css({ flex: 1 }),
     };
@@ -104,16 +172,14 @@ export class TestStateStore extends React.PureComponent<
         <div {...styles.left}>
           {this.renderInputButton({
             placeholder: 'label',
-            // button: 'add',
             value: this.store.root.props?.label,
-            // onClick: this.onLabelChange,
             onChange: this.onLabelChange,
           })}
         </div>
         <div {...styles.middle}></div>
         <div {...styles.right}>
           {this.renderInputButton({
-            placeholder: 'child label',
+            placeholder: 'new child',
             button: 'add',
             value: this.state.addLabel,
             onClick: this.addChild,
@@ -168,16 +234,20 @@ export class TestStateStore extends React.PureComponent<
   }
 
   private renderChildren() {
+    const connectorHeight = this.connectorHeight;
     const styles = {
-      base: css({
-        marginTop: 30,
+      base: css({ marginLeft: 12 }),
+      child: css({
+        marginTop: connectorHeight - 1,
       }),
     };
-
     const elList = this.store.children.map((store) => {
-      return <TestStateStore key={store.namespace} store={store} />;
+      return (
+        <div key={store.namespace} {...styles.child}>
+          <TestStateStore store={store} isRoot={false} connectorHeight={connectorHeight} />
+        </div>
+      );
     });
-
     return <div {...styles.base}>{elList}</div>;
   }
 
@@ -206,5 +276,9 @@ export class TestStateStore extends React.PureComponent<
     this.store.change((draft) => {
       S.props(draft, (props) => (props.label = e.to));
     });
+  };
+
+  private onCloseClick = () => {
+    this.store.dispose();
   };
 }
