a64b977612e238dbd55ebcaf752924212185505d
Phil Cockfield <phil@cockfield.net>
2020-08-21 11:14:00 +1200

    ui.tree: render test component list in code.

--- code/ui.tree/src/test.parcel/index.html.tsx
+++ code/ui.tree/src/test.parcel/index.html.tsx
@@ -1,17 +1,46 @@
 import * as React from 'react';
-import { render } from '@platform/ui.dev';
+import { render, css } from '@platform/ui.dev';
 
 const test = {
   Component: import('./components/Test.Component'),
-  State: import('./components/Test.State'),
   Columns: import('./components/Test.Columns'),
+  State: import('./components/Test.State'),
 };
 
 type M = { Test: React.FunctionComponent };
 const key = window.location.search.trim().replace(/^\?/, '');
-const module = test[key];
+const component = test[key];
 
-if (module) {
-  module.then((e: M) => render(React.createElement(e.Test)));
+if (component) {
+  /**
+   * Load selected component.
+   */
+  component.then((e: M) => render(React.createElement(e.Test)));
   document.title = `${document.title} - ${key}`;
+} else {
+  /**
+   * Index of test components.
+   */
+  const styles = {
+    ul: css({
+      fontFamily: 'sans-serif',
+      margin: 30,
+      marginTop: 40,
+      lineHeight: '1.8em',
+    }),
+    a: css({ textDecoration: 'none' }),
+  };
+
+  const items = Object.keys(test).map((key) => {
+    return (
+      <li key={key}>
+        <a href={`./?${key}`} {...styles.a}>
+          {key}
+        </a>
+      </li>
+    );
+  });
+
+  const ul = <ul {...styles.ul}>{items}</ul>;
+  render(ul);
 }
