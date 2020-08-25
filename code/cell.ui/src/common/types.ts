195488a35e3bcd0310b29e7ba20dd9eb78a91cdf
Phil Cockfield <phil@cockfield.net>
2020-08-25 13:43:32 +1200

    update refs

--- code/cell.ui/src/common/types.ts
+++ code/cell.ui/src/common/types.ts
@@ -2,7 +2,7 @@ export * from '../types';
 
 export * from '@platform/types';
 export * from '@platform/cell.types';
-export * from '@platform/ui.tree/lib/types';
+export * from '@platform/ui.tree.types';
 export * from '@platform/state/lib/types';
 export * from '@platform/cell.module/lib/types';
 
--- code/cell.ui/src/types.ts
+++ code/cell.ui/src/types.ts
@@ -1,2 +1,1 @@
 export * from './components/types';
-export * from './ViewModule/types';
