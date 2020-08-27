8cc04dc86de9040e67bc4f778c2aaa6f686ffd2b
Phil Cockfield <phil@cockfield.net>
2020-08-28 07:59:21 +1200

    .dev

--- code/cell.ui.sys/src/components.dev/module.Harness/.dev/SAMPLE.ts
+++ code/cell.ui.sys/src/components.dev/module.Harness/.dev/SAMPLE.ts
@@ -1,14 +1,22 @@
-import { t } from '../common';
-
+import { t, time } from '../common';
 import { SampleModule } from '../../ModuleView.dev/module.Sample';
+import { OneModule } from './module.One';
+import { TwoModule } from './module.Two';
 
 /**
  * Simulate module insertion into UIHarness.
  */
-export async function sampleInit(ctx: t.IAppContext) {
-  const bus = ctx.bus.type<t.HarnessEvent>();
+export async function sampleInit(bus: t.EventBus) {
+  const fire = bus.type<t.HarnessEvent>().fire;
 
-  const module = SampleModule.init(ctx.bus.type());
+  const sample = SampleModule.init(bus);
+  const one = OneModule.dev(bus);
+  const two = TwoModule.init(bus);
 
-  bus.fire({ type: 'Harness/add', payload: { module: module.id } });
+  // time.delay(800, () => {
+  // });
+  fire({ type: 'Harness/add', payload: { module: one.id } });
+  fire({ type: 'Harness/add', payload: { module: one.id } }); // NB: Does not fail (double entry)
+  fire({ type: 'Harness/add', payload: { module: two.id } });
+  fire({ type: 'Harness/add', payload: { module: sample.id } });
 }
---
+++ code/cell.ui.sys/src/components.dev/module.Harness/.dev/module.One/Module.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+
+import { Module, t } from './common';
+
+type P = t.OneProps;
+
+export const OneModule: t.SampleOneModuleDef = {
+  /**
+   * ENTRY: Initialize a new module from the definition.
+   */
+  init(bus, parent) {
+    const module = Module.create<P>({
+      bus,
+      root: { id: '', props: { treeview: { label: 'Sample One' }, view: 'DEFAULT' } },
+    });
+
+    /**
+     * Setup event pub/sub.
+     */
+    const match: t.ModuleFilterEvent = (e) => e.module == module.id || module.contains(e.module);
+    const events = Module.events<P>(Module.filter(bus.event$, match), module.dispose$);
+    const fire = Module.fire<P>(bus);
+
+    /**
+     * STRATEGY: render user-interface.
+     */
+    renderer(events);
+    events.selection$.subscribe((e) => {
+      const { view, data } = e;
+      const selected = e.selection?.id;
+      fire.render({ selected, module, data, view, notFound: '404' });
+    });
+
+    return module;
+  },
+
+  /**
+   * DEV: harness entry point.
+   */
+  dev(bus) {
+    const dev = Module.create<t.DevProps>({
+      bus,
+      root: { id: '', props: { treeview: { label: 'Sample One (Dev)' } } },
+    });
+
+    const node = (id: string, view?: t.OneView) => {
+      const host: t.HarnessHost = { view: view || '', layout: { location: 'yo' } };
+      const props: t.DevProps = {
+        view: 'HOST/component',
+        data: { host },
+      };
+      return { id, props };
+    };
+
+    dev.change((draft, ctx) => {
+      const children = draft.children || (draft.children = []);
+      children.push(node('123', 'FOO'), node('456'), node('789', '404'));
+    });
+
+    const match: t.ModuleFilterEvent = (e) => e.module == dev.id;
+    const events = Module.events<P>(Module.filter(bus.event$, match), dev.dispose$);
+    renderer(events);
+
+    return dev;
+  },
+};
+
+/**
+ * UI: View factory for the module.
+ */
+function renderer(events: t.IViewModuleEvents<P>) {
+  const render = events.render;
+
+  render('DEFAULT').subscribe((e) => {
+    const el = <div style={{ padding: 20 }}>Module One</div>;
+    e.render(el);
+  });
+
+  render('FOO').subscribe((e) => {
+    const el = <div style={{ padding: 20 }}>FOO</div>;
+    e.render(el);
+  });
+
+  /**
+   * Wildcard.
+   */
+  render('404').subscribe((e) => {
+    const el = <div style={{ padding: 20 }}>Module One (404)</div>;
+    e.render(el);
+  });
+}
