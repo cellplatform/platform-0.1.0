88617438681063b0d9a518a04bb183097260b120
Phil Cockfield <phil@cockfield.net>
2020-09-02 08:08:39 +1200

    Sidebar

--- code/cell.ui.sys/src/modules/module.DevHarness/components/Layout/Layout.tsx
+++ code/cell.ui.sys/src/modules/module.DevHarness/components/Layout/Layout.tsx
@@ -5,11 +5,13 @@ import { color, css, CssValue, t, ui, defaultValue } from '../../common';
 import { Harness } from '../../Module';
 import { LayoutTree } from './Layout.Tree';
 import { COLORS } from '@platform/cell.ui/lib/common/constants';
+import { Sidebar } from './Layout.Sidebar';
 
 type P = t.HarnessProps;
 type V = t.HarnessView;
 
 export type ILayoutProps = {
+  harness?: t.HarnessModule;
   focusOnLoad?: boolean;
   style?: CssValue;
 };
@@ -25,8 +27,7 @@ export class Layout extends React.PureComponent<ILayoutProps> {
    * [Lifecycle]
    */
   public componentDidMount() {
-    const ctx = this.context;
-    this.harness = Harness.init(ctx.bus);
+    this.harness = this.props.harness || Harness.module(this.context.bus);
     this.forceUpdate(); // NB: Redraw causes the newly created [module] to be rendered.
   }
 
@@ -53,6 +54,7 @@ export class Layout extends React.PureComponent<ILayoutProps> {
 
     const bus = this.context.bus;
     const focusOnLoad = defaultValue(this.props.focusOnLoad, true);
+    const main: t.HarnessTarget = 'Main';
 
     const styles = {
       base: css({
@@ -63,24 +65,23 @@ export class Layout extends React.PureComponent<ILayoutProps> {
       fill: css({
         Absolute: 0,
       }),
-      left: css({
+      edge: css({
         position: 'relative',
         display: 'flex',
-        width: 250,
         backgroundColor: color.darken(COLORS.WHITE, 5),
         WebkitAppRegion: 'drag',
+        overflow: 'hidden',
+      }),
+      left: css({
+        width: 250,
       }),
       right: css({
         width: 250,
-        backgroundColor: color.darken(COLORS.WHITE, 5),
-        WebkitAppRegion: 'drag',
       }),
       body: css({
         flex: 1,
         display: 'flex',
         position: 'relative',
-        // borderLeft: `solid 10px ${color.format(-0.06)}`,
-        // borderRight: `solid 10px ${color.format(-0.06)}`,
         borderLeft: `solid 10px ${color.format(0.1)}`,
         borderRight: `solid 10px ${color.format(0.1)}`,
         WebkitAppRegion: 'drag',
@@ -89,13 +90,20 @@ export class Layout extends React.PureComponent<ILayoutProps> {
     };
     return (
       <div {...css(styles.base, this.props.style)}>
-        <div {...styles.left}>
+        <div {...css(styles.edge, styles.left)}>
           <LayoutTree harness={harness} focusOnLoad={focusOnLoad} />
         </div>
         <div {...styles.body}>
-          <ui.ModuleView.Frame bus={bus} filter={this.bodyFilter} style={styles.fill} />
+          <ui.ModuleView.Frame
+            bus={bus}
+            filter={this.bodyFilter}
+            style={styles.fill}
+            target={main}
+          />
         </div>
-        <div {...styles.right}></div>
+        <div {...css(styles.edge, styles.right)}>
+          <Sidebar bus={bus} harness={harness} />
+        </div>
       </div>
     );
   }
