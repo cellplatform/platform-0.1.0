d2033e5a458d6e37771140c52e1163b7d82a15d2
Phil Cockfield <phil@cockfield.net>
2020-10-21 10:30:32 +1300

    fixup

--- code/cell.modules/pkg.sample/bar/src/compiler.config.ts
+++ code/cell.modules/pkg.sample/bar/src/compiler.config.ts
@@ -6,6 +6,6 @@ export default () =>
     .title('My Bar')
     .url(3002)
     .entry({ main: './src/test/entry' })
-    .shared((e) => e.add(e.dependencies).singleton(['preset.react', 'preset.css']))
+    .shared((e) => e.add(e.dependencies).singleton(['preset.react']))
     .variant('prod', (config) => config.mode('prod'))
     .variant('dev', (config) => config.mode('dev'));
--- code/react/src/events/util.ts
+++ code/react/src/events/util.ts
@@ -1,11 +1,8 @@
 import { fromEvent as rxFromEvent, Observable, Subject } from 'rxjs';
 import { FromEventTarget } from 'rxjs/internal/observable/fromEvent';
 import { share } from 'rxjs/operators';
-// import { is } from '../common';
 
 import { is } from '@platform/util.is';
-
-// export { is };
 
 /**
  * Create observable from event.
