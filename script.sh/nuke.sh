c4d0c20056e6dfefe02ac654f55f81028bad40b7
Phil Cockfield <phil@cockfield.net>
2020-10-21 08:57:02 +1300

    nuke

--- script.sh/nuke.sh
+++ script.sh/nuke.sh
@@ -8,6 +8,5 @@ rm -rf code/*/node_modules
 rm -f yarn.lock
 rm -f code/*/yarn.lock
 
-cd code/cell.modules && sh script.sh/nuke.sh && cd ../../
-cd code/cell.electron && sh script.sh/nuke.sh && cd ../../
-
+cd code/cell.modules && yarn nuke && cd ../../
+cd code/cell.electron && yarn nuke && cd ../../
