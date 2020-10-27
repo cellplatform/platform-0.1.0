f2eceddf976adcb0d80c5b607166d5b0ad566160
Phil Cockfield <phil@cockfield.net>
2020-10-28 08:18:57 +1300

    fs: load JSON

--- code/fs/src/stream/stream.ts
+++ code/fs/src/stream/stream.ts
@@ -3,21 +3,35 @@ import * as util from 'util';
 
 import { ensureDir, createWriteStream, writeFile } from 'fs-extra';
 import { dirname } from 'path';
+import { t } from '../common';
 
 const pipeline = util.promisify(stream.pipeline);
 
 /**
  * Saves a readable stream to disk.
  */
-export async function save(path: string, data: ReadableStream | string) {
+export async function save(path: string, data: ReadableStream | t.Json | string) {
   try {
     await ensureDir(dirname(path));
     if (typeof data === 'string') {
       await writeFile(path, data);
-    } else {
-      const output = createWriteStream(path);
-      await pipeline(data as any, output);
+      return;
     }
+
+    if (typeof data === 'object') {
+      const isStream = typeof (data as any).on === 'function';
+      if (isStream) {
+        // Stream.
+        await pipeline(data as any, createWriteStream(path));
+      } else {
+        // JSON.
+        await writeFile(path, JSON.stringify(data, null, '  '));
+      }
+
+      return;
+    }
+
+    throw new Error(`Type of data not saveable (${typeof data})`);
   } catch (err) {
     throw new Error(`Failed to save stream to '${path}'. ${err.message}`);
   }
