65e8cebdafb3fd08a292fe7dc948b7af2130d01c
Phil Cockfield <phil@cockfield.net>
2020-11-11 18:53:43 +1300

    bump

--- code/cell.http.router/src/common/constants.pkg.ts
+++ code/cell.http.router/src/common/constants.pkg.ts
@@ -1,6 +1,6 @@
 export const PKG = {
   name: '@platform/cell.http.router',
-  version: '0.8.12',
+  version: '0.8.13',
   dependencies: {
     '@platform/cache': '0.2.41',
     '@platform/cell.client': '0.2.170',
