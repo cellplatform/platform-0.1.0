c4694b43d5a16863ed42a858dab4efab48853353
Phil Cockfield <phil@cockfield.net>
2020-11-27 14:10:21 +1300

    bump

--- code/cell.client/src/common/constants.pkg.ts
+++ code/cell.client/src/common/constants.pkg.ts
@@ -1,15 +1,15 @@
 export const PKG = {
   name: '@platform/cell.client',
-  version: '0.3.16',
+  version: '0.3.20',
   dependencies: {
-    '@platform/cell.schema': '0.5.26',
-    '@platform/cell.types': '0.5.27',
-    '@platform/cell.typesystem': '0.0.145',
-    '@platform/http': '0.14.0',
-    '@platform/http.types': '0.2.25',
+    '@platform/cell.schema': '0.5.30',
+    '@platform/cell.types': '0.5.31',
+    '@platform/cell.typesystem': '0.0.149',
+    '@platform/http': '0.14.4',
+    '@platform/http.types': '0.2.29',
     '@platform/libs': '0.2.3',
-    '@platform/queue': '0.0.29',
-    '@platform/types': '0.5.8',
+    '@platform/queue': '0.0.33',
+    '@platform/types': '0.5.11',
     'form-data': '3.0.0',
   },
 };
--- code/cell.typesystem/src/common/constants.pkg.ts
+++ code/cell.typesystem/src/common/constants.pkg.ts
@@ -1,4 +1,4 @@
 export const PKG = {
   name: '@platform/cell.typesystem',
-  version: '0.0.145',
+  version: '0.0.149',
 };
