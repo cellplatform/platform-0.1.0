31e3c511a2ce3f6793c1eeb9a3550f62f6de0fef
Phil Cockfield <phil@cockfield.net>
2020-12-20 18:36:16 +1300

    util: is.subject(..)

--- code/util.is/src/is.TEST.ts
+++ code/util.is/src/is.TEST.ts
@@ -30,6 +30,24 @@ describe('Is', () => {
     test(new Subject().asObservable(), true);
   });
 
+  it('is.subject', () => {
+    const test = (input: any, expected: boolean) => {
+      expect(is.subject(input)).to.eql(expected);
+    };
+
+    test(undefined, false);
+    test(null, false);
+    test('hello', false);
+    test(1223, false);
+    test({}, false);
+
+    test({ subscribe: () => null }, false);
+    test({ subscribe: () => null, next: () => null }, true);
+
+    test(new Subject(), true);
+    test(new Subject().asObservable(), false);
+  });
+
   it('is.stream', () => {
     const test = (input: any, expected: boolean) => {
       expect(is.stream(input)).to.eql(expected);
---
+++ code/util.is/src/is.ts
@@ -0,0 +1,60 @@
+import * as t from './types';
+
+/**
+ * Environment flags.
+ */
+export const is: t.Is = {
+  get nodeEnv() {
+    return this.browser ? 'browser' : process.env.NODE_ENV || 'development';
+  },
+
+  get browser() {
+    return typeof window !== 'undefined';
+  },
+
+  get dev() {
+    const env = this.nodeEnv;
+    return this.browser
+      ? window && window.location.hostname === 'localhost'
+      : env !== 'production' && env !== 'prod';
+  },
+
+  get prod() {
+    return !this.dev;
+  },
+
+  get test() {
+    return this.nodeEnv === 'test';
+  },
+
+  toObject(): t.IsFlags {
+    return {
+      nodeEnv: this.nodeEnv,
+      browser: this.browser,
+      dev: this.dev,
+      prod: this.prod,
+      test: this.test,
+    };
+  },
+
+  /**
+   * Determine if the given input is an Observable.
+   */
+  observable(input?: any) {
+    return typeof input === 'object' && typeof input?.subscribe === 'function';
+  },
+
+  /**
+   * Determine if the given input is an observable Subject.
+   */
+  subject(input?: any) {
+    return is.observable(input) && typeof input?.next === 'function';
+  },
+
+  /**
+   * Determine if the given input is a node stream
+   */
+  stream(input?: any) {
+    return typeof input?.on === 'function';
+  },
+};
