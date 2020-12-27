f1c52e86b49331ab63f516a4538ae15263e06698
Phil Cockfield <phil@cockfield.net>
2020-12-28 12:37:00 +1300

    clean: typescript build arifacts

--- code/cell.compiler/src/node.bin/cmd/cmd.clean.ts
+++ code/cell.compiler/src/node.bin/cmd/cmd.clean.ts
@@ -1,20 +1,15 @@
-import { fs, log, t } from '../common';
-import * as util from '../util';
+import { fs, log, t, ProgressSpinner } from '../common';
+import { Typescript } from '../../node/ts';
 
 /**
  * Remove transient build artifacts.
  */
 export async function clean(argv: t.Argv) {
+  const spinner = ProgressSpinner({ label: 'cleaning...' });
+  spinner.start();
+
   const base = fs.resolve('.');
   const paths = ['dist', 'node_modules/.cache', 'tmp/runtime.node'].map((path) => fs.resolve(path));
-
-  log.info();
-  log.info.gray('Clean');
-  paths.forEach((path) => {
-    path = path.substring(base.length + 1);
-    log.info.gray(`  ${base}/${log.white(path)}`);
-  });
-  log.info();
 
   await Promise.all(
     paths.map(async (path) => {
@@ -23,4 +18,16 @@ export async function clean(argv: t.Argv) {
       }
     }),
   );
+
+  paths.push(...(await Typescript.clean()));
+  spinner.stop();
+
+  // Log changes.
+  log.info();
+  log.info.gray('Deleted');
+  paths.forEach((path) => {
+    path = path.substring(base.length + 1);
+    log.info.gray(`  ${base}/${log.white(path)}`);
+  });
+  log.info();
 }
