7d23092c94747f77b8eed414ad0cd6d78e134b30
Phil Cockfield <phil@cockfield.net>
2020-12-29 19:50:31 +1300

    tests for type lib info

--- code/cell.compiler/src/node/ts/Typescript.TEST.ts
+++ code/cell.compiler/src/node/ts/Typescript.TEST.ts
@@ -1,8 +1,10 @@
 import { Typescript } from '.';
-import { expect, fs, expectError } from '../../test';
+import { expect, fs, expectError, SampleBundles } from '../../test';
 
 describe('Typescript', function () {
   this.timeout(99999);
+
+  const config = SampleBundles.simpleNode.config;
 
   describe('compiler', () => {
     it('throw: tsconfig file not found', async () => {
@@ -11,13 +13,15 @@ describe('Typescript', function () {
     });
 
     it('compile declarations', async () => {
+      const model = config.toObject();
       const compiler = Typescript.compiler('tsconfig.json');
       const include = 'src/test/test.bundles/node.simple/**/*';
 
       const res = await compiler.declarations({
         base: 'tmp/types.d',
-        dir: 'foo',
+        dir: 'main',
         include,
+        model,
       });
 
       const expectExists = async (path: string) => {
@@ -25,17 +29,22 @@ describe('Typescript', function () {
         expect(exists).to.eql(true);
       };
 
-      await expectExists('tmp/types.d/foo/index.json');
-      await expectExists('tmp/types.d/foo/main.d.ts');
+      await expectExists('tmp/types.d/main/index.json');
+      await expectExists('tmp/types.d/main/main.d.ts');
       await expectExists('tmp/types.d/@platform/cell.types/index.json');
       await expectExists('tmp/types.d/@platform/log/lib/server/index.json');
 
       expect(res.tsconfig.include).to.eql([include]);
       expect(res.tsconfig.compilerOptions.emitDeclarationOnly).to.eql(true);
 
-      expect(res.output.manifest.kind).to.eql('typelib');
       expect(res.output.base).to.eql(fs.resolve('tmp/types.d'));
-      expect(res.output.dir).to.eql(fs.resolve('tmp/types.d/foo'));
+      expect(res.output.dir).to.eql(fs.resolve('tmp/types.d/main'));
+
+      const manifest = res.output.manifest;
+      expect(manifest.kind).to.eql('typelib');
+      expect(manifest.typelib.name).to.eql('node.simple');
+      expect(manifest.typelib.version).to.eql('0.0.1');
+      expect(manifest.typelib.entry).to.eql('./types.d.ts');
     });
   });
 });
