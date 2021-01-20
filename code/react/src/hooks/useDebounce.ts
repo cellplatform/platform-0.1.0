6cc4d0a1d7cc6273a95bdd3188228eb456b5d14a
Phil Cockfield <phil@cockfield.net>
2021-01-21 12:18:24 +1300

    comments

--- code/react/src/hooks/useDebounce.ts
+++ code/react/src/hooks/useDebounce.ts
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
 
 /**
+ * Provides a debounce for state updates.
+ * Use in conjunction with [useState], for example:
+ *
+ *      const [foo, setFoo] = useState<number>(0);
+ *      const fooDebounced = useDebounce(foo, 300);
+ *
  * Source:
  *    https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
+ *
  */
-export function useDebounce<T>(value: T, delay: number, onChanged?: (value: T) => void) {
+export function useDebounce<T>(value: T, msecs: number, onChanged?: (value: T) => void) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
@@ -15,7 +22,7 @@ export function useDebounce<T>(value: T, delay: number, onChanged?: (value: T) =
         if (onChanged) onChanged(value);
       }
     };
-    const timeout = setTimeout(change, delay);
+    const timeout = setTimeout(change, msecs);
     return () => clearTimeout(timeout);
   }, [value]); // eslint-disable-line
 
