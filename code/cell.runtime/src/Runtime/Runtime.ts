be082730f7dbe54e83ef9ecaa99ec89b67b4b5f4
Phil Cockfield <phil@cockfield.net>
2021-02-17 09:22:34 +1300

    bundle.path - strip path for localhost

--- code/cell.runtime/src/Runtime/Runtime.ts
+++ code/cell.runtime/src/Runtime/Runtime.ts
@@ -5,6 +5,10 @@ const toEnv = (input?: RuntimeModule) => {
   return !input && typeof __CELL__ !== 'undefined' ? __CELL__ : input;
 };
 
+const regex = {
+  localhost: /^http:\/\/localhost(\:\d+)?\//,
+};
+
 export const Runtime = {
   /**
    * Wrapper around the __CELL__ constants inserted into modules
@@ -16,23 +20,35 @@ export const Runtime = {
    */
   origin(input?: RuntimeModule) {
     input = toEnv(input);
+
     const location = typeof window === 'object' ? window.location : undefined;
     const origin = input?.origin;
-    const dev = !Boolean(origin) || (origin?.uri || '').startsWith('cell:dev:');
+    const host = origin?.host || location?.host || '';
+    const isLocalhost = !origin ? true : host === 'localhost' || host.startsWith('localhost:');
+    const uri = origin?.uri || 'cell:dev:A1';
+    const dev = uri.startsWith('cell:dev:');
+    const dir = trimSlash(origin?.dir || '');
+    const urls = Urls.create(host);
 
     if (dev) {
       Uri.ALLOW.NS = [...Uri.ALLOW.NS, 'dev'];
     }
 
-    const host = origin?.host || location?.host || 'localhost:3000';
-    const uri = origin?.uri || 'cell:dev:A1';
-    const dir = trimSlash(origin?.dir || '');
-    const urls = Urls.create(host);
+    const isPathOnLocalhost = (path: string) => {
+      if (!isLocalhost) return false;
+      return origin ? path.startsWith(`http://${host}`) : true;
+    };
 
     const path = (path: string) => {
-      return dev
+      let res = dev
         ? `${urls.origin}/${trimSlash(path)}`
         : urls.cell(uri).file.byName(prepend(dir, path)).toString();
+
+      if (isPathOnLocalhost(res)) {
+        res = `/${res.replace(regex.localhost, '')}`;
+      }
+
+      return res;
     };
 
     return { dev, host, uri, dir, path };
