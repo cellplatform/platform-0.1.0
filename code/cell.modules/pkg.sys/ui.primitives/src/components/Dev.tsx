0cb38d6f9513f5194e37136928c47808f946fa28
Phil Cockfield <phil@cockfield.net>
2021-03-03 16:11:05 +1300

    ui.primitives - remove DragTarget

--- code/cell.modules/pkg.sys/ui.primitives/src/components/Dev.tsx
+++ code/cell.modules/pkg.sys/ui.primitives/src/components/Dev.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Harness } from 'sys.ui.dev';
 
-import DragTarget from './DragTarget/DragTarget.DEV';
-
-export const ACTIONS = [DragTarget];
+export const ACTIONS = [];
 export const Dev: React.FC = () => <Harness actions={ACTIONS} />;
--- code/cell.modules/pkg.sys/ui.primitives/src/components/DragTarget/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './types';
-export { DragTarget, DragTargetProps } from './DragTarget';
