1c4ad426a443192fd1c1cdd9a67b3851076dbd46
Phil Cockfield <phil@cockfield.net>
2021-03-11 13:51:20 +1300

    dark prop

--- code/cell.modules/pkg.sys/ui.dev/src/test/sample-3/Component.tsx
+++ code/cell.modules/pkg.sys/ui.dev/src/test/sample-3/Component.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { color, css, CssValue, t } from '../../common';
 
-export type ComponentProps = { style?: CssValue };
+export type ComponentProps = { theme?: 'dark' | 'light'; style?: CssValue };
 
 export const Component: React.FC<ComponentProps> = (props) => {
+  const { theme = 'dark' } = props;
+  const isDark = theme === 'dark';
   const styles = {
     base: css({
       padding: 50,
-      color: color.format(1),
+      color: isDark ? color.format(1) : color.format(-0.6),
     }),
   };
   return <div {...css(styles.base, props.style)}>Sample-3</div>;
--- code/cell.modules/pkg.sys/ui.dev/src/test/sample-3/DEV.tsx
+++ code/cell.modules/pkg.sys/ui.dev/src/test/sample-3/DEV.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { DevActions } from '../..';
 import { Component, ComponentProps } from './Component';
-import { COLORS } from '../../common';
+import { COLORS, t } from '../../common';
 
-type Ctx = { props: ComponentProps };
+type Ctx = { theme: 'dark' | 'light' };
+
+const label = 'sample-3';
+const DARK: t.ActionHandlerSettingsArgs = {
+  layout: {
+    label,
+    border: 0.2,
+    cropmarks: 0.2,
+    background: 0.1,
+    labelColor: 0.5,
+  },
+  host: { background: COLORS.DARK },
+};
+
+const LIGHT: t.ActionHandlerSettingsArgs = {
+  layout: {
+    label,
+    border: -0.1,
+    cropmarks: -0.2,
+    background: 1,
+    labelColor: -0.5,
+  },
+  host: { background: -0.04 },
+};
 
 /**
  * Actions
@@ -12,25 +35,20 @@ export const actions = DevActions<Ctx>()
   .namespace('test/sample-3')
   .context((prev) => {
     if (prev) return prev;
-    return { props: {} };
+    return { theme: 'dark' };
   })
 
   .items((e) => {
-    //
+    e.title('theme');
+    e.button('dark', (e) => (e.ctx.theme = 'dark'));
+    e.button('light', (e) => (e.ctx.theme = 'light'));
+    e.hr();
   })
 
   .subject((e) => {
-    e.settings({
-      layout: {
-        border: 0.2,
-        cropmarks: 0.2,
-        background: 0.1,
-        labelColor: 0.5,
-        label: 'sample-3',
-      },
-      host: { background: COLORS.DARK },
-    });
-    e.render(<Component {...e.ctx.props} />);
+    const theme = e.ctx.theme;
+    e.settings(theme === 'dark' ? DARK : LIGHT);
+    e.render(<Component theme={theme} />);
   });
 
 export default actions;
