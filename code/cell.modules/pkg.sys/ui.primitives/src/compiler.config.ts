8f89776cca58ede4e93334f1de163dd85e849f23
Phil Cockfield <phil@cockfield.net>
2021-03-12 09:44:53 +1300

    clean up

--- code/cell.modules/pkg.sys/ui.primitives/src/compiler.config.ts
+++ code/cell.modules/pkg.sys/ui.primitives/src/compiler.config.ts
@@ -4,28 +4,16 @@ export default () =>
   Compiler.config()
     .namespace('sys.ui.primitives')
 
-    .variant(
-      'web',
-      (config) =>
-        config
-          .target('web')
-          .port(Package.compiler.port)
+    .variant('web', (config) =>
+      config
+        .target('web')
+        .port(Package.compiler.port)
 
-          .entry('main', './src/entry/main')
-          // .declarations('./src/**/*')
-          .static('static')
-          .files((e) => e.redirect(false, '*.worker.js').access('public', '**/*.{png,jpg,svg}'))
+        .entry('main', './src/entry/main')
+        // .declarations('./src/**/*')
+        .static('static')
+        .files((e) => e.redirect(false, '*.worker.js').access('public', '**/*.{png,jpg,svg}'))
 
-          .shared((e) => e.add(e.dependencies).singleton(['react', 'react-dom']))
-          .expose('./Dev', './src/Dev.Harness'),
-
-      // .expose('./DotTabstrip', './src/components/DotTabstrip/DotTabstrip')
-      // .expose('./Button', './src/components.ref/button/Button')
-      // .expose('./Switch', './src/components.ref/button/Switch')
-      // .expose('./image', './src/components.ref/image/image')
-      // .expose('./Avatar', './src/components.ref/image/Avatar')
-      // .expose('./Text', './src/components.ref/text/Text')
-      // .expose('./TextInput', './src/components.ref/text/TextInput')
-      // .expose('./Tree', './src/components.ref/tree/Tree')
-      // .expose('./Spinner', './src/components.ref/spinner/Spinner'),
+        .shared((e) => e.add(e.dependencies).singleton(['react', 'react-dom']))
+        .expose('./Dev', './src/Dev.Harness'),
     );
