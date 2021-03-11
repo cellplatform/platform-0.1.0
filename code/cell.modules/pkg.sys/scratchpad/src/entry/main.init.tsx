5771815da931730608818447116eb2de3c9796f9
Phil Cockfield <phil@cockfield.net>
2021-03-11 13:40:42 +1300

    clean up

--- code/cell.modules/pkg.sys/scratchpad/src/entry/main.init.tsx
+++ code/cell.modules/pkg.sys/scratchpad/src/entry/main.init.tsx
@@ -3,14 +3,7 @@ import '@platform/css/reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { DevHarness } from '../Dev.Harness';
-import { WebRuntime, log } from '../common';
-
-log.group('WebRuntime');
-log.info('module', WebRuntime.module);
-log.info('bundle', WebRuntime.bundle);
-log.groupEnd();
 
 const el = <DevHarness />;
 const root = <React.StrictMode>{el}</React.StrictMode>;
-
 ReactDOM.render(root, document.getElementById('root'));
