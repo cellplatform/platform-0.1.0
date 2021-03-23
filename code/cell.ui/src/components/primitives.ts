9a3945198f65d74ec7ff56c55be70801be53108d
Phil Cockfield <phil@cockfield.net>
2021-03-23 19:54:18 +1300

    types

--- code/cell.ui/src/components/primitives.ts
+++ code/cell.ui/src/components/primitives.ts
@@ -1,4 +1,4 @@
 export * from './Icons';
 
-export { Switch, ISwitchProps } from '@platform/ui.button/lib/components/Switch';
-export { Button, IButtonProps } from '@platform/ui.button/lib/components/Button';
+export { Switch, SwitchProps } from '@platform/ui.button/lib/components/Switch';
+export { Button, ButtonProps } from '@platform/ui.button/lib/components/Button';
