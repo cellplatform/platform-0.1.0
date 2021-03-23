fab43eac886d5b5f17468c572e5e1a8a643b6f1c
Phil Cockfield <phil@cockfield.net>
2021-03-23 19:35:42 +1300

    Clean up Icon

--- code/ui.icon/src/components/Icon/Icon.tsx
+++ code/ui.icon/src/components/Icon/Icon.tsx
@@ -1,59 +1,61 @@
 import * as React from 'react';
-import { mouse } from '@platform/react';
 import { css, color as colorUtil } from '@platform/css';
 import { t } from '../../common';
 
-type IReactIconBase = {
-  size: number;
-  color?: string;
-};
+type IReactIconBase = { size: number; color?: string };
 
-export type IconBase = React.StatelessComponent<IReactIconBase>;
-type IIconPropsInternal = t.IIconProps & {
+export type IconBase = React.FC<IReactIconBase>;
+type IconPropsInternal = t.IIconProps & {
   type: IconBase;
   tabIndex?: number;
   isGreyscale?: boolean;
 };
 
+export const Icon = {
+  renderer(type: IconBase): t.IIcon {
+    return (props: t.IIconProps) => <IconView type={type} {...props} />; // eslint-disable-line
+  },
+};
+
 /**
- * An individual SVG icon.
+ * Internal
  */
-export class Icon extends React.PureComponent<IIconPropsInternal> {
-  public static renderer = (type: IconBase): t.IIcon => {
-    return (props: t.IIconProps) => <Icon type={type} {...props} />;
+
+const IconView: React.FC<IconPropsInternal> = (props) => {
+  const { size = 24, opacity } = props;
+  const Component = props.type;
+
+  const styles = {
+    base: css({
+      display: 'inline-block',
+      opacity: opacity === undefined ? 1 : opacity,
+      width: size,
+      height: size,
+      overflow: 'hidden',
+    }),
   };
 
-  private mouse = mouse.fromProps(this.props);
+  return (
+    <div
+      {...css(styles.base, props.style)}
+      tabIndex={props.tabIndex}
+      onClick={props.onClick}
+      onDoubleClick={props.onDoubleClick}
+      onMouseDown={props.onMouseDown}
+      onMouseUp={props.onMouseUp}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
+    >
+      <Component size={size} color={formatColor(props)} />
+    </div>
+  );
+};
 
-  public get color() {
-    const { color = -0.4, isGreyscale } = this.props;
-    let result = colorUtil.format(color);
-    if (isGreyscale) {
-      result = colorUtil.create(result).greyscale().toHexString();
-    }
-    return result;
+function formatColor(props: IconPropsInternal) {
+  const { color = -0.4, isGreyscale } = props;
+  let result = colorUtil.format(color);
+  if (isGreyscale) {
+    result = colorUtil.create(result).greyscale().toHexString();
   }
-
-  public render() {
-    const { size = 24, opacity } = this.props;
-    const Icon = this.props.type;
-    const styles = {
-      base: css({
-        display: 'inline-block',
-        opacity: opacity === undefined ? 1 : opacity,
-        width: size,
-        height: size,
-        overflow: 'hidden',
-      }),
-    };
-    return (
-      <div
-        {...css(styles.base, this.props.style)}
-        tabIndex={this.props.tabIndex}
-        {...(this.mouse.events as any)}
-      >
-        <Icon size={size} color={this.color} />
-      </div>
-    );
-  }
+  return result;
 }
