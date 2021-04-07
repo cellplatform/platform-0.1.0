61d15e0b65943b47ce5c9162865061dd5a71383c
Phil Cockfield <phil@cockfield.net>
2021-04-08 10:41:28 +1200

    refactor

--- code/cell.modules/pkg.sys/net/src/PeerNetwork/dev/common.ts
+++ code/cell.modules/pkg.sys/net/src/PeerNetwork/dev/common.ts
@@ -1,16 +1,15 @@
-export * from '../common';
-
-export { Icons } from '../../components/Icons';
-export { Button, ButtonProps } from '@platform/ui.button';
 export { copyToClipboard } from '@platform/react';
+export { Button, ButtonProps } from '@platform/ui.button';
 
+export { ObjectView } from 'sys.ui.dev';
 export { Card } from 'sys.ui.primitives/lib/components/Card';
 export { CardStack, CardStackItem } from 'sys.ui.primitives/lib/components/CardStack';
 export { PropList, PropListItem } from 'sys.ui.primitives/lib/components/PropList';
 export { Hr } from 'sys.ui.primitives/lib/components/Hr';
-export { ObjectView } from 'sys.ui.dev';
-
-export { MediaStream, MediaEvent, VideoStream } from 'sys.ui.video/lib/components/MediaStream';
-export * from '../util';
 
 export { AudioWaveform } from 'sys.ui.video/lib/components/AudioWaveform';
+export { MediaStream, MediaEvent, VideoStream } from 'sys.ui.video/lib/components/MediaStream';
+
+export { Icons } from '../../components/Icons';
+export * from '../common';
+export * from '../util';
