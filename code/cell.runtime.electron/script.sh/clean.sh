e5c1ad0b328e0933cbe99549e3000cd3fd3003b0
Phil Cockfield <phil@cockfield.net>
2021-05-15 09:19:18 +1200

    rename folder

--- code/cell.runtime.electron/script.sh/clean.sh
+++ code/cell.runtime.electron/script.sh/clean.sh
@@ -3,9 +3,10 @@ rm -rf lib/
 rm -rf out/
 rm -rf tmp/
 
-
-rm -f  app/yarn-error.log
-rm -rf app/lib
-rm -rf app/.tmp.common_js
-rm -rf app/.tmp.es_module
-rm -rf app/tmp
+cd app.electron
+rm -f  yarn-error.log
+rm -rf lib
+rm -rf .tmp.common_js
+rm -rf .tmp.es_module
+rm -rf tmp
+cd ..
--- code/cell.runtime.electron/script.sh/nuke.sh
+++ code/cell.runtime.electron/script.sh/nuke.sh
@@ -4,5 +4,7 @@ echo
 
 sh script.sh/clean.sh
 
-rm -rf app/node_modules
-rm -f  app/yarn.lock
+cd app.electron
+rm -rf node_modules
+rm -f  yarn.lock
+cd ..
