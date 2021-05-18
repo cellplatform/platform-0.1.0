50dee6ad6a21146af8f6ce57a12844e1858bc38c
Phil Cockfield <phil@cockfield.net>
2021-05-19 08:24:54 +1200

    path

--- code/cell.modules/pkg.sys/net/src/common/types.ts
+++ code/cell.modules/pkg.sys/net/src/common/types.ts
@@ -4,6 +4,6 @@ export { PartialDeep, EventBus, JsonMap, Event, FireEvent, IDisposable } from '@
 export { IStateObject, IStateObjectWritable } from '@platform/state.types';
 export { MediaEvent } from 'sys.ui.video/lib/types';
 export { CssEdgesInput } from '@platform/css/lib/types';
-export { NetworkBus, NetworkBusFilter } from '@platform/cell.runtime/lib/NetworkBus/types';
+export { NetworkBus, NetworkBusFilter } from '@platform/cell.types/lib/types.Bus';
 
 export * from '../types';
