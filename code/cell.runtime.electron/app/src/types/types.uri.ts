fb8cdf1e25eb4fbe35013de233ea48d055492f62
Phil Cockfield <phil@cockfield.net>
2021-05-27 09:17:11 +1200

    URI types

---
+++ code/cell.runtime.electron/app/src/types/types.uri.ts
@@ -0,0 +1,17 @@
+import { t } from './common';
+
+export type ElectronUri = ElectronMainUri | ElectronWindowUri;
+export type ElectronMainUri = 'process:main';
+export type ElectronWindowUri = string; // "process:window:<id>"
+
+/**
+ * Parsed URI objects.
+ */
+export type ElectronUriObject = ElectronWindowUriObject;
+
+export type ElectronWindowUriObject = {
+  ok: boolean;
+  type: 'window';
+  id: t.ElectronWindowId;
+  errors: string[];
+};
