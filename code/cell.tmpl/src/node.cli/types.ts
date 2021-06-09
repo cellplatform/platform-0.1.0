3a455a6c5d14d36e4e3dd128f04735673bc3275f
Phil Cockfield <phil@cockfield.net>
2021-06-09 13:48:49 +1200

    Stub in new CLI structure for `cell.tmpl`

--- code/cell.tmpl/src/node.cli/types.ts
+++ code/cell.tmpl/src/node.cli/types.ts
@@ -1,5 +1,7 @@
 import { minimist } from './common';
 
+export * from '../types';
+
 export type Commands = Record<string, Command>;
 export type Command = { description: string; params: Record<string, string> };
 export type Argv = minimist.ParsedArgs;
--- code/cell.tmpl/templates/node.cli/types.ts
+++ code/cell.tmpl/templates/node.cli/types.ts
@@ -1,7 +1,5 @@
 import { minimist } from './common';
 
-export * from '../node/common/types';
-
 export type Commands = Record<string, Command>;
 export type Command = { description: string; params: Record<string, string> };
 export type Argv = minimist.ParsedArgs;
