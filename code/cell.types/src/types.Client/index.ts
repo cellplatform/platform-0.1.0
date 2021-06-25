d29ed7c8546982b863f94181f84219a71f83ec16
Phil Cockfield <phil@cockfield.net>
2021-06-26 10:41:16 +1200

    rename

--- code/cell.types/src/types.Client/index.ts
+++ code/cell.types/src/types.Client/index.ts
@@ -1,5 +1,5 @@
-export * from './types.Client.events';
 export * from './types.HttpClient';
+export * from './types.HttpClient.events';
 export * from './types.HttpClient.cell.links';
 export * from './types.HttpClient.cell.file';
-export * from './types.Client.typesystem';
+export * from './types.ClientTypesystem';
