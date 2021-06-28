723cddcf8a922a81cb77ee0fa41c675d5e82fa94
Phil Cockfield <phil@cockfield.net>
2021-06-28 13:03:33 +1200

    stub in module: ui.runtime

---
+++ code/cell.modules/pkg.sys/ui.runtime/src/compiler.config.ts
@@ -0,0 +1,24 @@
+import { Compiler, Package } from '@platform/cell.compiler';
+
+export default () =>
+  Compiler.config()
+    .namespace('ui.runtime')
+    .version(Package.version)
+
+    .variant('web', (config) =>
+      config
+        .target('web')
+        .port(Package.compiler.port)
+
+        .entry('main', './src/entry/dom')
+        .entry('service.worker', './src/workers/service.worker')
+        .entry('web.worker', './src/workers/web.worker')
+
+        .declarations('./src/**/*')
+
+        .static('static')
+        .files((e) => e.redirect(false, '*.worker.js').access('public', '**/*.{png,jpg,svg}'))
+
+        .expose('./Dev', './src/Dev.Harness')
+        .shared((e) => e.add(e.dependencies).singleton(['react', 'react-dom'])),
+    );
