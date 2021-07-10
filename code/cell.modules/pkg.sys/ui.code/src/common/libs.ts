2d5e9d85a2d06009d6b4ebf86f7d2ec6ed3d14db
Phil Cockfield <phil@cockfield.net>
2021-07-11 08:00:57 +1200

    ui.code: fix startup error

--- code/cell.modules/pkg.sys/ui.code/src/common/libs.ts
+++ code/cell.modules/pkg.sys/ui.code/src/common/libs.ts
@@ -2,7 +2,7 @@ import { equals } from 'ramda';
 export const R = { equals };
 
 export { css, CssValue, CssProps, color } from '@platform/css';
-export { WebRuntime } from '@platform/cell.runtime.web';
+export { WebRuntime } from '@platform/cell.runtime.web/lib/WebRuntime';
 export { http } from '@platform/http';
 export { log } from '@platform/log/lib/client';
 export { Uri, Schema } from '@platform/cell.schema';
