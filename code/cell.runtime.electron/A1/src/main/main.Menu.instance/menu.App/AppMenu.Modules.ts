936f91ed8bc08ed558bb889dd3065ab79eb66ecc
Phil Cockfield <phil@cockfield.net>
2021-07-11 19:31:05 +1200

    Open module window

--- code/cell.runtime.electron/A1/src/main/main.Menu.instance/menu.App/AppMenu.Modules.ts
+++ code/cell.runtime.electron/A1/src/main/main.Menu.instance/menu.App/AppMenu.Modules.ts
@@ -1,10 +1,11 @@
 import { filter } from 'rxjs/operators';
 
-import { Bundle, Menu, t, log } from '../common';
+import { Bundle, Menu, t, log, Window } from '../common';
 
 type Events = {
   menu: t.MenuEvents;
   bundle: t.BundleEvents;
+  window: t.WindowEvents;
 };
 
 /**
@@ -16,6 +17,7 @@ export function AppModulesMenuSection(args: { bus: t.EventBus<any> }): t.Menu {
   const events = {
     menu: Menu.Events({ bus }),
     bundle: Bundle.Events({ bus }),
+    window: Window.Events({ bus }),
   };
 
   const preferences = PreferencesMenuItem({ events });
@@ -28,6 +30,8 @@ export function AppModulesMenuSection(args: { bus: t.EventBus<any> }): t.Menu {
  * A1: Preferences
  */
 function PreferencesMenuItem(args: { events: Events }) {
+  const { events } = args;
+
   const id = 'sys.app.prefs';
   const item: t.MenuItem = {
     id,
@@ -49,13 +53,25 @@ function InstalledModulesItem(args: { events: Events }) {
   const { events } = args;
   const id = 'sys.app.modules.items';
 
+  const openWindow = async (url: string) => {
+    const res = await events.window.create.fire({
+      url,
+      devTools: true,
+      props: { width: 1200, height: 900 },
+    });
+    console.log('create/res:', res);
+  };
+
   const load = async () => {
     const list = await events.bundle.list.get();
 
     const items = list.items.map((module): t.MenuItem => {
       const { domain, namespace, version } = module;
+
+      const scope = domain.startsWith('runtime:electron') ? 'system' : 'domain';
+
       const id = `${domain}/${namespace}`;
-      const label = id;
+      const label = `Open: '${namespace}' (${scope})`;
 
       /**
        * Installed Module Item
@@ -64,10 +80,23 @@ function InstalledModulesItem(args: { events: Events }) {
       const item: t.MenuItem = {
         id,
         label,
-        type: 'checkbox',
-        click() {
+        type: 'normal',
+        async click() {
           //
           log.info('click', module);
+          // const {  } = module;
+          const info = await events.bundle.status.get({ domain, namespace, version });
+
+          if (info.error) log.error(`Failed to retrieve bundle info. ${info.error}`);
+
+          console.log('info', info);
+
+          const url = info.status?.urls.entry || '';
+          if (!url) log.error(`Failed to retieve module entry URL.`);
+          if (url) {
+            console.log('OPEN:', url);
+            openWindow(url);
+          }
           //
         },
       };
