1678117d52cec764ab9071ccb9cb74758dd93faf
Phil Cockfield <phil@cockfield.net>
2021-08-16 18:52:18 +1200

    Ref rename: IPosixFs

--- code/cell.types/src/common/types.ts
+++ code/cell.types/src/common/types.ts
@@ -1,7 +1,7 @@
 export { Observable, Subject } from 'rxjs';
 
 export * from '@platform/types';
-export * from '@platform/fs.types';
+export { IFs as IPosixFs } from '@platform/fs.types';
 export * from '@platform/http.types';
 
 export {
@@ -19,7 +19,9 @@ export {
 } from '@platform/state.types';
 
 export { TreeNodeIcon } from '@platform/ui.tree.types';
+
 export { IMemoryCache } from '@platform/cache/lib/types';
+
 export {
   S3StorageClass,
   S3SignedUrlGetObjectOptions,
