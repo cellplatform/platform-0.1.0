628e248bd5b4829e261f6c7038cadda3a14643f7
Phil Cockfield <phil@cockfield.net>
2021-08-28 11:29:50 +1200

    test: refactor

--- code/test/src/util.ts
+++ code/test/src/util.ts
@@ -1,32 +1,4 @@
 import { exec } from 'child_process';
-import { expect } from './libs';
-
-/**
- * Checks for an error within an async function.
- * Example:
- *    Return the result of this function to the test-runner (mocha).
- *
- *        it('should throw', () =>
- *            expectError(async () => {
- *              ! ...code that throws here...
- *          }, 'my error message'));
- *
- */
-export async function expectError(fn: () => Promise<any>, message?: string) {
-  try {
-    await fn();
-  } catch (error) {
-    if (message) {
-      return expect(error.message || '').to.contain(message);
-    } else {
-      return error;
-    }
-  }
-  const msg = message
-    ? `Should fail with error message '${message || ''}'`
-    : 'Should fail with error';
-  return expect(undefined).to.be.a('Error', msg);
-}
 
 /**
  * Better parameter order for setTimeout returning a promise.
