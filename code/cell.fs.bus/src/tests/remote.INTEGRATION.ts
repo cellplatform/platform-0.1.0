d135c80330986d5429731103e4cfce44e2443d49
Phil Cockfield <phil@cockfield.net>
2021-09-03 13:42:11 +1200

    Integration test for upload (S3, Uint8Array)

--- code/cell.fs.bus/src/tests/remote.INTEGRATION.ts
+++ code/cell.fs.bus/src/tests/remote.INTEGRATION.ts
@@ -1,11 +1,15 @@
-import { TestPrep, log } from '../test';
+import { TestPrep, log, HttpClient, expect } from '../test';
 
 describe('INTEGRATION', function () {
-  this.timeout(10000);
+  const timeout = 30000;
 
-  it('tmp', async () => {
-    // const one = await TestPrep({ id: 'one', dir: 'foo' });
+  this.timeout(timeout);
 
+  const uri = 'cell:ckt2dyzgq000008jx3664824f:A1';
+  const host = '5000';
+  const url = `${host}/${uri}/fs`;
+
+  it.only('via fs.bus', async () => {
     const mock = await TestPrep();
     const png = await mock.readFile('static.test/child/tree.png');
 
@@ -13,12 +17,28 @@ describe('INTEGRATION', function () {
     const fs = mock.controller.fs('drop-test');
     await fs.write('tree.png', png.data);
 
-    const remote = events.remote.cell('dev.db.team', 'cell:ckt2dyzgq000008jx3664824f:A1');
-    const res = await remote.push();
+    const remote = events.remote.cell(host, uri);
+    const res = await remote.push('/', { timeout });
+
+    log.info('-------------------------------------------');
+    log.info('res', res);
+    log.info(url);
+
+    await mock.dispose();
+  });
+
+  it('via http (client)', async () => {
+    const mock = await TestPrep();
+    const png = await mock.readFile('static.test/child/tree.png');
+    const data = png.data.buffer;
+
+    const client = HttpClient.create(host).cell(uri);
+    const res = await client.fs.upload({ filename: 'tree.png', data });
+    await mock.dispose();
 
     log.info('-------------------------------------------');
     log.info('res', res);
 
-    await mock.dispose();
+    expect(res.ok).to.eql(true);
   });
 });
