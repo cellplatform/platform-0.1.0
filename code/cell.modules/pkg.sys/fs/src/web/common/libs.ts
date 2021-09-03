5a4731a911c532bc0ddb49f0636a510f79b3e7bd
Phil Cockfield <phil@cockfield.net>
2021-09-04 09:51:29 +1200

    libs

--- code/cell.modules/pkg.sys/fs/src/web/common/libs.ts
+++ code/cell.modules/pkg.sys/fs/src/web/common/libs.ts
@@ -9,7 +9,7 @@ export { Hash, Uri, Schema } from '@platform/cell.schema';
 export { css, color, CssValue, formatColor } from '@platform/css';
 export { WebRuntime, bundle } from '@platform/cell.runtime.web';
 export { useResizeObserver } from '@platform/react';
-export { rx, slug } from '@platform/util.value';
+export { rx, slug, cuid, time } from '@platform/util.value';
 export { Filesystem, Stream } from '@platform/cell.fs.bus/lib/web';
 export { Path, PathUri } from '@platform/cell.fs';
 
