ae614cab66f3e52b2f186a3d5af1b82797ec3312
Phil Cockfield <phil@cockfield.net>
2021-09-11 10:26:23 +1200

    refs

---
+++ code/cell.modules/pkg.scratchpad/web/src/components/SlugProject/common/index.ts
@@ -0,0 +1,9 @@
+import * as t from './types';
+
+export * from './libs';
+export * from '../../../common';
+export { t };
+
+export const DEFAULT = {
+  MIN: { WIDTH: 400, HEIGHT: 400 },
+};
---
+++ code/cell.modules/pkg.scratchpad/web/src/components/SlugProject/common/libs.ts
@@ -0,0 +1,17 @@
+/**
+ * @system : sys.ui.primitives
+ */
+export { MinSize, MinSizeResizeEventHandler } from 'sys.ui.primitives/lib/ui/MinSize';
+export { MinSizeProperties } from 'sys.ui.primitives/lib/ui/MinSize/MinSize.Properties';
+export { useDragTarget } from 'sys.ui.primitives/lib/hooks/useDragTarget';
+export { Filesystem } from 'sys.fs';
+
+/**
+ * @system : sys.ui.video
+ */
+export { Vimeo } from 'sys.ui.video/lib/ui/Vimeo';
+
+/**
+ * @system : sys.runtime
+ */
+export { IpcBus } from 'sys.runtime.electron';
