2470c3256bf333baf0b547535030d515a52be928
Phil Cockfield <phil@cockfield.net>
2021-10-04 13:05:34 +1300

    namespace

--- code/cell.modules/pkg.vendor/cloud.pusher/src/compiler.config.ts
+++ code/cell.modules/pkg.vendor/cloud.pusher/src/compiler.config.ts
@@ -2,7 +2,7 @@ import { Compiler, Package } from '@platform/cell.compiler';
 
 export default () =>
   Compiler.config()
-    .namespace('cloud.pusher')
+    .namespace('vendor.cloud.pusher')
     .version(Package.version)
 
     .variant('web', (config) =>
