a16329a5c5be4abbdad4384c498021e13508144d
Phil Cockfield <phil@cockfield.net>
2021-10-14 14:00:47 +1300

    tmpl

--- code/cell.tmpl/templates/module.web/src/Compiler.config.ts
+++ code/cell.tmpl/templates/module.web/src/Compiler.config.ts
@@ -18,7 +18,7 @@ export default () =>
 
         .static('static')
         .files((e) => e.redirect(false, '*.worker.js').access('public', '**/*.{png,jpg,svg}'))
+        .shared((e) => e.add(e.dependencies).singleton(['react', 'react-dom']))
 
-        .expose('./Dev', './src/Dev.Harness')
-        .shared((e) => e.add(e.dependencies).singleton(['react', 'react-dom'])),
+        .expose('./Dev', './src/Dev.Harness'),
     );
