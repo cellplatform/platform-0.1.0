b53115482337f29cc1b8f0eecda24601e0c7ec87
Phil Cockfield <phil@cockfield.net>
2021-11-10 10:45:16 +1300

    stub in ui.runtime

---
+++ code/cell.modules/pkg.dns/ui.runtime/src/Dev.Harness.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Harness } from 'sys.ui.dev';
+
+const imports = {
+  Sample: import('./web.ui/Sample/dev/DEV'),
+};
+
+const url = new URL(location.href);
+const dev = url.searchParams.get('dev');
+
+export const DevHarness: React.FC = () => (
+  <Harness actions={Object.values(imports)} initial={dev} />
+);
+export default DevHarness;
---
+++ code/cell.modules/pkg.dns/ui.runtime/src/entry/main.ts
@@ -0,0 +1,25 @@
+import Worker from 'worker-loader?inline=no-fallback!../workers/web.worker';
+import { ServiceWorker } from '@platform/cell.runtime.web/lib/ServiceWorker';
+
+/**
+ * Web-worker.
+ */
+const worker = new Worker();
+worker.onmessage = (e: MessageEvent) => console.log('🌼 event (from worker thread)', e.data);
+setTimeout(() => worker.postMessage({ msg: 'Hello from [workers.init.ts]' }), 500);
+
+/**
+ * DOM (User Interface)
+ */
+export async function startDom() {
+  const init = import('./main.dom');
+  init.catch((err) => console.log('INIT ERROR 🐷', err));
+}
+
+/**
+ * Startup
+ */
+(async () => {
+  await ServiceWorker.start('./service.worker.js', { localhost: false });
+  await startDom();
+})();
