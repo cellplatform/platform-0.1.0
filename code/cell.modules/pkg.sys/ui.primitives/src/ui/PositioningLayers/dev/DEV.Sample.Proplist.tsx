d9641e8dffc26dbd39c18f77f0b5b8889c1729f8
Phil Cockfield <phil@cockfield.net>
2021-11-10 09:54:26 +1300

    refactor

--- code/cell.modules/pkg.sys/ui.primitives/src/ui/PositioningLayers/dev/DEV.Sample.Proplist.tsx
+++ code/cell.modules/pkg.sys/ui.primitives/src/ui/PositioningLayers/dev/DEV.Sample.Proplist.tsx
@@ -11,34 +11,40 @@ export type DevSampleProplistProps = {
 
 export const DevSampleProplist: React.FC<DevSampleProplistProps> = (props) => {
   const { info, id, overlaps } = props;
-
-  const items: PropListItem[] = (() => {
-    const index = info ? `${info.index}` : '?';
-    const list: PropListItem[] = [{ label: `layer-${index}`, value: `id:"${props.id}"` }];
-    if (info) {
-      const { position, size } = info;
-      list.push(
-        ...[
-          { label: 'position (x, y)', value: `${position.x} ${position.y}` },
-          { label: 'size', value: `x:${size.x} y:${size.y}, ${size.width} x ${size.height} px` },
-        ],
-      );
-
-      overlaps.forEach((overlap) => {
-        const { index, x, y } = overlap;
-        list.push({
-          label: `overlaps with: layer-${index}`,
-          value: `${x ? 'x' : ''}${y ? 'y' : ''}`,
-        });
-      });
-    }
-
-    return list;
-  })();
-
-  /**
-   * [Render]
-   */
-
+  const items = toList({ id, info, overlaps });
   return <PropList items={items} defaults={{ clipboard: false, monospace: true }} />;
 };
+
+/**
+ * [Helpers]
+ */
+
+function toList(args: {
+  id: string;
+  info?: t.PositioningLayerInfo;
+  overlaps: t.PositioningLayerOverlapInfo[];
+}) {
+  const { id, info, overlaps } = args;
+  const index = info ? `${info.index}` : '?';
+  const list: PropListItem[] = [{ label: `layer-${index}`, value: `id:"${id}"` }];
+
+  if (info) {
+    const { position, size } = info;
+    list.push(
+      ...[
+        { label: 'position (x, y)', value: `${position.x} ${position.y}` },
+        { label: 'size', value: `x:${size.x} y:${size.y}, ${size.width} x ${size.height} px` },
+      ],
+    );
+
+    overlaps.forEach((overlap) => {
+      const { index, x, y } = overlap;
+      list.push({
+        label: `overlaps with: layer-${index}`,
+        value: `${x ? 'x' : ''}${y ? 'y' : ''}`,
+      });
+    });
+  }
+
+  return list;
+}
