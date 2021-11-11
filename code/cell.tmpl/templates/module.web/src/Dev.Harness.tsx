d8edde87afb9e934f4af087f7dd4eb9bc62a6010
Phil Cockfield <phil@cockfield.net>
2021-11-12 10:32:26 +1300

    template - default export

--- code/cell.tmpl/templates/module.web/src/Dev.Harness.tsx
+++ code/cell.tmpl/templates/module.web/src/Dev.Harness.tsx
@@ -5,10 +5,12 @@ const imports = {
   Sample: import('./web.ui/Sample/dev/DEV'),
 };
 
+/**
+ * UI Harness (Dev)
+ */
 const url = new URL(location.href);
 const dev = url.searchParams.get('dev');
+const actions = Object.values(imports);
 
-export const DevHarness: React.FC = () => (
-  <Harness actions={Object.values(imports)} initial={dev} />
-);
+export const DevHarness: React.FC = () => <Harness actions={actions} initial={dev} />;
 export default DevHarness;
