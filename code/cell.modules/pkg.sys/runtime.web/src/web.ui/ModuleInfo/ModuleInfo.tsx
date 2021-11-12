b99f9bfefcab694f260ef4d69d4351e75b4a5d95
Phil Cockfield <phil@cockfield.net>
2021-11-12 15:48:30 +1300

    Button

--- code/cell.modules/pkg.sys/runtime.web/src/web.ui/ModuleInfo/ModuleInfo.tsx
+++ code/cell.modules/pkg.sys/runtime.web/src/web.ui/ModuleInfo/ModuleInfo.tsx
@@ -49,16 +49,22 @@ function toList(manifest?: t.ModuleManifest): PropListItem[] {
 
   const elapsed = time.duration(time.now.timestamp - module.compiledAt);
   const total = manifest.files.length;
+  const noFiles = total === 0;
   const bytes = manifest.files.reduce((acc, next) => acc + next.bytes, 0);
   const filesize = bytes === 0 ? '' : Filesize(bytes);
   const files = `${total} ${value.plural(total, 'file', 'files')}`;
+
+  const trimHashPrefix = (hash: string) => hash.replace(/^sha256-/, '');
 
   const list: PropListItem[] = [
     { label: 'namespace', value: module.namespace },
     {
       label: 'version',
-      value: module.version,
-      tooltip: `module hash: ${manifest.hash.module}`,
+      value: {
+        data: `${module.version}`,
+        clipboard: `Version: ${module.version}\nModule Hash:\n${manifest.hash.module}`,
+      },
+      tooltip: `Module Hash (SHA256):\n${trimHashPrefix(manifest.hash.module)}`,
     },
     {
       label: 'compiled',
@@ -66,13 +72,28 @@ function toList(manifest?: t.ModuleManifest): PropListItem[] {
       tooltip: module.compiler,
     },
     { label: 'kind', value: `${module.target}, ${module.mode}` },
-    { label: 'files', value: total === 0 ? 'none' : `${filesize} (${files})`.trim() },
+    {
+      label: 'files',
+      value: {
+        data: noFiles ? 'none' : `${filesize} (${files})`.trim(),
+        clipboard: () => {
+          if (noFiles) return 'No files.';
+          const lines = manifest.files.map((file) => `- ${file.path} (${Filesize(file.bytes)})`);
+          let msg = `${files} (${filesize})`;
+          msg = `${msg}\nHash: ${manifest.hash.files}\n\n`;
+          msg = `${msg}${lines.join('\n')}`;
+          msg = `${msg}\n`;
+          return msg;
+        },
+      },
+      tooltip: noFiles ? undefined : `Files Hash (SHA256):\n${trimHashPrefix(manifest.hash.files)}`,
+    },
   ];
 
   if (module.remote?.exports.length || 0 > 0) {
     const total = module.remote?.exports.length || 0;
     const plural = value.plural(total, 'export', 'exports');
-    list.push({ label: 'remote', value: total === 0 ? 'none' : `${total} ${plural}` });
+    list.push({ label: 'remote', value: noFiles ? 'none' : `${total} ${plural}` });
   }
 
   return list;
--- code/cell.modules/pkg.sys/ui.primitives/src/ui/PositioningLayers/dev/DEV.tsx
+++ code/cell.modules/pkg.sys/ui.primitives/src/ui/PositioningLayers/dev/DEV.tsx
@@ -59,8 +59,11 @@ export const actions = DevActions<Ctx>()
     e.button('insert: center bottom', (e) => insert(e.ctx, { x: 'center', y: 'bottom' }));
     e.button('insert: right center', (e) => insert(e.ctx, { x: 'right', y: 'center' }));
     e.button('insert: bottom right', (e) => insert(e.ctx, { x: 'right', y: 'bottom' }));
+    e.hr(1, 0.1);
+    e.button('insert: full screen', (e) => insert(e.ctx, { x: 'stretch', y: 'stretch' }));
 
     e.hr(1, 0.1);
+
     e.button('clear', (e) => {
       e.ctx.props.layers = undefined;
       e.ctx.debug.current = undefined;
