c82c9de0253ce48290023d8c8add79379e682c1f
Phil Cockfield <phil@cockfield.net>
2021-11-14 09:31:01 +1300

    folder rename

--- code/cell.modules/pkg.dns/web/src/Compiler.config.ts
+++ code/cell.modules/pkg.dns/web/src/Compiler.config.ts
@@ -2,7 +2,7 @@ import { Compiler, Package } from '@platform/cell.compiler';
 
 export default () =>
   Compiler.config()
-    .namespace('dns.ui')
+    .namespace('dns.web')
     .version(Package.version)
 
     .variant('web', (config) =>
