af3402ff2220bf1b12eb8cb5451a419ad327efd4
Phil Cockfield <phil@cockfield.net>
2022-01-08 13:35:42 +1300

    Unit test refactor

--- code/cell.modules/pkg.sys/crdt/src/web.ui/dev/UnitTests/DEV.UnitTests.tsx
+++ code/cell.modules/pkg.sys/crdt/src/web.ui/dev/UnitTests/DEV.UnitTests.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DevActions, Test } from 'sys.ui.dev';
 import { TestSuiteRunResponse, TestSuiteModel } from 'sys.ui.dev/lib/types';
-import { RepoLink } from './RepoLink';
+import { RepoLink } from './ui/RepoLink';
 
 type CtxRunTests = () => Promise<TestSuiteRunResponse>;
 
@@ -9,6 +9,7 @@ type Ctx = {
   results?: TestSuiteRunResponse;
   tests: {
     Automerge: CtxRunTests;
+    AutomergeDoc: CtxRunTests;
     CrdtBus: CtxRunTests;
   };
 };
@@ -29,7 +30,10 @@ export const actions = DevActions<Ctx>()
 
     const tests: Ctx['tests'] = {
       async Automerge() {
-        return run(await Test.bundle(import('../../../web.Automerge/Automerge.TEST')));
+        return run(await Test.bundle(import('../../../web.Automerge/Automerge.lib.TEST')));
+      },
+      async AutomergeDoc() {
+        return run(await Test.bundle(import('../../../web.Automerge/AutomergeDoc.TEST')));
       },
       async CrdtBus() {
         return run(await Test.bundle(import('../../../web.CrdtBus/TEST')));
@@ -38,20 +42,24 @@ export const actions = DevActions<Ctx>()
 
     const ctx: Ctx = { tests };
 
-    // tests.Automerge(); // Auto-run on load.
-    tests.CrdtBus(); // Auto-run on load.
+    // Auto-run on load.
+    tests.Automerge();
+    // tests.AutomergeDoc();
+    // tests.CrdtBus();
 
     return ctx;
   })
 
   .items((e) => {
     e.title('Run Tests');
-
-    e.button('run: Automerge (baseline)', (e) => e.ctx.tests.Automerge());
     e.component((e) => {
-      return <RepoLink url={'https://github.com/automerge/automerge'} marginLeft={75} />;
+      return <RepoLink url={'https://github.com/automerge/automerge'} marginLeft={15} />;
     });
 
+    e.hr(1, 0.1);
+
+    e.button('run: Automerge (baseline)', (e) => e.ctx.tests.Automerge());
+    e.button('run: AutomergeDoc (helpers)', (e) => e.ctx.tests.AutomergeDoc());
     e.button('run: CrdtBus', (e) => e.ctx.tests.CrdtBus());
 
     e.hr();
