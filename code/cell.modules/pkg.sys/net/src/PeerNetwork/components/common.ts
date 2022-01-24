0f1d3a3b244ae9ef4b0dcbcb9d7fa4eeb6b6c7b9
Phil Cockfield <phil@cockfield.net>
2022-01-25 08:01:16 +1300

    ref fix

--- code/cell.modules/pkg.sys/net/src/PeerNetwork/components/common.ts
+++ code/cell.modules/pkg.sys/net/src/PeerNetwork/components/common.ts
@@ -7,4 +7,4 @@ export { Icons } from '../../web.ui/Icons';
 export { PropList, PropListItem } from 'sys.ui.primitives/lib/ui/PropList';
 export { Hr } from 'sys.ui.primitives/lib/ui/Hr';
 export { Button, ButtonProps } from 'sys.ui.primitives/lib/ui.ref/button/Button';
-export { Textbox } from 'sys.ui.dev/lib/ui/Textbox';
+export { Textbox } from 'sys.ui.dev/lib/web.ui/Textbox';
