814ff9913692dd15a2f015fabb642b209e2142ba
Phil Cockfield <phil@cockfield.net>
2022-02-11 10:49:02 +1300

    net: common toolbar

---
+++ code/cell.modules/pkg.sys/net/src/web.ui/primitives/Toolbar.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { color, css, CssValue, t, Style } from '../../common';
+
+export type ToolbarProps = {
+  children?: React.ReactNode;
+  edge?: 'N' | 'S';
+  padding?: t.CssEdgesInput;
+  style?: CssValue;
+};
+
+export const Toolbar: React.FC<ToolbarProps> = (props) => {
+  const { edge = 'N', padding = 8 } = props;
+  const border = `solid 1px ${color.format(-0.08)}`;
+
+  /**
+   * [Render]
+   */
+  const styles = {
+    base: css({
+      boxSizing: 'border-box',
+      Flex: `x-spaceBetween-center`,
+      borderTop: edge === 'S' ? border : undefined,
+      borderBottom: edge === 'N' ? border : undefined,
+      backgroundColor: color.format(-0.03),
+      color: color.format(-0.6),
+      borderRadius: edge === 'N' ? [2, 2, 0, 0] : [0, 0, 2, 2],
+      ...Style.toPadding(padding),
+    }),
+  };
+  return <div {...css(styles.base, props.style)}>{props.children}</div>;
+};
