22663160aa277d98406da217e04ae0139f037890
Phil Cockfield <phil@cockfield.net>
2022-03-08 08:35:41 +1300

    FC.decorate - "displayName"

--- code/react/src/FC/FC.TEST.tsx
+++ code/react/src/FC/FC.TEST.tsx
@@ -2,7 +2,7 @@ import { expect } from '../test';
 import React from 'react';
 import { FC } from '.';
 
-describe('FC (Functional Component)', () => {
+describe.only('FC (Functional Component)', () => {
   describe('FC.decorate', () => {
     it('assigns fields', () => {
       type FooProps = { count?: number };
@@ -14,6 +14,20 @@ describe('FC (Functional Component)', () => {
 
       expect(Foo).to.equal(View);
       expect(Foo.helper).to.eql(helper);
+    });
+
+    it('displayName', () => {
+      type FooProps = { count?: number };
+      const View1: React.FC<FooProps> = (props) => <div />;
+      const View2: React.FC<FooProps> = (props) => <div />;
+
+      type F = { helper: () => boolean };
+      const helper = () => true;
+      const Foo1 = FC.decorate<FooProps, F>(View1, { helper });
+      const Foo2 = FC.decorate<FooProps, F>(View2, { helper }, { displayName: 'FooBar' });
+
+      expect(Foo1.displayName).to.eql(undefined);
+      expect(Foo2.displayName).to.eql('FooBar');
     });
   });
 });
