730b4bd8c3067b714c7116671f675f8ce1f7b27b
Phil Cockfield <phil@cockfield.net>
2022-03-09 10:33:02 +1300

    net: loader swap out query string "d" => "dev"

--- code/cell.modules/pkg.sys/net/src/entry/main.dom.tsx
+++ code/cell.modules/pkg.sys/net/src/entry/main.dom.tsx
@@ -5,6 +5,19 @@ import ReactDOM from 'react-dom';
 import { DevHarness } from '../Dev.Harness';
 import { DevSampleApp } from '../web.ui/dev.Networks/DEV.Sample.App';
 
-const url = new URL(location.href);
-const el = url.searchParams.has('dev') ? <DevHarness /> : <DevSampleApp />;
+const query = () => {
+  const url = new URL(location.href);
+  const q = url.searchParams;
+  if (q.has('dev')) return q;
+
+  if (q.has('d')) {
+    q.set('dev', q.get('d') || '');
+    q.delete('d');
+    window.history.pushState({}, '', url);
+  }
+
+  return q;
+};
+
+const el = query().has('dev') ? <DevHarness /> : <DevSampleApp />;
 ReactDOM.render(el, document.getElementById('root'));
