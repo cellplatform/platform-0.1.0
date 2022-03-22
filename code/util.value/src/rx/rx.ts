a3eb3843c6d0afd65c2fd7a9ca05b8aa7b1f4031
Phil Cockfield <phil@cockfield.net>
2022-03-23 10:18:40 +1300

    rx: Export Observable

--- code/util.value/src/rx/rx.ts
+++ code/util.value/src/rx/rx.ts
@@ -1,3 +1,6 @@
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+export { Observable, Subject, BehaviorSubject };
+
 export * from './rx.bus';
 export * from './rx.debounce';
 export * from './rx.disposable';
