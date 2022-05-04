37f35b8489c185f89ce542f4eda3ecad646a11fb
Phil Cockfield <phil@cockfield.net>
2022-05-05 10:29:10 +1200

    vendor.vercel - fix refs and refactor

--- code/cell.modules/pkg.vendor/cloud.vercel/src/Dev.Harness.tsx
+++ code/cell.modules/pkg.vendor/cloud.vercel/src/Dev.Harness.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Harness } from 'sys.ui.dev';
+import { t } from './web/common';
 
 const imports = {
   Foo: import('./web.ui/Foo/dev/DEV'),
 };
 
-const dev = new URL(location.href).searchParams.get('dev');
+/**
+ * UI Harness (Dev)
+ */
+type Props = { bus?: t.EventBus };
 
-export const DevHarness: React.FC = () => (
-  <Harness actions={Object.values(imports)} initial={dev} />
-);
+export const DevHarness: React.FC<Props> = (props) => {
+  const url = new URL(location.href);
+  return (
+    <Harness
+      bus={props.bus}
+      actions={Object.values(imports)}
+      showActions={url.hostname === 'localhost'}
+    />
+  );
+};
+
+export default DevHarness;
--- code/cell.modules/pkg.vendor/cloud.vercel/src/web/common/index.ts
+++ code/cell.modules/pkg.vendor/cloud.vercel/src/web/common/index.ts
@@ -4,6 +4,3 @@ export { t };
 export * from './libs';
 export * from './colors';
 export * from './constants';
-
-import { WebRuntime } from './libs';
-export const bundle = WebRuntime.bundle;
