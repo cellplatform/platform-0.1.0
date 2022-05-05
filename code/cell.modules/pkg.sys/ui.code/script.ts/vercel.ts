c84a1360b4d84cc71a8e407b0c3d505b69cf743e
Phil Cockfield <phil@cockfield.net>
2022-05-06 08:43:03 +1200

    refactor

--- code/cell.modules/pkg.sys/ui.code/script.ts/vercel.ts
+++ code/cell.modules/pkg.sys/ui.code/script.ts/vercel.ts
@@ -12,22 +12,23 @@ const token = process.env.VERCEL_TEST_TOKEN;
  */
 export async function deploy(team: string, project: string, alias: string) {
   const deployment = Vercel.Deploy({ token, dir: 'dist/web', team, project });
-  await deployment.ensureProject(project);
 
-  const manifest = await deployment.manifest<t.ModuleManifest>();
-
-  console.log('\ndeploying:');
-  console.log(' • module:', manifest.hash.module);
-  console.log(' • files: ', `${manifest.files.length} files`);
+  const info = await deployment.info();
+  console.log();
+  console.log('deploying:');
+  console.log(' • hash:  ', info.files.hash);
+  console.log(' • size:  ', info.files.toString());
   console.log(' • alias: ', alias);
   console.log();
 
-  const wait = deployment.commit({
-    target: 'production',
-    regions: ['sfo1'],
-    alias,
-  });
-  const res = await wait;
+  const res = await deployment.commit(
+    {
+      target: 'production',
+      regions: ['sfo1'],
+      alias,
+    },
+    { ensureProject: true },
+  );
   const status = res.status;
   const name = res.deployment.name;
 
