e30eeff2596a485e74097cf9fd75a0b816715b29
Phil Cockfield <phil@cockfield.net>
2022-05-06 08:46:36 +1200

    refactor - types file

---
+++ code/cell.modules/pkg.sys/ui.code/src/types.ts
@@ -0,0 +1,9 @@
+export * from './types.editor';
+export * from './types.monaco';
+export * from './ui/DevEnv/types';
+
+/**
+ * Environment
+ */
+import * as env from '@platform/cell.types/lib/types.Runtime';
+export type Global = env.Global;
--- code/cell.modules/pkg.sys/ui.code/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '../types.editor';
-export * from '../types.monaco';
-
-export * from '../ui/DevEnv/types';
