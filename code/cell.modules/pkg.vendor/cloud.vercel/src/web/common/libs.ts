9cbb23a23e80bb961446dfa1d12b124fdf87928f
Phil Cockfield <phil@cockfield.net>
2022-05-20 08:54:27 +1200

    fix refs

--- code/cell.modules/pkg.vendor/cloud.vercel/src/web/common/libs.ts
+++ code/cell.modules/pkg.vendor/cloud.vercel/src/web/common/libs.ts
@@ -15,7 +15,7 @@ export { Mime } from '@platform/util.mimetype';
  */
 export { WebRuntime } from 'sys.runtime.web.ui';
 export { Path, Filesize } from 'sys.fs/lib';
-export { Filesystem } from 'sys.fs/lib/web.FsBus.IndexedDb';
+export { Filesystem } from 'sys.fs/lib/web/FsBus.IndexedDb';
 export { PropList } from 'sys.ui.primitives/lib/ui/PropList';
 export { Text } from 'sys.ui.primitives/lib/ui/Text';
 export { Chip } from 'sys.ui.primitives/lib/ui/Chip';
