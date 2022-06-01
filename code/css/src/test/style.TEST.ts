bf24405a2740be324c2a87706f8fafa3e936e8b1
Phil Cockfield <phil@cockfield.net>
2022-06-01 16:33:30 +1200

    css: support pseudo-types

--- code/css/src/test/style.TEST.ts
+++ code/css/src/test/style.TEST.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { style, css } from '..';
+import { Style, css } from '..';
 
 describe('css', () => {
   it('is a function', () => {
@@ -10,33 +10,48 @@ describe('css', () => {
 describe('style', () => {
   describe('format', () => {
     it('is a function', () => {
-      expect(style.format).to.be.an.instanceof(Function);
+      expect(Style.format).to.be.an.instanceof(Function);
     });
   });
 
   describe('transform', () => {
     it('is a function', () => {
-      expect(style.transform).to.be.an.instanceof(Function);
+      expect(Style.transform).to.be.an.instanceof(Function);
     });
 
     it('returns the given object', () => {
       const input = { color: 'red' };
-      expect(style.transform(input)).to.equal(input);
+      expect(Style.transform(input)).to.equal(input);
     });
 
     it('returns an empty object if no `style` parameter is given', () => {
-      expect(style.transform()).to.eql({});
+      expect(Style.transform()).to.eql({});
     });
 
     it('removes undefined values', () => {
       const input = { color: undefined, background: null };
-      expect(style.transform(input)).to.eql({});
+      expect(Style.transform(input)).to.eql({});
     });
   });
 
   describe('head', () => {
     it('head', () => {
-      expect(style.head.importStylesheet).to.be.an.instanceof(Function);
+      expect(Style.head.importStylesheet).to.be.an.instanceof(Function);
+    });
+  });
+
+  describe('global', () => {
+    it('global: psudo-elements', () => {
+      Style.global({
+        div: {
+          color: 'red',
+          ':first-child': { color: 'blue' },
+          ':last-child': { color: 'green' },
+          ':focus': { color: 'green' },
+          ':hover': { color: 'green' },
+          ':visited': { color: 'green' },
+        },
+      });
     });
   });
 });
