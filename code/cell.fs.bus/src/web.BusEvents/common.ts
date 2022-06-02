61b228b4c4ee4b7c78273df9194c91e2ee8c06f8
Phil Cockfield <phil@cockfield.net>
2022-06-02 15:54:05 +1200

    FS: Increase default timeout

--- code/cell.fs.bus/src/web.BusEvents/common.ts
+++ code/cell.fs.bus/src/web.BusEvents/common.ts
@@ -2,6 +2,6 @@ export * from '../web/common';
 
 export const timeoutWrangler = (defaultTimeout?: number) => {
   return (options: { timeout?: number } = {}) => {
-    return options.timeout ?? defaultTimeout ?? 3000;
+    return options.timeout ?? defaultTimeout ?? 20000;
   };
 };
