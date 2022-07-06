d326ea865e670577e146b9c867681cce247ba932
Phil Cockfield <phil@cockfield.net>
2022-07-07 09:47:03 +1200

    Stub in CrdtFile test view

---
+++ code/cell.modules/pkg.sys/data.crdt/src/Crdt/Crdt.ts
@@ -0,0 +1,7 @@
+import { CrdtBus as Bus } from '../Crdt.Bus';
+import { CrdtFile as File } from '../Crdt.File';
+
+export const Crdt = {
+  Bus,
+  File,
+};
--- code/cell.modules/pkg.sys/data.crdt/src/ui/common.ts
+++
@@ -1,3 +0,0 @@
-export * from '../common';
-export { Icons } from './Icons';
-export { Filesystem } from 'sys.fs/lib/web/ui';
