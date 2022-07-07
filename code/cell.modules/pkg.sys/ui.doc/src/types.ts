7bbbd04a396bc69da8598f05ca327f502232eba9
Phil Cockfield <phil@cockfield.net>
2022-07-07 13:33:28 +1200

    move types files

---
+++ code/cell.modules/pkg.sys/ui.doc/src/types.ts
@@ -0,0 +1,12 @@
+export * from './ui/Font/types';
+
+export * from './ui/Doc.Blocks/types';
+export * from './ui/Doc.LayoutContainer/types';
+export * from './ui/Doc.List/types';
+export * from './ui/Doc/types';
+
+export * from './ui/Diagram.TalkingDiagram/types';
+
+export * from './ui/Route.Bus/types';
+export * from './ui/Route.Table/types';
+export * from './ui/Route.View/types';
--- code/cell.modules/pkg.sys/ui.doc/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export * from '../ui/Font/types';
-export * from '../ui/Doc.Blocks/types';
-export * from '../ui/Doc.LayoutContainer/types';
-export * from '../ui/Doc.List/types';
-export * from '../ui/Doc/types';
-
-export * from '../ui/Route.Bus/types';
-export * from '../ui/Route.Table/types';
-export * from '../ui/Route.View/types';
