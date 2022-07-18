68a6a9b8044e4a175d7a4057e417451ab7363c89
Phil Cockfield <phil@cockfield.net>
2022-07-19 10:54:12 +1200

    cache matching

--- code/cell.modules/pkg.sys/runtime.web.ui/src/workers/worker.service.cache.ts
+++ code/cell.modules/pkg.sys/runtime.web.ui/src/workers/worker.service.cache.ts
@@ -1,10 +1,13 @@
 export function isCacheable(url: URL) {
+  const path = url.pathname;
+
   /**
-   * Cache matching for a compiled module bundle.
+   * Cache matching for a compiled [module] bundle.
    */
+  if (path.startsWith('/sockjs-node')) return false; // NB: turnes up when running the local "dev" build server (HMR).
 
-  if (url.pathname.startsWith('/sockjs-node')) return false;
-  if (!url.pathname.endsWith('.js')) return false;
+  const extensions = ['.js', '.ttf', '.woff', '.woff2', '.ico'];
+  if (extensions.some((ext) => path.endsWith(ext))) return true;
 
-  return true;
+  return false;
 }
