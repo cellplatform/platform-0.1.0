f2b11de97719123100b373f7848ee4ef05c02923
Phil Cockfield <phil@cockfield.net>
2022-07-18 19:14:05 +1200

    remove old code ref

--- code/cell.tmpl/templates/module.web/src/workers/worker.service.ts
+++ code/cell.tmpl/templates/module.web/src/workers/worker.service.ts
@@ -1,4 +1,1 @@
 const ctx: ServiceWorker = self as any;
-
-// import { BundleCache } from '@platform/cell.runtime.web/lib/BundleCache';
-// BundleCache.serviceWorker(self, { log: 'verbose', localhost: false });
