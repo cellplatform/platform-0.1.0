33798f943b9c2e275d8aade3f3f992f59128ac42
Phil Cockfield <phil@cockfield.net>
2022-07-21 08:08:57 +1200

    WIP environment configuration

--- code/cell.modules/pkg.sys/ui.code/src/api/CodeEditor.ts
+++ code/cell.modules/pkg.sys/ui.code/src/api/CodeEditor.ts
@@ -1,9 +1,10 @@
 import { loader } from '@monaco-editor/react';
 
-import { configure } from './config';
-import { t, LANGUAGES } from '../common';
+import { Configure } from './Configure';
+import { t, LANGUAGES } from './common';
 import { CodeEditorSingleton } from './singleton';
 import { Events } from './event';
+import { staticPaths } from './Configure/Configure.paths';
 
 let singleton: Promise<t.ICodeEditorSingleton>;
 
@@ -11,29 +12,45 @@ let singleton: Promise<t.ICodeEditorSingleton>;
  * Static entry points.
  */
 export const CodeEditor = {
+  Configure,
   languages: LANGUAGES,
 
   /**
    * The singleton instance of monaco-editor logical controller (API).
    */
-  singleton(bus: t.EventBus<any>) {
+  singleton(args: { bus: t.EventBus<any>; staticRoot?: string }) {
     if (singleton) {
       return singleton;
     }
 
     return (singleton = new Promise<t.ICodeEditorSingleton>(async (resolve, reject) => {
       try {
-        // Initialize.
+        const { bus, staticRoot } = args;
+        const paths = staticPaths(args.staticRoot);
+
+        /**
+         * Ensure the environment is configured.
+         *
+         * NOTE:     If any custom configuration parameters need to be expressed
+         *           call this function seperately BEFORE the first instantiation of the
+         *           of the [CodeEditor.singleton()] method.
+         */
+        Configure.env({ staticRoot });
+
+        // Initialize editor.
         const editor = await loader.init();
         const monaco = editor as unknown as t.IMonaco;
         const api = CodeEditorSingleton({ bus, monaco });
 
         // Run configuration routines.
         await Promise.all([
-          configure.defineThemes(api),
-          configure.registerLanguage(api),
-          configure.registerPrettier(api),
+          Configure.defineThemes({ api }),
+          Configure.registerLanguage({ api }),
+          Configure.registerPrettier({ api }),
         ]);
+
+        // Register language types.
+        await api.libs.fromNetwork(paths.types.es);
 
         // Finish up.
         resolve(api);
