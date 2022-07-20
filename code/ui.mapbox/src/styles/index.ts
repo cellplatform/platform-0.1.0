79ecb6c6137a9b0b1613bd03b033b4bab23a12ad
Phil Cockfield <phil@cockfield.net>
2022-07-21 08:22:22 +1200

    archive: ui.mapbox

--- code/ui.mapbox/src/styles/index.ts
+++
@@ -1,8 +0,0 @@
-import { style } from '../common';
-
-/**
- * Docs:
- *    https://docs.mapbox.com/mapbox-gl-js/overview/
- */
-const URL = 'https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css';
-style.head.importStylesheet(URL);
