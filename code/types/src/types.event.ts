9974e95f37611c37413b20e2e480c69dfd1d8122
Phil Cockfield <phil@cockfield.net>
2022-07-26 10:01:26 +1200

    Delete bus.clone (never used, updated to EventPump)

--- code/types/src/types.event.ts
+++ code/types/src/types.event.ts
@@ -16,6 +16,30 @@ export type FireEvent<E extends Event = Event> = (event: E) => void;
  * An structure that exposes an observable and can fire events.
  */
 export type EventBus<E extends Event = Event> = {
-  $: Observable<E>;
+  readonly $: Observable<E>;
   fire: FireEvent<E>;
 };
+
+/**
+ * A two-way message pump that can ferry events IN and OUT between buses.
+ * NOTE:
+ *    This is a useful way of reducing Observable libraries (eg "rxjs") down to
+ *    the simple functions, a bridging between them, ensuring competing
+ *    versions of the same library do not fight with each other.
+ */
+export type EventPump<E extends Event = Event> = {
+  id: string;
+  in: EventPumpIn<E>;
+  out: EventPumpOut<E>;
+};
+
+/**
+ * Recieves messages/events inward from the network.
+ */
+export type EventPumpIn<E extends Event> = (fn: EventPumpInSubscriber<E>) => void;
+export type EventPumpInSubscriber<T extends Event> = (e: T) => void;
+
+/**
+ * Pumps messages/events outward into the network.
+ */
+export type EventPumpOut<E extends Event> = (e: E) => void;
--- code/util.value/src/rx/rx.bus.ts
+++ code/util.value/src/rx/rx.bus.ts
@@ -1,26 +1,18 @@
 import * as t from '@platform/types';
 import { is } from '@platform/util.is';
-import { Subject, Observable } from 'rxjs';
-import { filter, takeWhile } from 'rxjs/operators';
-import { isEvent } from './rx.event';
+import { Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
 import { id } from '../id';
+import { isEvent } from './rx.event';
 
 type E = t.Event;
-
-type BusClone = <T extends E = E>(
-  source: t.EventBus<any>,
-  config?: BusCloneDuplex<T> | BusCloneDuplexConfig<T>,
-) => t.EventBus<T>;
-type BusClonePipe<T extends E> = ($: Observable<T>) => Observable<T>;
-type BusCloneDuplex<T extends E> = BusClonePipe<T>; // NB: Same filter both ways.
-type BusCloneDuplexConfig<T extends E> = { source: BusClonePipe<T>; target: BusClonePipe<T> };
 
 type BusFactory = <T extends E = E>(input?: Subject<any> | t.EventBus<any>) => t.EventBus<T>;
 type Bus = BusFactory & {
   isBus(input: any): boolean;
   asType<T extends E>(bus: t.EventBus<any>): t.EventBus<T>;
   instance(bus: t.EventBus<any>): string;
-  clone: BusClone;
 };
 
 /**
@@ -64,81 +56,9 @@ function instance(bus: t.EventBus<any>) {
 }
 
 /**
- * Create a live clone of an event-bus.
- */
-function clone<T extends E = E>(
-  source: t.EventBus<any>,
-  config?: BusCloneDuplex<T> | BusCloneDuplexConfig<T>,
-): t.EventBus<T> {
-  if (!isBus(source)) throw new Error('Input not an event-bus');
-
-  // Create the target (child) bus.
-  const targetSubject$ = new Subject<void>();
-  const target = factory<T>(targetSubject$);
-  (target as any)._instance = `${(source as any)._instance}/bus.${id.slug()}`;
-
-  let source$ = source.$;
-  let target$ = target.$;
-
-  // Apply configuration filters.
-  if (Boolean(config)) {
-    if (typeof config === 'function') {
-      source$ = config(source$);
-      target$ = config(target$);
-    }
-    if (typeof config === 'object') {
-      if (typeof config.source === 'function') source$ = config.source(source$);
-      if (typeof config.target === 'function') target$ = config.target(target$);
-    }
-  }
-
-  let isDisposed = false;
-  let _ignoreSource: E | undefined; // NB: used to prevent firing loop between observables.
-  let _ignoreTarget: E | undefined;
-
-  const dispose = () => {
-    isDisposed = true;
-    targetSubject$.complete();
-  };
-
-  // Ferry events between [source:target]
-  source$
-    .pipe(
-      filter((e) => e !== _ignoreTarget),
-      takeWhile(() => !isDisposed),
-    )
-    .subscribe({
-      next(e) {
-        _ignoreSource = e;
-        target.fire(e);
-        _ignoreSource = undefined;
-      },
-      complete: () => dispose(),
-    });
-
-  target$
-    .pipe(
-      filter((e) => e !== _ignoreSource),
-      takeWhile(() => !isDisposed),
-    )
-    .subscribe({
-      next(e) {
-        _ignoreTarget = e;
-        source.fire(e);
-        _ignoreTarget = undefined;
-      },
-      complete: () => dispose(),
-    });
-
-  // Finish up.
-  return target;
-}
-
-/**
  * Export extended [bus] function.
  */
 (factory as any).isBus = isBus;
 (factory as any).asType = busAsType;
 (factory as any).instance = instance;
-(factory as any).clone = clone;
 export const bus = factory as Bus;
