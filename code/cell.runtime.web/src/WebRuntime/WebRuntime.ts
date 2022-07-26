ae74917b125b2529189e8f9fd36f705ff4763df7
Phil Cockfield <phil@cockfield.net>
2022-07-26 13:25:10 +1200

    comment

--- code/cell.runtime.web/src/WebRuntime/WebRuntime.ts
+++ code/cell.runtime.web/src/WebRuntime/WebRuntime.ts
@@ -5,7 +5,7 @@ import { remote } from './WebRuntime.remote';
  * Runtime environment for executing bundles within a web (browser) context.
  */
 export const WebRuntime = {
-  module: Runtime.module(), // NB: __CELL__ used within this method.
+  module: Runtime.module(), // NB: the compiled __CELL__ constant used within this method.
   remote,
 };
 
