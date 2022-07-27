9156d4d054e225b94a47031e95e7c334348f599a
Phil Cockfield <phil@cockfield.net>
2022-07-27 13:41:09 +1200

    clean

--- code/cell.modules/pkg.sys/net/src/ui/DEV.Sample/DEV.Version.tsx
+++ code/cell.modules/pkg.sys/net/src/ui/DEV.Sample/DEV.Version.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Color, COLORS, css, CssValue, Icons, t, time, WebRuntime } from './common';
 
