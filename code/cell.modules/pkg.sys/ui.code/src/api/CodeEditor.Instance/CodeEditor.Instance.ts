cee68f44108ee612488bf9abb257b5d08a6c2812
Phil Cockfield <phil@cockfield.net>
2022-07-29 09:41:58 +1200

    CodeEditor instance firing lifecycle events

--- code/cell.modules/pkg.sys/ui.code/src/api/CodeEditor.Instance/CodeEditor.Instance.ts
+++ code/cell.modules/pkg.sys/ui.code/src/api/CodeEditor.Instance/CodeEditor.Instance.ts
@@ -1,7 +1,7 @@
-import { Monaco } from '../../api';
+import { Monaco } from '..';
 import { slug, t, DEFAULT, rx } from '../common';
 import { CodeEditorAction } from './CodeEditor.Action';
-import { CodeEditorInstanceEvents } from '../Events';
+import { CodeEditorInstanceEvents, CodeEditorEvents } from '../Events';
 import { InstanceController } from './CodeEditor.Instance.Controller';
 import { MonacoListeners } from './CodeEditor.Instance.monacoListeners';
 import { select } from './CodeEditor.Instance.select';
@@ -30,32 +30,50 @@ export function CodeEditorInstance(args: {
   const listeners = MonacoListeners({ instance: { bus, id }, editor: monaco.instance });
   const events = CodeEditorInstanceEvents({ bus, id });
 
+  const { dispose$ } = events;
+  const global = CodeEditorEvents(bus, { dispose$ });
+
+  let _isDisposed = false;
+  dispose$.subscribe(() => (_isDisposed = true));
+
+  /**
+   * TODO 🐷
+   * - construct URI from derived filename?
+   *   Look this up in docs to understand usage scope more.
+   * - Drop the idea of an instance-id AND a filename, possibly just ONE unique identifier (???)
+   */
+
   const filename = args.filename ? args.filename?.replace(/^\/*/, '') : 'default.ts';
-  const uri = singleton.monaco.Uri.parse(`file:///${args.filename?.replace(/^\/*/, '')}`);
+  const uri = singleton.monaco.Uri.parse(`file:///${filename.replace(/^\/*/, '')}`);
 
   /**
    * TODO 🐷 TEMP
+   * Set this up globally via API ??
    */
   if (filename === 'one.ts') {
     const def = 'export declare function add(a: number, b: number): number';
     singleton.libs.add('node_modules/@types/math/index.d.ts', def);
   }
 
-  let language: t.CodeEditorLanguage = args.language ?? DEFAULT.MODEL.language;
-  const model = singleton.monaco.editor.createModel(DEFAULT.MODEL.text, language, uri);
-  instance.setModel(model);
+  let _language: t.CodeEditorLanguage = args.language ?? DEFAULT.MODEL.language;
+  instance.setModel(singleton.monaco.editor.createModel(DEFAULT.MODEL.text, _language, uri));
+
+  const getStatus = (): t.CodeEditorInstanceStatus => {
+    return { id, filename };
+  };
 
   const api: t.CodeEditorInstance = {
     id,
     instance,
     singleton,
     events,
+    dispose$,
 
     /**
-     * Assign focus to the editor.
+     * Flag indicating if the instance has been disposed.
      */
-    focus() {
-      instance.focus();
+    get isDisposed() {
+      return _isDisposed;
     },
 
     /**
@@ -66,6 +84,7 @@ export function CodeEditorInstance(args: {
     },
     set text(value: string) {
       // NB: Done via push-edit operation to preserve the undo stack.
+      const model = instance.getModel();
       model.pushEditOperations(
         [],
         [
@@ -81,13 +100,14 @@ export function CodeEditorInstance(args: {
      * Editor language.
      */
     get language() {
-      return language;
+      return _language;
     },
     set language(value: t.CodeEditorLanguage) {
-      if (value !== language) {
+      if (value !== _language) {
         const editor = singleton.monaco.editor;
-        editor.setModelLanguage(instance.getModel(), value);
-        language = value;
+        const model = instance.getModel();
+        editor.setModelLanguage(model, value);
+        _language = value;
       }
     },
 
@@ -96,6 +116,13 @@ export function CodeEditorInstance(args: {
      */
     get selection() {
       return Monaco.getSelection(instance);
+    },
+
+    /**
+     * Assign focus to the editor.
+     */
+    focus() {
+      instance.focus();
     },
 
     /**
@@ -118,9 +145,22 @@ export function CodeEditorInstance(args: {
     dispose() {
       listeners.dispose();
       events.dispose();
+
+      global.status.instance.fire({
+        instance: id,
+        action: 'Lifecycle:End',
+        info: getStatus(),
+      });
     },
   };
 
   InstanceController(bus, api);
+
+  global.status.instance.fire({
+    instance: id,
+    action: 'Lifecycle:Start',
+    info: getStatus(),
+  });
+
   return api;
 }
