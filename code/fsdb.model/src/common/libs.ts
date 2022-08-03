108527210a0c9ca360866537c504ededc49d0abd
Phil Cockfield <phil@cockfield.net>
2022-08-04 08:23:41 +1200

    archive

--- code/fsdb.model/src/common/libs.ts
+++
@@ -1,7 +0,0 @@
-export { value, defaultValue, time } from '@platform/util.value';
-
-/**
- * Ramda.
- */
-import { uniq, equals } from 'ramda';
-export const R = { uniq, equals };
--- code/fsdb.model/src/common/types.ts
+++
@@ -1,4 +0,0 @@
-export * from '../types';
-
-export * from '@platform/fsdb.types/lib/types';
-export { JsonMap } from '@platform/types';
