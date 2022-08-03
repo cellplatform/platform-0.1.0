a4ea4f43b67fc16095e5a3eef5434675b741de8b
Phil Cockfield <phil@cockfield.net>
2022-08-03 13:42:22 +1200

    micro - archive

--- code/micro/src/common/libs.ts
+++
@@ -1,6 +0,0 @@
-import micro, { send } from 'micro';
-export { micro, send };
-
-export { log } from '@platform/log/lib/server';
-export { value, time, defaultValue } from '@platform/util.value';
-export { Router } from '@platform/http.router';
