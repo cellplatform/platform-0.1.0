0a00a051f37d4f829c9e0e8c748f652151c4e9d2
Phil Cockfield <phil@cockfield.net>
2022-08-04 09:11:02 +1200

    archive

--- code/cell.cli/src/common/libs.ts
+++
@@ -1,8 +0,0 @@
-import { cli } from '@platform/cli';
-export { cli };
-
-import { log, chalk } from '@platform/log/lib/server';
-export { log, chalk };
-
-import { defaultValue, time } from '@platform/util.value';
-export { defaultValue, time };
--- code/cell.cli/src/common/types.ts
+++
@@ -1,5 +0,0 @@
-export * from '@platform/types';
-export * from '@platform/cell.types';
-export * from '@platform/cli/lib/types';
-
-export * from '../types';
