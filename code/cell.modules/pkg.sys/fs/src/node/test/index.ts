d1b4be03ae9817748bfe3d9a2a316ba1c61f9699
Phil Cockfield <phil@cockfield.net>
2022-08-04 10:46:51 +1200

    fix: sys.fs

--- code/cell.modules/pkg.sys/fs/src/node/test/index.ts
+++ code/cell.modules/pkg.sys/fs/src/node/test/index.ts
@@ -1,9 +1,5 @@
 export { expect, expectError } from '@platform/test';
-import { Schema } from '../../web/common';
 
 export { Filesystem } from '../../web';
 export * from '../../web/common';
 export * from './TestFs';
-export * from './TestPrep';
-
-Schema.Uri.ALLOW.NS = ['foo*'];
