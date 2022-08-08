6af91ca809bf444bb3b2196a35beb49b3973983d
Phil Cockfield <phil@cockfield.net>
2022-08-09 08:22:11 +1200

    archive: util.animation

--- code/util.animate/src/animation/animation.TEST.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { animation } from '.';
-
-describe('animation', () => {
-  it('exists', () => {
-    expect(animation.start).to.be.an.instanceof(Function);
-  });
-});
