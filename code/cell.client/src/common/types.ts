5bf123aa45ecb52916d83a71b3626b33f68563cd
Phil Cockfield <phil@cockfield.net>
2022-08-09 10:42:08 +1200

    Inline MemoryQueue => cell.client

--- code/cell.client/src/common/types.ts
+++ code/cell.client/src/common/types.ts
@@ -8,6 +8,8 @@ export * from '@platform/types';
 export * from '@platform/cell.types';
 export * from '@platform/http/lib/types';
 export * from '@platform/http.types';
+
+export * from '../MemoryQueue/types';
 export * from '../types';
 
 /**
