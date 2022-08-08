da0a86d0f36395413ba93320190495395f9247d7
Phil Cockfield <phil@cockfield.net>
2022-08-08 12:15:05 +1200

    Update refs

--- code/cli.prompt/src/prompt/prompt.text.ts
+++ code/cli.prompt/src/prompt/prompt.text.ts
@@ -1,11 +1,11 @@
-import { inquirer } from '../common';
+import { inquirer, t } from '../common';
 
 /**
  * Prompts the user for a simple text value.
  */
 export async function text(args: { message: string; default?: string }) {
   const { message } = args;
-  const question: inquirer.InputQuestion = {
+  const question: t.InputQuestion = {
     type: 'input',
     name: 'result',
     message,
